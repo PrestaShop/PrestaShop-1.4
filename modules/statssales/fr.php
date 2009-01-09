f2d17c43141a5d32706b1829f1d16d617e589f0a
dMetzger <damien.metzger@prestashop.com>
2009-01-09 13:51:44 +0000

    // typo fixed

--- modules/statssales/fr.php
+++ modules/statssales/fr.php
@@ -3,8 +3,10 @@
 global $_MODULE;
 $_MODULE = array();
 $_MODULE['<{statssales}prestashop>statssales_45c4b3e103155326596d6ccd2fea0f25'] = 'Commandes et CA';
-$_MODULE['<{statssales}prestashop>statssales_09a6e97d1ccb13182c8fd316450dbebf'] = 'Affiche l\'évolution du chiffre d\'affaire et la répartition des commandes par statut';
-$_MODULE['<{statssales}prestashop>statssales_a252c24ec397a14367098b74b482d7a0'] = 'Ces graphiques représentent l\'évolution de vos commandes et chiffre d\'affaire pour une période donnée. Ce n\'est pas un outil d\'analyse avancé mais il vous permet toutefois de visualiser la rentabilité de votre boutique en un clin d\'oeil. Vous pouvez également comparer les ventes entre différentes périodes (Noël...).rnSeules les commandes valides sont inclues dans ces deux graphes.';
+$_MODULE['<{statssales}prestashop>statssales_09a6e97d1ccb13182c8fd316450dbebf'] = 'Affiche l\'évolution du chiffre d\'affaires et la répartition des commandes par statut';
+$_MODULE['<{statssales}prestashop>statssales_b1c94ca2fbc3e78fc30069c8d0f01680'] = 'Tout afficher';
+$_MODULE['<{statssales}prestashop>statssales_d7778d0c64b6ba21494c97f77a66885a'] = 'Filtrer';
+$_MODULE['<{statssales}prestashop>statssales_a252c24ec397a14367098b74b482d7a0'] = 'Ces graphiques représentent l\'évolution de vos commandes et chiffre d\'affaires pour une période donnée. Ce n\'est pas un outil d\'analyse avancé mais il vous permet toutefois de visualiser la rentabilité de votre boutique en un clin d\'oeil. Vous pouvez également comparer les ventes entre différentes périodes (Noël...).rnSeules les commandes valides sont inclues dans ces deux graphes.';
 $_MODULE['<{statssales}prestashop>statssales_0d6f98eca901ce80817bfe6a475fa45c'] = 'Total des commandes passées';
 $_MODULE['<{statssales}prestashop>statssales_cbeb7927352ee55fc2d3d2c05495db80'] = 'Total des produits commandés';
 $_MODULE['<{statssales}prestashop>statssales_ec3e48bb9aa902ba2ad608547fdcbfdc'] = 'Chiffre d\'affaires :';
@@ -15,7 +17,7 @@ $_MODULE['<{statssales}prestashop>statssales_cc23e852e21d85a834defe24867e3d05'] 
 $_MODULE['<{statssales}prestashop>statssales_58d14111d598ec5174fe85dfebe71c56'] = 'Nombre de commandes et produits commandés';
 $_MODULE['<{statssales}prestashop>statssales_7442e29d7d53e549b78d93c46b8cdcfc'] = 'Commandes';
 $_MODULE['<{statssales}prestashop>statssales_068f80c7519d0528fb08e82137a72131'] = 'Produits';
-$_MODULE['<{statssales}prestashop>statssales_14f1c54626d722168ee62dff05ed811e'] = 'Chiffre d\'affaire en';
+$_MODULE['<{statssales}prestashop>statssales_14f1c54626d722168ee62dff05ed811e'] = 'Chiffre d\'affaires en';
 $_MODULE['<{statssales}prestashop>statssales_33eee7690a8cd62490ed9eeadd47d163'] = 'Pourcentage de commandes par statut';
 
 ?>
