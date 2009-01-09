afec8bc0feb5a925660a518191f986f24e70cd43
dMetzger <damien.metzger@prestashop.com>
2009-01-09 13:13:33 +0000

    [+] MO : new module statssearch (keywords of the internal search engine)

---
+++ modules/statssearch/fr.php
@@ -0,0 +1,13 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{statssearch}prestashop>statssearch_5fb9f4ff81ee04efa9f4039099a588dc'] = 'Recherche interne';
+$_MODULE['<{statssearch}prestashop>statssearch_8bd5e5a265ca16af735b925ac9d6d7f4'] = 'Affiche les mots clés recherchés par vos visiteurs';
+$_MODULE['<{statssearch}prestashop>statssearch_867343577fa1f33caa632a19543bd252'] = 'Mots clés';
+$_MODULE['<{statssearch}prestashop>statssearch_40d28e924bb30a0c3d5e45dab97861b5'] = 'Occurences';
+$_MODULE['<{statssearch}prestashop>statssearch_19945d4dc9feea9e8267ed0468b53e53'] = 'Aucun mot clé trouvé';
+$_MODULE['<{statssearch}prestashop>statssearch_8d01ac22a68176ddd7067da99508751d'] = '10 premiers mots clés';
+$_MODULE['<{statssearch}prestashop>statssearch_52ef9633d88a7480b3a938ff9eaa2a25'] = 'Autres';
+
+?>
---
+++ modules/statssearch/statssearch.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+  * Statistics
+  * @category stats
+  *
+  * @author Damien Metzger / Epitech
+  * @copyright Epitech / PrestaShop
+  * @license http://www.opensource.org/licenses/osl-3.0.php Open-source licence 3.0
+  * @version 1.1
+  */
+  
+class StatsSearch extends ModuleGraph
+{
+    private $_html = '';
+	private $_query = '';
+	private $_query2 = '';
+
+    function __construct()
+    {
+        $this->name = 'statssearch';
+        $this->tab = 'Stats';
+        $this->version = 1.0;
+		$this->page = basename(__FILE__, '.php');
+		$this->_query = '
+		SELECT ss.`keyword`, COUNT(TRIM(ss.`keyword`)) as occurences
+		FROM `'._DB_PREFIX_.'statssearch` ss
+		WHERE ss.`date_add` LIKE \'';
+		$this->_query2 = '\'
+		GROUP BY TRIM(ss.`keyword`)
+		ORDER BY COUNT(ss.`keyword`) DESC';
+
+        parent::__construct();
+		
+        $this->displayName = $this->l('Customer search');
+        $this->description = $this->l('Display which keywords have been searched by your visitors');
+    }
+
+	function install()
+	{
+		if (!parent::install() OR !$this->registerHook('top') OR !$this->registerHook('AdminStatsModules'))
+			return false;
+		return Db::getInstance()->Execute('
+		CREATE TABLE `'._DB_PREFIX_.'statssearch` (
+			id_statssearch INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+			keyword VARCHAR(256) NOT NULL,
+			date_add DATETIME NOT NULL,
+			PRIMARY KEY(id_statssearch)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8');
+	}
+	
+    function uninstall()
+    {
+        if (!parent::uninstall())
+			return false;
+		return (Db::getInstance()->Execute('DROP TABLE `'._DB_PREFIX_.'statssearch`'));
+    }
+	
+	function hookTop($params)
+	{
+		if ($query = trim(Tools::getValue('search_query')) AND Validate::isValidSearch($query))
+			Db::getInstance()->Execute('
+			INSERT INTO `'._DB_PREFIX_.'statssearch` (`keyword`,`date_add`)
+			VALUES (\''.pSQL($query).'\', NOW())');
+	}
+	
+	function hookAdminStatsModules()
+	{
+		$result = Db::getInstance()->ExecuteS($this->_query.pSQL(ModuleGraph::getDateLike()).$this->_query2);
+		$this->_html = '
+		<fieldset class="width3"><legend><img src="../modules/'.$this->name.'/logo.gif" /> '.$this->displayName.'</legend>';
+		$table = '<table class="table" border="0" cellspacing="0" cellspacing="0">
+		<thead>
+			<tr><th style="width:400px;">'.$this->l('Keywords').'</th>
+			<th style="width:50px; text-align: right">'.$this->l('Occurences').'</th></tr>
+		</thead>';
+
+		$i = 0;
+		foreach ($result as $index => $row)
+		{
+			$keyword =& $row['keyword'];
+			$occurences =& $row['occurences'];
+			if ($i === 0)
+				$table .= '<tbody>';
+			$table .= '<tr><td>'.$keyword.'</td><td style="text-align: right">'.$occurences.'</td></tr>';
+			$i++;
+		}
+		
+		if ($i > 0)
+		{
+			$table .= '</tbody></table>';
+			$this->_html .= $table;
+			$this->_html .= '<br /><center>'.ModuleGraph::engine(array('type' => 'pie')).'</center>';
+		}
+		else
+			$this->_html .= '<p><strong>'.$this->l('No keyword found').'</strong></p>';
+		$this->_html .= '</fieldset>';
+		return $this->_html;
+	}
+	
+	protected function getData($layers)
+	{
+		$this->_titles['main'] = $this->l('10 first keywords');
+		$totalResult = Db::getInstance()->ExecuteS($this->_query.pSQL(ModuleGraph::getDateLike()).$this->_query2);
+		$total = 0;
+		$total2 = 0;
+		foreach ($totalResult as $totalRow)
+			$total += $totalRow['occurences'];
+		$result = Db::getInstance()->ExecuteS($this->_query.pSQL(ModuleGraph::getDateLike()).$this->_query2.' LIMIT 9');
+		foreach ($result as $row)
+		{
+			if (!$row['occurences'])
+				continue;
+			$this->_legend[] = $row['keyword'];
+			$this->_values[] = $row['occurences'];
+			$total2 += $row['occurences'];
+		}
+		if ($total > $total2)
+		{
+			$this->_legend[] = $this->l('Others');
+			$this->_values[] = $total - $total2;
+		}
+	}
+}
+
+?>
