21fbf543229e443d98bc76b6cd2edb1a45744d1a
lCherifi <lucas.cherifi@prestashop.com>
2009-01-14 18:25:25 +0000

    [-] FO : fixed #1074 : block categorie when depth = 0

--- modules/blockcategories/blockcategories.php
+++ modules/blockcategories/blockcategories.php
@@ -97,10 +97,13 @@ class BlockCategories extends Module
 		));
 		/* ELSE */
 		
+		$maxdepth = Configuration::get('BLOCK_CATEG_MAX_DEPTH');
+		
 		$result = Db::getInstance()->ExecuteS('
 		SELECT * FROM '._DB_PREFIX_.'category c 
 		LEFT JOIN '._DB_PREFIX_.'category_lang cl ON (c.id_category = cl.id_category AND id_lang = '.intval($params['cookie']->id_lang).')
-		WHERE level_depth <= '.intval(Configuration::get('BLOCK_CATEG_MAX_DEPTH')).'
+		WHERE 1'
+		.(intval($maxdepth) != 0 ? 'AND level_depth <= '.intval($maxdepth) : '').'
 		AND c.active = 1
 		ORDER BY level_depth, cl.name ASC');
 		$resultParents = array();
