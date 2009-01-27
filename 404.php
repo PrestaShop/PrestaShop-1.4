ac3fc3466c580f89a4b2b530a2fcfcb8deee1017
dMetzger <damien.metzger@prestashop.com>
2009-01-27 10:44:47 +0000

    [*] FO : 404 page now manage images

--- 404.php
+++ 404.php
@@ -1,5 +1,12 @@
 <?php
 
+if (in_array(substr($_SERVER['REQUEST_URI'], -3), array('png', 'jpg', 'gif')))
+{
+	include(dirname(__FILE__).'/config/settings.inc.php');
+	header('Location: '.__PS_BASE_URI__.'img/404.gif');
+	exit;
+}
+
 include(dirname(__FILE__).'/config/config.inc.php');
 include(dirname(__FILE__).'/header.php');
 
