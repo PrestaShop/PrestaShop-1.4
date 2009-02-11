c436c21102ad6d207b07fe6b2808449af2f3107b
lCherifi <lucas.cherifi@prestashop.com>
2009-02-11 18:20:31 +0000

    [-] fix url rewriting bug (fixed #1249)

--- js/admin.js
+++ js/admin.js
@@ -5,13 +5,16 @@ function str2url(str,encoding,ucfirst)
 	str = str.toUpperCase();
 	str = str.toLowerCase();
 
-	str = str.replace(/[\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5]/g,'a');
-	str = str.replace(/[\u00E7]/g,'c');
-	str = str.replace(/[\u00E8\u00E9\u00EA\u00EB]/g,'e');
+	str = str.replace(/[\u0105\u0104\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5]/g,'a');
+	str = str.replace(/[\u00E7\u0107\u0106]/g,'c');
+	str = str.replace(/[\u00E8\u00E9\u00EA\u00EB\u0119\u0118]/g,'e');
 	str = str.replace(/[\u00EC\u00ED\u00EE\u00EF]/g,'i');
-	str = str.replace(/[\u00F2\u00F3\u00F4\u00F5\u00F6\u00F8]/g,'o');
+	str = str.replace(/[\u0142\u0141]/g,'l');
+	str = str.replace(/[\u00F2\u00F3\u00F4\u00F5\u00F6\u00F8\u00D3]/g,'o');
+	str = str.replace(/[\u015B\u015A]/g,'s');
 	str = str.replace(/[\u00F9\u00FA\u00FB\u00FC]/g,'u');
 	str = str.replace(/[\u00FD\u00FF]/g,'y');
+	str = str.replace(/[\u017C\u017A\u017B\u0179]/g,'z');
 	str = str.replace(/[\u00F1]/g,'n');
 	str = str.replace(/[\u0153]/g,'oe');
 	str = str.replace(/[\u00E6]/g,'ae');
