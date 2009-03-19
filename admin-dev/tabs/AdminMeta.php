9f96ba95bd86fa94b03b8668f69653e5746bd860
gRoussac <gRoussac@prestashop.com>
2009-03-19 10:12:48 +0000

    [-] BO : Fix bug in AdminMeta.php

--- admin-dev/tabs/AdminMeta.php
+++ admin-dev/tabs/AdminMeta.php
@@ -57,7 +57,11 @@ class AdminMeta extends AdminTab
 					echo '
 					<select name="page">';
 					foreach ($files as $file)
-						echo '<option value="'.$file.'">'.$file.'.php&nbsp;</option>';
+					{
+						echo '<option value="'.$file.'"';
+						echo $meta->page == $file? ' selected="selected"' : '' ;
+						echo'>'.$file.'.php&nbsp;</option>';
+					}
 					echo '
 					</select><sup> *</sup>
 					<p style="clear: both;">'.$this->l('Name of the related page').'</p>';
