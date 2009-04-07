d87b5423900e5c54cc37f86eb32df7bb39a8b4df
dMetzger <damien.metzger@prestashop.com>
2009-04-07 11:27:27 +0000

    // Disable trackingfront display when inactive

--- modules/trackingfront/stats.php
+++ modules/trackingfront/stats.php
@@ -5,6 +5,8 @@ include(dirname(__FILE__).'/../../init.php');
 include(dirname(__FILE__).'/trackingfront.php');
 
 $tf = new TrackingFront();
+if (!$tf->active)
+	Tools::redirect('404.php');
 $tf->postProcess();
 echo $tf->isLogged() ? $tf->displayAccount() : $tf->displayLogin();
 
