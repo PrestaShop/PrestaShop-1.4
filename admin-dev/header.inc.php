dee59a17ac5f320ba13469b7baa4d43fd967984b
pSang <pSang@prestashop.com>
2009-05-05 13:05:16 +0000

    [*] BO : added no cache header (thanks to Antony)

--- admin-dev/header.inc.php
+++ admin-dev/header.inc.php
@@ -14,6 +14,10 @@
 // P3P Policies (http://www.w3.org/TR/2002/REC-P3P-20020416/#compact_policies)
 header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"');
 
+header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0'); // HTTP/1.1
+header('Pragma: no-cache');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+
 require_once(dirname(__FILE__).'/init.php');
 ?>
 
