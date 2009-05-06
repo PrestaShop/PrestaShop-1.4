fdea1dcea982c5fa0573551a3580a6eb1b6921cb
pSang <pSang@prestashop.com>
2009-05-06 14:31:29 +0000

    [*] MO : fixed some bugs in cashondelivery

--- modules/cashondelivery/cashondelivery.php
+++ modules/cashondelivery/cashondelivery.php
@@ -6,7 +6,7 @@ class CashOnDelivery extends PaymentModule
 	{
 		$this->name = 'cashondelivery';
 		$this->tab = 'Payment';
-		$this->version = '0.2';
+		$this->version = '0.3';
 		
 		$this->currencies = false;
 
@@ -18,7 +18,7 @@ class CashOnDelivery extends PaymentModule
 
 	public function install()
 	{
-        if (!parent::install() OR !$this->registerHook('payment') OR !$this->registerHook('paymentReturn'))
+		if (!parent::install() OR !$this->registerHook('payment') OR !$this->registerHook('paymentReturn'))
 			return false;
 		return true;
 	}
@@ -26,15 +26,19 @@ class CashOnDelivery extends PaymentModule
 	public function hookPayment($params)
 	{
 		global $smarty;
-		
+
+		// Check if cart has product download
 		foreach ($params['cart']->getProducts() AS $product)
-			if (Validate::isUnsignedInt(ProductDownload::getIdFromIdProduct(intval($product['id_product']))))
+		{
+			$pd = ProductDownload::getIdFromIdProduct(intval($product['id_product']));
+			if ($pd AND Validate::isUnsignedInt($pd))
 				return false;
-		
+		}
+
 		$smarty->assign(array(
-            'this_path' => $this->_path,
-            'this_path_ssl' => (Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://').htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT, 'UTF-8').__PS_BASE_URI__.'modules/'.$this->name.'/'
-            ));
+			'this_path' => $this->_path,
+			'this_path_ssl' => (Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://').htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT, 'UTF-8').__PS_BASE_URI__.'modules/'.$this->name.'/'
+		));
 		return $this->display(__FILE__, 'payment.tpl');
 	}
 	
