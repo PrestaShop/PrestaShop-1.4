eb7b9c0a296cfcb70cd5deb9d60e27146b98cb81
pSang <pSang@prestashop.com>
2009-05-18 15:42:22 +0000

    [-] MO : fixed secure key bug in cashondelivery

--- modules/cashondelivery/validation.php
+++ modules/cashondelivery/validation.php
@@ -9,11 +9,12 @@ $confirm = Tools::getValue('confirm');
 /* Validate order */
 if ($confirm)
 {
- 	$cashOnDelivery = new CashOnDelivery();
-	$total = floatval(number_format($cart->getOrderTotal(true, 3), 2, '.', ''));
+	$customer = new Customer(intval($cart->id_customer));
+	$cashOnDelivery = new CashOnDelivery();
+	$total = $cart->getOrderTotal(true, 3);
 	$cashOnDelivery->validateOrder(intval($cart->id), _PS_OS_PREPARATION_, $total, $cashOnDelivery->displayName);
 	$order = new Order(intval($cashOnDelivery->currentOrder));
-	Tools::redirectLink(__PS_BASE_URI__.'order-confirmation.php?id_cart='.intval($cart->id).'&id_module='.intval($cashOnDelivery->id).'&id_order='.intval($cashOnDelivery->currentOrder));
+	Tools::redirectLink(__PS_BASE_URI__.'order-confirmation.php?key='.$customer->secure_key.'&id_cart='.intval($cart->id).'&id_module='.intval($cashOnDelivery->id).'&id_order='.intval($cashOnDelivery->currentOrder));
 }
 else
 {
@@ -24,9 +25,8 @@ else
 		'this_path_ssl' => (Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://').htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT, 'UTF-8').__PS_BASE_URI__.'modules/cashondelivery/'
 	));
 
-    $smarty->assign('this_path', __PS_BASE_URI__.'modules/cashondelivery/');
-    echo Module::display(__FILE__, 'validation.tpl');
+	$smarty->assign('this_path', __PS_BASE_URI__.'modules/cashondelivery/');
+	echo Module::display(__FILE__, 'validation.tpl');
 }
 
 include(dirname(__FILE__).'/../../footer.php');
-?>
--- order-confirmation.php
+++ order-confirmation.php
@@ -12,7 +12,6 @@ $id_order = Order::getOrderByCartId(intval($id_cart));
 $secure_key = isset($_GET['key']) ? $_GET['key'] : false;
 if (!$id_order OR !$id_module OR !$secure_key OR empty($secure_key))
 	Tools::redirect('history.php');
-	
 $order = new Order(intval($id_order));
 if (!Validate::isLoadedObject($order) OR $order->id_customer != $cookie->id_customer OR $secure_key != $order->secure_key)
 	Tools::redirect('history.php');
@@ -22,9 +21,7 @@ if ($order->payment != $module->displayName)
 $smarty->assign(array(
 	'HOOK_ORDER_CONFIRMATION' => Hook::orderConfirmation(intval($id_order)),
 	'HOOK_PAYMENT_RETURN' => Hook::paymentReturn(intval($id_order), intval($id_module))));
-	
+
 $smarty->display(_PS_THEME_DIR_.'order-confirmation.tpl');
 
 include(dirname(__FILE__).'/footer.php');
-
-?>
