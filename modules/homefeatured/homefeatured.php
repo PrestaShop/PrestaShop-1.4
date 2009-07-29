0a2901ecb1a6522f83e16fdd62a67f3f5eeab5d0
pSang <pSang@prestashop.com>
2009-07-29 14:32:54 +0000

    [-] MO : deleted an old Tools::d()

--- modules/homefeatured/homefeatured.php
+++ modules/homefeatured/homefeatured.php
@@ -66,7 +66,6 @@ class HomeFeatured extends Module
 		$category = new Category(1);
 		$nb = intval(Configuration::get('HOME_FEATURED_NBR'));
 		$products = $category->getProducts(intval($params['cookie']->id_lang), 1, ($nb ? $nb : 10), 'date_add', 'DESC');
-		//Tools::d($products);
 		$smarty->assign(array(
 			'allow_buy_when_out_of_stock' => Configuration::get('PS_ORDER_OUT_OF_STOCK', false),
 			'max_quantity_to_allow_display' => Configuration::get('PS_LAST_QTIES'),
