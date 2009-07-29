c259fabbe91caa755785450d329993dae9da8e2d
pSang <pSang@prestashop.com>
2009-07-29 15:10:31 +0000

    //v1.2 (1.2.0.8)

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Improve PHP configuration to prevent issues */
-@ini_set('display_errors', 'on');
+@ini_set('display_errors', 'off');
 @ini_set('upload_max_filesize', '100M');
 @ini_set('default_charset', 'utf-8');
 
