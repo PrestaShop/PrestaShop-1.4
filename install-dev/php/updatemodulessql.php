760dea0036bc412acc9bd9a6a17e9357df714fa2
pSang <pSang@prestashop.com>
2009-07-29 12:15:30 +0000

    [-] Installer : fixed while updating modules structure (#2492)

---
+++ install-dev/php/updatemodulessql.php
@@ -0,0 +1,15 @@
+<?php
+
+function update_modules_sql()
+{
+	$blocklink = Module::getInstanceByName('blocklink');
+	if ($blocklink->id)
+		Db::getInstance()->Execute('ALTER IGNORE TABLE `'._DB_PREFIX_.'blocklink_lang` ADD PRIMARY KEY (`id_link`, `id_lang`);');
+	$productComments = Module::getInstanceByName('productcomments');
+	if ($productComments->id)
+	{
+		Db::getInstance()->Execute('ALTER IGNORE TABLE `'._DB_PREFIX_.'product_comment_grade` ADD PRIMARY KEY (`id_product_comment`, `id_product_comment_criterion`);');
+		Db::getInstance()->Execute('ALTER IGNORE TABLE `'._DB_PREFIX_.'product_comment_criterion` DROP PRIMARY KEY, ADD PRIMARY KEY (`id_product_comment_criterion`, `id_lang`);');
+		Db::getInstance()->Execute('ALTER IGNORE TABLE `'._DB_PREFIX_.'product_comment_criterion_product` ADD PRIMARY KEY(`id_product`, `id_product_comment_criterion`);');
+	}
+}
--- install-dev/sql/upgrade/1.2.0.7.sql
+++ install-dev/sql/upgrade/1.2.0.7.sql
@@ -4,11 +4,9 @@ SET NAMES 'utf8';
 /* 				STRUCTURE			 		 */
 /* ##################################### */
 
-ALTER IGNORE TABLE `PREFIX_blocklink_lang` ADD PRIMARY KEY (`id_link`, `id_lang`);
-ALTER IGNORE TABLE `PREFIX_product_comment_grade` ADD PRIMARY KEY (`id_product_comment`, `id_product_comment_criterion`);
-ALTER IGNORE TABLE `PREFIX_product_comment_criterion` DROP PRIMARY KEY, ADD PRIMARY KEY (`id_product_comment_criterion`, `id_lang`);
-ALTER IGNORE TABLE `PREFIX_product_comment_criterion_product` ADD PRIMARY KEY(`id_product`, `id_product_comment_criterion`);
 
 /* ##################################### */
 /* 					CONTENTS					 */
 /* ##################################### */
+
+/* PHP:update_modules_sql(); */;
