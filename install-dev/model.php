7d9cb303999b50336be9a7425b88980ce817338c
pSang <pSang@prestashop.com>
2009-09-09 13:38:32 +0000

    [*] Installer : added 1.2.3.0.sql file

--- install-dev/model.php
+++ install-dev/model.php
@@ -7,7 +7,7 @@ if ($tmp = strpos($_SERVER['REQUEST_URI'], '?'))
 	$_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 0, $tmp);
 $_SERVER['REQUEST_URI'] = str_replace('//', '/', $_SERVER['REQUEST_URI']);
 
-define('INSTALL_VERSION', '1.2.2.0');
+define('INSTALL_VERSION', '1.2.3.0');
 define('INSTALL_PATH', dirname(__FILE__));
 define('SETTINGS_FILE', INSTALL_PATH.'/../config/settings.inc.php');
 define('INSTALLER__PS_BASE_URI', substr($_SERVER['REQUEST_URI'], 0, -1 * (strlen($_SERVER['REQUEST_URI']) - strrpos($_SERVER['REQUEST_URI'], '/')) - strlen(substr(dirname($_SERVER['REQUEST_URI']), strrpos(dirname($_SERVER['REQUEST_URI']), '/')+1))));
