adbd30393631194bcc6643de5cad6eae70d6e28c
pSang <pSang@prestashop.com>
2009-09-09 14:36:12 +0000

    //[-] Classes : fixed group SQL slowness

--- modules/blockcategories/blockcategories.php
+++ modules/blockcategories/blockcategories.php
@@ -104,12 +104,11 @@ class BlockCategories extends Module
 		SELECT DISTINCT c.*, cl.*
 		FROM `'._DB_PREFIX_.'category` c 
 		LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category` AND `id_lang` = '.intval($params['cookie']->id_lang).')
-		LEFT JOIN `'._DB_PREFIX_.'category_group` ctg ON (ctg.`id_category` = c.`id_category`)
-		'.($id_customer ? 'INNER JOIN `'._DB_PREFIX_.'customer_group` cg ON (cg.`id_group` = ctg.`id_group` AND cg.`id_customer` = '.intval($id_customer).')' : '' ).'
+		LEFT JOIN `'._DB_PREFIX_.'category_group` cg ON (cg.`id_category` = c.`id_category`)
 		WHERE 1'
 		.(intval($maxdepth) != 0 ? ' AND `level_depth` <= '.intval($maxdepth) : '').'
 		AND (c.`active` = 1 OR c.`id_category`= 1)
-		'.(!$id_customer ? 'AND ctg.`id_group` = 1' : '' ).'
+		AND cg.`id_group` '.(!$cookie->id_customer ?  '= 1' : 'IN (SELECT id_group FROM '._DB_PREFIX_.'customer_group WHERE id_customer = '.intval($cookie->id_customer).')').'
 		ORDER BY `level_depth` ASC, cl.`name` ASC'))
 			return;
 		$resultParents = array();
