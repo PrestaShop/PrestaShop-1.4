47e1b53505578967f89a7bd8ae11f3e8300b4689
pSang <pSang@prestashop.com>
2009-09-28 14:07:47 +0000

    //[*] MO : dynamic images for PayPalAPI

--- modules/paypalapi/express/submit.php
+++ modules/paypalapi/express/submit.php
@@ -77,7 +77,7 @@ function displayConfirm()
 	include(_PS_ROOT_DIR_.'/header.php');
 
 	$smarty->assign(array(
-		'logo' => $ppPayment->getLogo(),
+		'logo' => $ppExpress->getLogo(),
 		'ppToken' => strval($cookie->paypal_token),
 		'cust_currency' => $cookie->id_currency,
 		'currencies' => $ppExpress->getCurrency(),
