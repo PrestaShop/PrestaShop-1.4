3a5460174bb6e96c7694d4678457fdea7803efe8
mBiart <mmathieu.biart@prestashop.com>
2009-10-01 09:32:40 +0000

    [-] BO/FO : Swift SMTP sending fixed (when no authenticator was set)

--- tools/swift/Swift/Authenticator/index.php
+++ tools/swift/Swift/Authenticator/index.php
@@ -7,5 +7,7 @@ header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-header("Location: ../");
+// The header location has been commented because swift do a require() on each .php of this folder
+// (and this relocation make it do it recursively on each PrestaShop folder and so PrestaShop PHP file)
+//header("Location: ../");
 exit;
