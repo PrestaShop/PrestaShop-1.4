7340cdfe3975b3dbfeadd8bb44a003315e050f33
jVincent <jVincent@prestashop.com>
2009-10-28 13:07:55 +0000

    [-] BO : fixed bug sorting products in catalog

--- admin-dev/tabs/AdminProducts.php
+++ admin-dev/tabs/AdminProducts.php
@@ -2729,7 +2729,7 @@ class AdminProducts extends AdminTab
 		$id_category = intval(Tools::getValue('id_category'));
 		if ($id_category)
 			$currentIndex .= '&id_category='.$id_category.'&token='.$this->token;
-		parent::displayListHeader();
+		parent::displayListHeader($token);
 	}
 }
 
