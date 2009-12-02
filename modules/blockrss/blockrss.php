4a99cf8883392c15a6d490638ba48a2a7d2e16c3
aFolletete <alain.folletete@prestashop.com>
2009-12-02 15:34:09 +0000

    [-] MO : blockrss, fixed bug on install

--- modules/blockrss/blockrss.php
+++ modules/blockrss/blockrss.php
@@ -26,6 +26,7 @@ class Blockrss extends Module
 		Configuration::updateValue('RSS_FEED_NBR', 5);
  	 	if (parent::install() == false OR $this->registerHook('leftColumn') == false)
  	 		return false;
+		return true;
   	}
 
 	public function getContent()
