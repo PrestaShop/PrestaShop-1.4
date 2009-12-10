3abe0d7c0c1e7918ab60ff7cf85cb215c1447777
aFolletete <alain.folletete@prestashop.com>
2009-12-10 10:55:43 +0000

    [-] Translations : Fixed all mail templates for Spanish

--- mails/es/lang.php
+++ mails/es/lang.php
@@ -2,5 +2,12 @@
 
 global $_LANGMAIL;
 $_LANGMAIL = array();
+$_LANGMAIL['Your new admin password'] = 'Su nueva contraseña de Administrador';
+$_LANGMAIL['Your password'] = 'Su contraseña';
+$_LANGMAIL['Welcome!'] = 'Bienvenido !';
+$_LANGMAIL['Order confirmation'] = 'Confirmación de pedido';
+$_LANGMAIL['Message from contact form'] = 'Mensaje desde el formulario de contacto';
+$_LANGMAIL['My personal informations'] = 'Información personal';
+$_LANGMAIL['Message from a customer'] = 'Mensaje de un cliente';
 
 ?>
