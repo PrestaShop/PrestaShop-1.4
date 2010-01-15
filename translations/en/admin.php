7523e24fb97b8bf56755d6efaefa922451235d7a
aFolletete <alain.folletete@prestashop.com>
2010-01-15 16:16:56 +0000

    [-] Translations : Fixed a translation

--- translations/en/admin.php
+++ translations/en/admin.php
@@ -7,6 +7,7 @@ $_LANGADM['AdminEmails1d4f4376ecd104b52b7e805f7277f46e'] = 'Send e-mail as Text'
 $_LANGADM['AdminEmailsc23370df50e7f5c6d161da0096b1dd9d'] = 'Test your e-mail configuration';
 $_LANGADM['AdminEmailsf1251a7f9b3741c1be21596cc52f30b0'] = 'Send a e-mail test at:';
 $_LANGADM['AdminEmails9b161d94627dcda71b2df1391107f00c'] = 'Send a e-mail test';
+$_LANGADM['AdminGenerator8f106af7bcea3cc7150abc9519345ef3'] = 'This tool will automatically generate a \"robots.txt\" file that will grant you the possibility to deny access to search engines for some pages.';
 $_LANGADM['AdminImport287234a1ff35a314b5b6bc4e5828e745'] = 'Combinations';
 $_LANGADM['AdminStatuses0133c7a0fa55069df27f8d9be2c6ab89'] = 'Order statuses';
 $_LANGADM['AdminStatusesdc0d0ea31daf9b78138943e8fd06e3a3'] = 'Order return statuses';
