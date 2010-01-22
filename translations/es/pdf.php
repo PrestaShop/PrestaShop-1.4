cfb9dc99fd8e87e80fee28257a756c6c215fc2f5
aFolletete <alain.folletete@prestashop.com>
2010-01-22 13:38:07 +0000

    // new spannish translations added

--- translations/es/pdf.php
+++ translations/es/pdf.php
@@ -2,12 +2,11 @@
 
 global $_LANGPDF;
 $_LANGPDF = array();
-
-
 $_LANGPDF['PDF_invoicee0036b67b754e9f6442f9f9e6b200c33'] = 'Devolución nº ';
 $_LANGPDF['PDF_invoice77c3e447f60541a1c346a08134078c7f'] = 'Vale de compra nº ';
 $_LANGPDF['PDF_invoicecb5efbba6a6babef9082ca6976928ca7'] = 'Albarán nº ';
 $_LANGPDF['PDF_invoicefe4da50838bb10dcbf1bf8dcd9b12ab5'] = 'Factura nº ';
+$_LANGPDF['PDF_invoiceba160acb58001c904c57507da3689265'] = 'Pedido n°';
 $_LANGPDF['PDF_invoice8dae339c20973fe3fcdeb5f2e8e8f894'] = 'La versión electrónica de este vale de descuento está disponible en su cuenta. Para acceder a ella, identifíquese en';
 $_LANGPDF['PDF_invoice15b57bef332e7e0327d89fe93711a954'] = 'La versión electrónica de esta factura está disponible en su cuenta. Para acceder a ella, identifíquese en';
 $_LANGPDF['PDF_invoicef14e57cfbfd08615bbb665cb803f46b1'] = 'nuestra Web usando su dirección de correo electrónico y contraseña (que creó cuando realizó su primer pedido).';
@@ -33,25 +32,33 @@ $_LANGPDF['PDF_invoicec896fe3f3895e8ef54194eec601b8f3c'] = 'nos reservamos el de
 $_LANGPDF['PDF_invoiceb5a7adde1af5c87d7fd797b6245c2a39'] = 'Descripción';
 $_LANGPDF['PDF_invoice63d5049791d9d79d86e9a108b0a999ca'] = 'Referencia';
 $_LANGPDF['PDF_invoice03ab340b3f99e03cff9e84314ead38c0'] = 'Cant.';
+$_LANGPDF['PDF_invoice396124314c5fd1fa5bf463274896dc80'] = 'No se ha definido ningún método de precio para este grupo';
 $_LANGPDF['PDF_invoice065ab3a28ca4f16f55f103adc7d0226f'] = 'Entrega';
 $_LANGPDF['PDF_invoice6bdf2efe1a99072849d4e87baee37dbe'] = 'Facturación';
+$_LANGPDF['PDF_invoice60da3bd6867e3952ef0057a0772ea885'] = 'id del impuesto';
 $_LANGPDF['PDF_invoiced1228f5476d15142b1358ae4b5fa2454'] = 'Pedido nº ';
 $_LANGPDF['PDF_invoicef8617a92ba0a0a4eabee724eab7c9f48'] = 'Transportista:';
 $_LANGPDF['PDF_invoicec1f6368d15f7c13c4e5e8f70c68c747f'] = 'Modo de pago:';
 $_LANGPDF['PDF_invoicea3a79b28bc45b7fd393effd056b917e1'] = 'Fecha de envío:';
-$_LANGPDF['PDF_invoiceb76c1a1463e45cece931ef4467c0bdf8'] = 'Descuentos totales';
-$_LANGPDF['PDF_invoice7a5b34426b4672759796f035bbc2674e'] = 'Totzl embalaje';
+$_LANGPDF['PDF_invoice115dfa9ced2cf136ae677b9a389a5e60'] = 'paquete regalo';
+$_LANGPDF['PDF_invoice2370a4958c4ebe6d6c8045cd49a6b392'] = 'Total productos sin IVA';
+$_LANGPDF['PDF_invoiceebaee01719541f61281c16ba22ebbfde'] = 'Total productos IVA incluído';
+$_LANGPDF['PDF_invoiceb76c1a1463e45cece931ef4467c0bdf8'] = 'Total descuentos';
+$_LANGPDF['PDF_invoice7a5b34426b4672759796f035bbc2674e'] = 'Total embalaje';
 $_LANGPDF['PDF_invoicef4e8b53a114e5a17d051ab84d326cae5'] = 'Gastos de envío';
-$_LANGPDF['PDF_invoiceae5c2883df7651730799e1036984a6a3'] = 'Total IVA incluído';
+$_LANGPDF['PDF_invoice96b0141273eabab320119c467cdcaf17'] = 'Total';
 $_LANGPDF['PDF_invoice7d3548c5b974fa710296c1b3f5564753'] = 'Ecotasa';
 $_LANGPDF['PDF_invoiceafd42dfebce46ec93972b2e0cbb8b83f'] = 'Precio Un.';
-$_LANGPDF['PDF_invoicecd6576cc33506a6c44e2debbde5c14d9'] = 'Total sin IVA';
-$_LANGPDF['PDF_invoice96b0141273eabab320119c467cdcaf17'] = 'Total';
 $_LANGPDF['PDF_invoicec2808546f3e14d267d798f4e0e6f102e'] = 'Personalizado';
 $_LANGPDF['PDF_invoicea1e7379abfdbc3b8e03506e5489c6110'] = 'Descuento:';
 $_LANGPDF['PDF_invoice42c26bf38ece1dcd27aca8c6f8c076ed'] = 'Desglose IVA';
 $_LANGPDF['PDF_invoicea7fa76ae71664800f8994ad52581e3c2'] = '% IVA';
+$_LANGPDF['PDF_invoicecd6576cc33506a6c44e2debbde5c14d9'] = 'Total sin IVA';
 $_LANGPDF['PDF_invoiceb602e0d0c6a72053d0a5be60cb2f8126'] = 'Total IVA';
+$_LANGPDF['PDF_invoiceae5c2883df7651730799e1036984a6a3'] = 'Total IVA incluído';
 $_LANGPDF['PDF_invoice068f80c7519d0528fb08e82137a72131'] = 'Artículos';
 $_LANGPDF['PDF_invoice914419aa32f04011357d3b604a86d7eb'] = 'Transportista';
 $_LANGPDF['PDF_invoiceba794350deb07c0c96fe73bd12239059'] = 'Embalaje';
+$_LANGPDF['PDF_invoice6f3455d187a23443796efdcbe044096b'] = 'Sin impuestos';
+
+?>
