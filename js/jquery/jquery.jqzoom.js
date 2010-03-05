e51b0ebc5875ff9fa9f82fb5d4138c943c84e3f1
jVincent <jVincent@prestashop.com>
2010-03-05 10:06:36 +0000

    [*] FO : Optimized jQZoom js file

--- js/jquery/jquery.jqzoom.js
+++ js/jquery/jquery.jqzoom.js
@@ -9,111 +9,72 @@
 // mail: renzi.mrc@gmail.com
 //**************************************************************
 
-(function($){
-
-		$.fn.jqueryzoom = function(options){
-
-		var settings = {
-				xzoom: 200,		//zoomed width default width
-				yzoom: 200,		//zoomed div default width
-				offset: 10,		//zoomed div default offset
-				position: "right"  //zoomed div default position,offset position is to the right of the image
+(function($) {
+		$.fn.jqueryzoom = function(options) {
+			var settings = {
+				xzoom: 200,			//zoomed width default width
+				yzoom: 200,			//zoomed div default width
+				offset: 10,			//zoomed div default offset
+				position: "right"	//zoomed div default position,offset position is to the right of the image
 			};
 
-			if(options) {
+			if(options)
 				$.extend(settings, options);
-			}
 
-            var noalt ='';
+			var noalt ='';
 
-		$(this).hover(function(){
+			$(this).hover(function() {
+				var imageLeft = $(this).get(0).offsetLeft;
+				var imageTop =  $(this).get(0).offsetTop;
+				var imageWidth = $(this).get(0).offsetWidth;
+				var imageHeight = $(this).get(0).offsetHeight;
 
-           
-		    var imageLeft = $(this).get(0).offsetLeft;
-		    var imageRight = $(this).get(0).offsetRight;
-		    var imageTop =  $(this).get(0).offsetTop;
-		    var imageWidth = $(this).get(0).offsetWidth;
-		    var imageHeight = $(this).get(0).offsetHeight;
+				noalt = $(this).attr("alt");
+				var bigimage = noalt;
+				$(this).attr("alt", '');
 
-		    var bigimage = $(this).attr("alt");
-                noalt = $(this).attr("alt");
-                $(this).attr("alt",'');
-                
+				if($("div.zoomdiv").get().length == 0)
+					$(this).after("<div class='zoomdiv'><img class='bigimg' src='"+bigimage+"'/></div>");
 
-		    if($("div.zoomdiv").get().length == 0){
+				if(settings.position == "right")
+					leftpos = imageLeft + imageWidth + settings.offset;
+				else
+					leftpos = imageLeft - settings.xzoom - settings.offset;
 
-		    $(this).after("<div class='zoomdiv'><img class='bigimg' src='"+bigimage+"'/></div>");
+				$("div.zoomdiv").css({top: imageTop,left: leftpos});
+				$("div.zoomdiv").width(settings.xzoom);
+				$("div.zoomdiv").height(settings.yzoom);
+				$("div.zoomdiv").show();
 
-		    }
+				$(document.body).mousemove(function(e) {
+					var bigwidth = $(".bigimg").get(0).offsetWidth;
+					var bigheight = $(".bigimg").get(0).offsetHeight;
+					var scaley ='x';
+					var scalex= 'y';
 
-		    if(settings.position == "right"){
-
-		    leftpos = imageLeft + imageWidth + settings.offset;
-
-		    }else{
-
-		    leftpos = imageLeft - settings.xzoom - settings.offset;
-
-		    }
-
-		    $("div.zoomdiv").css({ top: imageTop,left: leftpos });
-
-		    $("div.zoomdiv").width(settings.xzoom);
-
-		    $("div.zoomdiv").height(settings.yzoom);
-
-		    $("div.zoomdiv").show();
-
-
-					$(document.body).mousemove(function(e){
-
-				    var bigwidth = $(".bigimg").get(0).offsetWidth;
-
-				    var bigheight = $(".bigimg").get(0).offsetHeight;
-
-				    var scaley ='x';
-
-				    var scalex= 'y';
-
-
-				    if(isNaN(scalex)|isNaN(scaley)){
-
-				    var scalex = Math.round(bigwidth/imageWidth) ;
-
-				    var scaley = Math.round(bigheight/imageHeight);
-
-				    }
+					if(isNaN(scalex)|isNaN(scaley)) {
+						var scalex = Math.round(bigwidth/imageWidth) ;
+						var scaley = Math.round(bigheight/imageHeight);
+					}
 
 					mouse = new MouseEvent(e);
 
-
-
 					scrolly = mouse.y - imageTop - ($("div.zoomdiv").height()*1/scaley)/2 ;
-
 					$("div.zoomdiv").get(0).scrollTop = scrolly * scaley  ;
-
-				    scrollx =    mouse.x - imageLeft - ($("div.zoomdiv").width()*1/scalex)/2 ;
-
+					scrollx = mouse.x - imageLeft - ($("div.zoomdiv").width()*1/scalex)/2 ;
 					$("div.zoomdiv").get(0).scrollLeft = (scrollx) * scalex ;
-
-
-				    });
-		    },function(){
-                   $(this).attr("alt",noalt);
-		       $("div.zoomdiv").hide();
-		       $(document.body).unbind("mousemove");
-		       $(".lenszoom").remove();
-		       $("div.zoomdiv").remove();
-		    });
-
+				});
+			}, function() {
+				$(this).attr("alt", noalt);
+				$("div.zoomdiv").hide();
+				$(document.body).unbind("mousemove");
+				$(".lenszoom").remove();
+				$("div.zoomdiv").remove();
+			});
 		}
-
 })(jQuery);
 
 function MouseEvent(e) {
-this.x = e.pageX
-this.y = e.pageY
+	this.x = e.pageX
+	this.y = e.pageY
 }
-
-
-
