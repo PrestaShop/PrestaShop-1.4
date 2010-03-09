08ff6771cacbd6c6a8c91c3066e29d23cfc311f5
aFolletete <alain.folletete@prestashop.com>
2010-03-09 08:37:21 +0000

    [-] TR : fixed english translation in BO

--- translations/en/admin.php
+++ translations/en/admin.php
@@ -9,6 +9,7 @@ $_LANGADM['AdminEmailsf1251a7f9b3741c1be21596cc52f30b0'] = 'Send a e-mail test a
 $_LANGADM['AdminEmails9b161d94627dcda71b2df1391107f00c'] = 'Send a e-mail test';
 $_LANGADM['AdminGenerator8f106af7bcea3cc7150abc9519345ef3'] = 'This tool will automatically generate a \"robots.txt\" file that will grant you the possibility to deny access to search engines for some pages.';
 $_LANGADM['AdminImport287234a1ff35a314b5b6bc4e5828e745'] = 'Combinations';
+$_LANGADM['AdminModulesPositionsc89a9e08172e8c615c6d908fd3d982b5'] = 'Display non-positionable hook';
 $_LANGADM['AdminStatuses0133c7a0fa55069df27f8d9be2c6ab89'] = 'Order statuses';
 $_LANGADM['AdminStatusesdc0d0ea31daf9b78138943e8fd06e3a3'] = 'Order return statuses';
 $_LANGADM['AdminTranslations228e51fac119b96085eae54fe69644f6'] = 'Error messages translations';
