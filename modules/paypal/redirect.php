ce966e584b000dc2208eb296935b5e73ac50da96
aFolletete <alain.folletete@prestashop.com>
2010-03-22 13:49:54 +0000

    // small change on PayPal module

--- modules/paypal/redirect.php
+++ modules/paypal/redirect.php
@@ -29,49 +29,25 @@ if ($currency_order->id != $currency_module->id)
 	$cart->update();
 }
 
-$amount = $cart->getOrderTotal(true, 4);
-$total = $cart->getOrderTotal(true, 3);
+$smarty->assign(array(
+	'redirect_text' => $paypal->getL('Please wait, redirecting to Paypal... Thanks.'),
+	'cancel_text' => $paypal->getL('Cancel'),
+	'cart_text' => $paypal->getL('My cart'),
+	'return_text' => $paypal->getL('Return to shop'),
+	'paypal_url' => $paypal->getPaypalUrl(),
+	'address' => $address,
+	'country' => $country,
+	'amount' => floatval($cart->getOrderTotal(true, 4)),
+	'customer' => $customer,
+	'total' => floatval($cart->getOrderTotal(true, 3)),
+	'business' => $business,
+	'currency_module' => $currency_module,
+	'cart_id' => intval($cart->id),
+	'paypal_id' => intval($paypal->id),
+	'header' => $header,
+	'url' => Tools::getHttpHost(false, true).__PS_BASE_URI__
+));
 
-echo '
-<html><head><script type="text/javascript" src="http://'.Tools::getHttpHost(false, true).__PS_BASE_URI__.'/js/jquery/jquery-1.2.6.pack.js"></script></head><body>
-<p>'.$paypal->getL('Please wait, redirecting to Paypal... Thanks.').'<br /><a href="javascript:history.go(-1);">'.$paypal->getL('Cancel').'</a></p>
-<form action="'.$paypal->getPaypalUrl().'" method="post" id="paypal_form" class="hidden">
-	<input type="hidden" name="upload" value="1" />
-	<input type="hidden" name="address_override" value="1" />
-	<input type="hidden" name="first_name" value="'.$address->firstname.'" />
-	<input type="hidden" name="last_name" value="'.$address->lastname.'" />
-	<input type="hidden" name="address1" value="'.$address->address1.'" />
-	'.($address->address2 ? '<input type="hidden" name="address2" value="'.$address->address2.'" />' : '').'
-	<input type="hidden" name="city" value="'.$address->city.'" />
-	<input type="hidden" name="zip" value="'.$address->postcode.'" />
-	<input type="hidden" name="country" value="'.$country->iso_code.'" />
-	<input type="hidden" name="amount" value="'.$amount.'" />
-	<input type="hidden" name="email" value="'.$customer->email.'" />
-	<input type="hidden" name="item_name_1" value="'.$paypal->getL('My cart').'" />
-	<input type="hidden" name="amount_1" value="'.$total.'" />
-	<input type="hidden" name="quantity_1" value="1" />
-	<input type="hidden" name="business" value="'.$business.'" />
-	<input type="hidden" name="receiver_email" value="'.$business.'" />
-	<input type="hidden" name="cmd" value="_cart" />
-	<input type="hidden" name="charset" value="utf-8" />
-	<input type="hidden" name="currency_code" value="'.$currency_module->iso_code.'" />
-	<input type="hidden" name="payer_id" value="'.$customer->id.'" />
-	<input type="hidden" name="payer_email" value="'.$customer->email.'" />
-	<input type="hidden" name="custom" value="'.$cart->id.'" />
-	<input type="hidden" name="return" value="'.Tools::getHttpHost(true, true).__PS_BASE_URI__.'order-confirmation.php?key='.$customer->secure_key.'&id_cart='.intval($cart->id).'&id_module='.intval($paypal->id).'" />
-	<input type="hidden" name="cancel_return" value="http://'.Tools::getHttpHost(false, true).__PS_BASE_URI__.'index.php" />
-	<input type="hidden" name="notify_url" value="http://'.Tools::getHttpHost(false, true).__PS_BASE_URI__.'modules/paypal/validation.php" />
-	'.($header ? '<input type="hidden" name="cpp_header_image" value="'.$header.'" />' : '').'
-    <input type="hidden" name="rm" value="2" />
-	<input type="hidden" name="bn" value="PRESTASHOP_WPS" />
-	<input type="hidden" name="cbt" value="'.$paypal->getL('Return to shop').'" />
-</form>
-<script type="text/javascript">
-$(document).ready(function() {
-	$(\'#paypal_form\').submit();
-});
-</script>
-</body></html>
-';
+echo $paypal->display(__FILE__, 'redirect.tpl');
 
 ?>
---
+++ modules/paypal/redirect.tpl
@@ -0,0 +1,50 @@
+<html>
+	<head>
+		<script type="text/javascript" src="http://{$url}js/jquery/jquery-1.2.6.pack.js"></script>
+	</head>
+	<body>
+		<p>{$redirect_text}<br /><a href="javascript:history.go(-1);">{$cancel_text}</a></p>
+		<form action="{$paypal_url}" method="post" id="paypal_form" class="hidden">
+			<input type="hidden" name="upload" value="1" />
+			<input type="hidden" name="address_override" value="1" />
+			<input type="hidden" name="first_name" value="{$address->firstname}" />
+			<input type="hidden" name="last_name" value="{$address->lastname}" />
+			<input type="hidden" name="address1" value="{$address->address1}" />
+			{if $address->address2 != NULL}
+			<input type="hidden" name="address2" value="{$address->address2}" />
+			{/if}
+			<input type="hidden" name="city" value="{$address->city}" />
+			<input type="hidden" name="zip" value="{$address->postcode}" />
+			<input type="hidden" name="country" value="{$country->iso_code}" />
+			<input type="hidden" name="amount" value="{$amount}" />
+			<input type="hidden" name="email" value="{$customer->email}" />
+			<input type="hidden" name="item_name_1" value="{$cart_text}" />
+			<input type="hidden" name="amount_1" value="{$total}" />
+			<input type="hidden" name="quantity_1" value="1" />
+			<input type="hidden" name="business" value="{$business}" />
+			<input type="hidden" name="receiver_email" value="{$business}" />
+			<input type="hidden" name="cmd" value="_cart" />
+			<input type="hidden" name="charset" value="utf-8" />
+			<input type="hidden" name="currency_code" value="{$currency_module->iso_code}" />
+			<input type="hidden" name="payer_id" value="{$customer->id}" />
+			<input type="hidden" name="payer_email" value="{$customer->email}" />
+			<input type="hidden" name="custom" value="{$cart_id}" />
+			<input type="hidden" name="return" value="http://{$url}order-confirmation.php?key={$customer->secure_key}&id_cart={$cart_id}&id_module={$paypal_id}" />
+			<input type="hidden" name="cancel_return" value="http://{$url}index.php" />
+			<input type="hidden" name="notify_url" value="http://{$url}modules/paypal/validation.php" />
+			{if $header != NULL}
+			<input type="hidden" name="cpp_header_image" value="{$header}" />
+			{/if}
+			<input type="hidden" name="rm" value="2" />
+			<input type="hidden" name="bn" value="PRESTASHOP_WPS" />
+			<input type="hidden" name="cbt" value="{$return_text}" />
+		</form>
+		<script type="text/javascript">
+		{literal}
+		$(document).ready(function() {
+			$('#paypal_form').submit();
+		});
+		{/literal}
+		</script>
+	</body>
+</html>
