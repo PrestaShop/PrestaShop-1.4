01b332403238c604bc18c80705f325d117b96383
aFolletete <alain.folletete@prestashop.com>
2010-03-24 10:23:08 +0000

    //[-] Classes : fixed bug on strlen method (#3937)

--- classes/Tools.php
+++ classes/Tools.php
@@ -787,7 +787,7 @@ class Tools
 	{
 		if (is_array($str))
 			return false;
-		$str = htmlspecialchars_decode($str);
+		$str = html_entity_decode($str, ENT_COMPAT, 'UTF-8');
 		if (function_exists('mb_strlen'))
 			return mb_strlen($str, 'utf-8');
 		return strlen($str);
