b2d969326d9ee432fad1626c8d2463d1d679b468
mBiart <mmathieu.biart@prestashop.com>
2010-03-25 09:38:16 +0000

    [-] FO : Downloadable product ordering fixed when stock management is OFF

--- classes/Product.php
+++ classes/Product.php
@@ -1606,7 +1606,7 @@ class		Product extends ObjectModel
 
 	public static function isAvailableWhenOutOfStock($oos)
 	{
-		return (intval($oos) == 2 ? intval(Configuration::get('PS_ORDER_OUT_OF_STOCK')) : intval($oos));
+		return !Configuration::get('PS_STOCK_MANAGEMENT') ? true : (intval($oos) == 2 ? intval(Configuration::get('PS_ORDER_OUT_OF_STOCK')) : intval($oos));
 	}
 
 	/**
