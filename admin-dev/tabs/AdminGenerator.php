7d8875ee7d07bc8866e39e655b49c771adfc6bc1
aFolletete <alain.folletete@prestashop.com>
2010-04-19 10:08:27 +0000

    [-] Project : fixed bug #4197 - remove contact-form.php from robots.txt

--- admin-dev/tabs/AdminGenerator.php
+++ admin-dev/tabs/AdminGenerator.php
@@ -193,7 +193,7 @@ class AdminGenerator extends AdminTab
 		$tab['Directories'] = array('classes/', 'config/', 'download/', 'mails/', 'modules/', 'translations/', 'tools/');
 
 		// Files
-		$tab['Files'] = array('addresses.php', 'address.php', 'authentication.php', 'cart.php', 'contact-form.php', 'discount.php', 'footer.php',
+		$tab['Files'] = array('addresses.php', 'address.php', 'authentication.php', 'cart.php', 'discount.php', 'footer.php',
 		'get-file.php', 'header.php', 'history.php', 'identity.php', 'images.inc.php', 'init.php', 'my-account.php', 'order.php',
 		'order-slip.php', 'order-detail.php', 'order-follow.php', 'order-return.php', 'order-confirmation.php', 'pagination.php', 'password.php',
 		'pdf-invoice.php', 'pdf-order-return.php', 'pdf-order-slip.php', 'product-sort.php', 'search.php', 'statistics.php');
