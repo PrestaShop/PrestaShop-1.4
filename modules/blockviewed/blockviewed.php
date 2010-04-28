23c57edeeb152e3c72e2a7bcb5367e3997d925c4
aFolletete <alain.folletete@prestashop.com>
2010-04-28 07:59:06 +0000

    [-] MO : now image cover of product is showed on block Product Viewed (#4289)

--- modules/blockviewed/blockviewed.php
+++ modules/blockviewed/blockviewed.php
@@ -74,14 +74,15 @@ class BlockViewed extends Module
 			$productIds = implode(',', $productsViewed);
 			$productsImages = Db::getInstance()->ExecuteS('
 			SELECT i.id_image, p.id_product, il.legend, p.active, pl.name, pl.description_short, pl.link_rewrite, cl.link_rewrite AS category_rewrite
-            FROM '._DB_PREFIX_.'product p
-            LEFT JOIN '._DB_PREFIX_.'product_lang pl ON (pl.id_product = p.id_product)
-            LEFT JOIN '._DB_PREFIX_.'image i ON (i.id_product = p.id_product)
-            LEFT JOIN '._DB_PREFIX_.'image_lang il ON (il.id_image = i.id_image)
-            LEFT JOIN '._DB_PREFIX_.'category_lang cl ON (cl.id_category = p.id_category_default)
-            WHERE p.id_product IN ('.$productIds.')
-            AND pl.id_lang = '.intval($params['cookie']->id_lang).'
-            AND cl.id_lang = '.intval($params['cookie']->id_lang)
+			FROM '._DB_PREFIX_.'product p
+			LEFT JOIN '._DB_PREFIX_.'product_lang pl ON (pl.id_product = p.id_product)
+			LEFT JOIN '._DB_PREFIX_.'image i ON (i.id_product = p.id_product)
+			LEFT JOIN '._DB_PREFIX_.'image_lang il ON (il.id_image = i.id_image)
+			LEFT JOIN '._DB_PREFIX_.'category_lang cl ON (cl.id_category = p.id_category_default)
+			WHERE p.id_product IN ('.$productIds.')
+			AND pl.id_lang = '.intval($params['cookie']->id_lang).'
+			AND cl.id_lang = '.intval($params['cookie']->id_lang).'
+			AND i.cover = 1'
 			);
 
 			$productsImagesArray = array();
