f150bb8889d5c947ae73248591e7e7cf1acef0c1
aFolletete <alain.folletete@prestashop.com>
2010-04-30 14:19:43 +0000

    [-] BO : fixed bug #4352 - BO doctype still lang="fr"

--- admin-dev/header.inc.php
+++ admin-dev/header.inc.php
@@ -22,7 +22,7 @@ require_once(dirname(__FILE__).'/init.php');
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $iso; ?>" lang="<?php echo $iso; ?>">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link type="text/css" rel="stylesheet" href="../js/jquery/datepicker/datepicker.css" />
