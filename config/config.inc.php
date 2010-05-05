cde87d0b5cdbf07f783b341f7933c54a5a86b40f
aFolletete <alain.folletete@prestashop.com>
2010-05-05 10:59:20 +0000

    // fixed PHP error

--- config/config.inc.php
+++ config/config.inc.php
@@ -36,12 +36,12 @@ if (!isset($_SERVER['REQUEST_URI']) OR empty($_SERVER['REQUEST_URI']))
 		$_SERVER['REQUEST_URI'] .= '?'.$_SERVER['QUERY_STRING'];
 }
 
+/* Include all defines */
+include(dirname(__FILE__).'/defines.inc.php');
+
 define('_PS_MAGIC_QUOTES_GPC_',         get_magic_quotes_gpc());
 define('_PS_MODULE_DIR_',           _PS_ROOT_DIR_.'/modules/');
 define('_PS_MYSQL_REAL_ESCAPE_STRING_', function_exists('mysql_real_escape_string'));
-
-/* Include all defines */
-include(dirname(__FILE__).'/defines.inc.php');
 
 /* aliases */
 function p($var) {
