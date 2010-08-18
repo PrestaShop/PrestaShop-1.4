d2da9cfb13da8ea564338ce54965dd737374bb30
dMetzger <damien.metzger@prestashop.com>
2010-08-18 16:07:14 +0000

    // cache-control improved

--- admin-dev/tabs/AdminGenerator.php
+++ admin-dev/tabs/AdminGenerator.php
@@ -126,6 +126,7 @@ class AdminGenerator extends AdminTab
 		ExpiresByType image/png \"access plus 1 month\"
 		ExpiresByType text/css \"access plus 1 week\"
 		ExpiresByType application/javascript \"access plus 1 week\"
+		ExpiresByType application/x-javascript \"access plus 1 week\"
 		ExpiresByType image/x-icon \"access plus 1 year\"
 </IfModule>
 ";
