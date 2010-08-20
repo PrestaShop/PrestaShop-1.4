953dcb75dee3cfd961fcb6e8a3765a77d879fcb1
aFolletete <alain.folletete@prestashop.com>
2010-08-20 08:53:15 +0000

    [-] BO : fixed bug #5255 - can’t enable/disable zone

--- admin-dev/tabs/AdminZones.php
+++ admin-dev/tabs/AdminZones.php
@@ -25,7 +25,8 @@ class AdminZones extends AdminTab
 				
 		$this->fieldsDisplay = array(
 		'id_zone' => array('title' => $this->l('ID'), 'align' => 'center', 'width' => 25),
-		'name' => array('title' => $this->l('Zone'), 'width' => 150)
+		'name' => array('title' => $this->l('Zone'), 'width' => 150),
+		'active' => array('title' => $this->l('Enabled'), 'align' => 'center', 'active' => 'status', 'type' => 'bool', 'orderby' => false)
 		);
 	
 		parent::__construct();
@@ -46,6 +47,14 @@ class AdminZones extends AdminTab
 					<input type="text" size="33" name="name" value="'.htmlentities(Tools::getValue('name', $obj->name), ENT_COMPAT, 'UTF-8').'" /> <sup>*</sup>
 					<span class="hint" name="help_box">'.$this->l('Allowed characters: letters, spaces and').' (-)<span class="hint-pointer">&nbsp;</span></span>
 					<p class="clear">'.$this->l('Zone name, e.g., Africa, West Coast, Neighboring Countries').'</p>
+				</div>
+				<label>'.$this->l('Status:').' </label>
+				<div class="margin-form">
+					<input type="radio" name="active" id="active_on" value="1" '.((!$obj->id OR Tools::getValue('active', $obj->active)) ? 'checked="checked" ' : '').'/>
+					<label class="t" for="active_on"> <img src="../img/admin/enabled.gif" alt="'.$this->l('Enabled').'" title="'.$this->l('Enabled').'" /></label>
+					<input type="radio" name="active" id="active_off" value="0" '.((!Tools::getValue('active', $obj->active) AND $obj->id) ? 'checked="checked" ' : '').'/>
+					<label class="t" for="active_off"> <img src="../img/admin/disabled.gif" alt="'.$this->l('Disabled').'" title="'.$this->l('Disabled').'" /></label>
+					<p>'.$this->l('Allow or disallow shipping to this zone').'</p>
 				</div>
 				<div class="margin-form">
 					<input type="submit" value="'.$this->l('   Save   ').'" name="submitAdd'.$this->table.'" class="button" />
