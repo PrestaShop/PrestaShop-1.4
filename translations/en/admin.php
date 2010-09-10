867d5dd52e02d033bbf99d8ceaa3bdac5d371365
fBrignoli <franck.brignoli@prestashop.com>
2010-09-10 14:39:31 +0000

    [-] BO : Bug fixed #5454 - Bad EN translations on product page

--- translations/en/admin.php
+++ translations/en/admin.php
@@ -10,8 +10,12 @@ $_LANGADM['AdminEmails9b161d94627dcda71b2df1391107f00c'] = 'Send a e-mail test';
 $_LANGADM['AdminGenerator8f106af7bcea3cc7150abc9519345ef3'] = 'This tool will automatically generate a \"robots.txt\" file that will grant you the possibility to deny access to search engines for some pages.';
 $_LANGADM['AdminImport287234a1ff35a314b5b6bc4e5828e745'] = 'Combinations';
 $_LANGADM['AdminModulesPositionsc89a9e08172e8c615c6d908fd3d982b5'] = 'Display non-positionable hook';
+$_LANGADM['AdminProducts0f317a01fd0c6c66853a02fa41b690ba'] = 'Click here to edit the unit prices';
+$_LANGADM['AdminProducts32e5f5aa47b9eb6c3d5e1756ea603242'] = 'Unit price without tax:';
+$_LANGADM['AdminProducts43b090faa8f69eaccffab8424f0eccd9'] = 'per unit';
+$_LANGADM['AdminProducts179848d41777c5c5bd13bff5411c1eda'] = 'Minimum quantity:';
+$_LANGADM['AdminProducts4c95c2e91f3ee35993bc3d9aacdaa501'] = 'The minimum quantity to buy this product (set to 1 to disable this feature)';
 $_LANGADM['AdminStatuses0133c7a0fa55069df27f8d9be2c6ab89'] = 'Order statuses';
 $_LANGADM['AdminStatusesdc0d0ea31daf9b78138943e8fd06e3a3'] = 'Order return statuses';
-$_LANGADM['AdminTranslations228e51fac119b96085eae54fe69644f6'] = 'Error messages translations';
 
 ?>
