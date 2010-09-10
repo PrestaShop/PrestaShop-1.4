b6e59d0d3b6c1bb0114d9e9627a7d1b2d63f4637
dMetzger <damien.metzger@prestashop.com>
2010-09-10 14:45:07 +0000

    // Confirmation page Ogone

--- modules/ogone/confirmation.php
+++ modules/ogone/confirmation.php
@@ -6,9 +6,9 @@ include(dirname(__FILE__).'/ogone.php');
 $ogone = new Ogone();
 
 $id_module = $ogone->id;
-$id_order = Tools::getValue('orderID');
-$key = Db::getInstance()->getValue('SELECT secure_key FROM '._DB_PREFIX_.'customer WHERE id_customer = '.(int)$cookie->id_lang);
+$id_cart = Tools::getValue('orderID');
+$key = Db::getInstance()->getValue('SELECT secure_key FROM '._DB_PREFIX_.'customer WHERE id_customer = '.(int)$cookie->id_customer);
 
-Tools::redirect('order-confirmation.php?id_order='.(int)$id_order.'&id_module='.(int)$id_module.'&key='.$key);
-	
+Tools::redirect('order-confirmation.php?id_cart='.(int)$id_cart.'&id_module='.(int)$id_module.'&key='.$key);
+
 ?>
