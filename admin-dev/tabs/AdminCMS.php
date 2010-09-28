ae5f30adde635739bc1bd5ad02a665ec8aae3d36
vAugagneur <vincent.augagneur@prestashop.com>
2010-09-28 14:36:24 +0000

    [-] BO : fixed upload picture tinymce

--- admin-dev/tabs/AdminCMS.php
+++ admin-dev/tabs/AdminCMS.php
@@ -156,29 +156,29 @@ class AdminCMS extends AdminTab
 						
 					});
 					function ajaxfilemanager(field_name, url, type, win) {
-			var ajaxfilemanagerurl = "'.__PS_BASE_URI__.'js/tinymce/jscripts/tiny_mce/plugins/ajaxfilemanager/ajaxfilemanager.php";
-			switch (type) {
-				case "image":
-					break;
-				case "media":
-					break;
-				case "flash": 
-					break;
-				case "file":
-					break;
-				default:
-					return false;
-			}
-            tinyMCE.activeEditor.windowManager.open({
-                url: "'.__PS_BASE_URI__.'js/tinymce/jscripts/tiny_mce/plugins/ajaxfilemanager/ajaxfilemanager.php",
-                width: 782,
-                height: 440,
-                inline : "yes",
-                close_previous : "no"
-            },{
-                window : win,
-                input : field_name
-            });
+						var ajaxfilemanagerurl = "'.__PS_BASE_URI__.'admin-dev/ajaxfilemanager/ajaxfilemanager.php";
+						switch (type) {
+							case "image":
+								break;
+							case "media":
+								break;
+							case "flash": 
+								break;
+							case "file":
+								break;
+							default:
+								return false;
+					}
+		            tinyMCE.activeEditor.windowManager.open({
+		                url: "'.__PS_BASE_URI__.'admin-dev/ajaxfilemanager/ajaxfilemanager.php",
+		                width: 782,
+		                height: 440,
+		                inline : "yes",
+		                close_previous : "no"
+		            },{
+		                window : win,
+		                input : field_name
+		            });
             
 		}
 	</script>';
