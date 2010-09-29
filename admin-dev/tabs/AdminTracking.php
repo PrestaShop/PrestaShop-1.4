a0690f46e33753a0dd261d351d5396ed40e6c75c
fBrignoli <franck.brignoli@prestashop.com>
2010-09-29 07:23:22 +0000

    [*] BO : Tracking displays out of stock attributes

--- admin-dev/tabs/AdminTracking.php
+++ admin-dev/tabs/AdminTracking.php
@@ -34,10 +34,14 @@ class AdminTracking extends AdminTab
 		$this->displayProducts();
 		$this->getObjects('products_nostock');
 		$this->displayProducts();
+		$this->getObjects('attributes_nostock');
+		$this->displayAttributes();		
 	}
 	
 	public function getObjects($type)
 	{
+		global $cookie;
+	
 		switch ($type)
 		{
 			case 'categories_empty':
@@ -71,14 +75,28 @@ class AdminTracking extends AdminTab
 						FROM `'._DB_PREFIX_.'product_attribute`
 					  )
 					  AND quantity <= 0
-					) OR id_product IN (
-					  SELECT pa.id_product
-					  FROM `'._DB_PREFIX_.'product_attribute` pa
-					  WHERE pa.quantity <= 0
 					)
 				';
-				$this->_list['message'] = $this->l('List of out of stock products:');
-				break ;
+				$this->_list['message'] = $this->l('List of out of stock products without attributes:');
+				break ;		
+			case 'attributes_nostock':
+				$sql = '
+				SELECT pa.*, ag.`id_attribute_group`, ag.`is_color_group`, agl.`name` AS group_name, al.`name` AS attribute_name, a.`id_attribute`,
+				m.`name` AS manufacturer_name, pl.`name` AS name, p.`weight` AS product_weight, p.`id_tax`, p.`active` AS active
+				FROM `'._DB_PREFIX_.'product_attribute` pa
+				LEFT JOIN `'._DB_PREFIX_.'product_attribute_combination` pac ON pac.`id_product_attribute` = pa.`id_product_attribute`
+				LEFT JOIN `'._DB_PREFIX_.'attribute` a ON a.`id_attribute` = pac.`id_attribute`
+				LEFT JOIN `'._DB_PREFIX_.'attribute_group` ag ON ag.`id_attribute_group` = a.`id_attribute_group`
+				LEFT JOIN `'._DB_PREFIX_.'attribute_lang` al ON (a.`id_attribute` = al.`id_attribute` AND al.`id_lang` = '.intval($cookie->id_lang).')
+				LEFT JOIN `'._DB_PREFIX_.'attribute_group_lang` agl ON (ag.`id_attribute_group` = agl.`id_attribute_group` AND agl.`id_lang` = '.intval($cookie->id_lang).')
+				LEFT JOIN `'._DB_PREFIX_.'product` p ON (p.`id_product` = pa.`id_product`)
+				LEFT JOIN `'._DB_PREFIX_.'product_lang` pl ON (pl.`id_product` = p.`id_product` AND pl.`id_lang`  = '.intval($cookie->id_lang).')
+				LEFT JOIN `'._DB_PREFIX_.'manufacturer` m ON (p.`id_manufacturer` = m.`id_manufacturer`)
+				WHERE pa.quantity <= 0
+				ORDER BY pa.`id_product_attribute`
+				';
+				$this->_list['message'] = $this->l('List of out of stock products with attributes:');
+				break ;					
 		}
 		$this->_list['obj'] = Db::getInstance()->ExecuteS($sql);
 	}
@@ -101,6 +119,7 @@ class AdminTracking extends AdminTab
 			echo '</table><br /><br />';
 		}
 	}
+	
 	
 	public function displayProducts()
 	{
@@ -137,6 +156,7 @@ class AdminTracking extends AdminTab
 				$product = new Product(intval($prod['id_product']));
 				$product->name = $product->name[intval($cookie->id_lang)];
 				$tax = new Tax(intval($product->id_tax));
+		
 				echo '
 				<tr>
 					<td>'.$product->id.'</td>
@@ -152,6 +172,81 @@ class AdminTracking extends AdminTab
 						<a href="index.php?tab=AdminCatalog&id_product='.$product->id.'&addproduct&token='.Tools::getAdminToken('AdminCatalog'.intval(Tab::getIdFromClassName('AdminCatalog')).intval($cookie->id_employee)).'">
 						<img src="../img/admin/edit.gif" alt="'.$this->l('Modify this product').'" /></a>&nbsp;
 						<a href="index.php?tab=AdminCatalog&id_product='.$product->id.'&deleteproduct&token='.Tools::getAdminToken('AdminCatalog'.intval(Tab::getIdFromClassName('AdminCatalog')).intval($cookie->id_employee)).'" onclick="return confirm(\''.addslashes($this->l('Do you want to delete').' '.$product->name).' ?\');">
+						<img src="../img/admin/delete.gif" alt="'.$this->l('Delete this product').'" /></a>
+					</td>
+				</tr>';
+			}
+			echo '</table><br /><br />';
+		}
+	}
+	
+	public function displayAttributes()
+	{
+		global $currentIndex, $cookie;
+
+		if (isset($this->_list['obj']))
+		{
+			$nbAttributes = sizeof($this->_list['obj']);
+			echo '<h3>'.$this->_list['message'].' '.$nbAttributes.' '.$this->l('found').'</h3>';
+			if (!$nbAttributes)
+				return ;
+			$this->fieldsDisplay = (array(
+					'ID' => array('title' => $this->l('ID')),
+					'manufacturer' => array('title' => $this->l('Manufacturer')),
+					'reference' => array('title' => $this->l('Reference')),
+					'name' => array('title' => $this->l('Name')),
+					'price' => array('title' => $this->l('Price')),
+					'tax' => array('title' => $this->l('Tax')),
+					'stock' => array('title' => $this->l('Stock')),
+					'weight' => array('title' => $this->l('Weight')),
+					'status' => array('title' => $this->l('Status')),
+					'action' => array('title' => $this->l('Actions'))
+				));
+				
+			$currency = new Currency(Configuration::get('PS_CURRENCY_DEFAULT'));
+			echo '
+			<table class="table" cellpadding="0" cellspacing="0">
+				<tr>';
+			foreach ($this->fieldsDisplay AS $field)
+				echo '<th'.(isset($field['width']) ? 'style="width: '.$field['width'].'"' : '').'>'.$field['title'].'</th>';
+			echo '
+				</tr>';
+			
+			$attributes = array();
+			$prevAttributeId = '';
+			foreach ($this->_list['obj'] AS $prod)
+			{
+				if ($prevAttributeId == $prod['id_product_attribute'])
+					$prod['combination_name'] = $attributes[$prod['id_product_attribute']]['combination_name'].', '.$prod['group_name'].' : '.$prod['attribute_name'];
+				else
+					$prod['combination_name'] = $prod['group_name'].' : '.$prod['attribute_name'];
+					
+				$attributes[$prod['id_product_attribute']] = $prod;
+				$prevAttributeId = $prod['id_product_attribute'];
+			}
+			
+			foreach ($attributes AS $prod)
+			{
+				/*
+				$product = new Product(intval($prod['id_product']));
+				$product->name = $product->name[intval($cookie->id_lang)];*/
+				$tax = new Tax(intval($prod['id_tax']));
+		
+				echo '
+				<tr>
+					<td>'.$prod['id_product'].'</td>
+					<td align="center">'.($prod['manufacturer_name'] != NULL ? stripslashes($prod['manufacturer_name']) : '--').'</td>
+					<td>'.$prod['reference'].'</td>
+					<td><a href="index.php?tab=AdminCatalog&id_product='.$prod['id_product'].'&addproduct&token='.Tools::getAdminToken('AdminCatalog'.intval(Tab::getIdFromClassName('AdminCatalog')).intval($cookie->id_employee)).'">'.stripslashes($prod['name']).' ('.$prod['combination_name'].')'.'</a></td>
+					<td>'.Tools::displayPrice(Product::getPriceStatic(intval($prod['id_product']), true, $prod['id_product_attribute']), $currency).'</td>
+					<td>'.stripslashes($tax->name[intval($cookie->id_lang)]).'</td>
+					<td align="center">'.$prod['quantity'].'</td>
+					<td align="center">'.($prod['weight'] + $prod['product_weight']).' '.Configuration::get('PS_WEIGHT_UNIT').'</td>
+					<td align="center"><a href="index.php?tab=AdminCatalog&id_product='.$prod['id_product'].'&status&token='.Tools::getAdminToken('AdminCatalog'.intval(Tab::getIdFromClassName('AdminCatalog')).intval($cookie->id_employee)).'"><img src="../img/admin/'.($prod['active'] ? 'enabled.gif' : 'disabled.gif').'" alt="" /></a></td>
+					<td>
+						<a href="index.php?tab=AdminCatalog&id_product='.$prod['id_product'].'&addproduct&token='.Tools::getAdminToken('AdminCatalog'.intval(Tab::getIdFromClassName('AdminCatalog')).intval($cookie->id_employee)).'">
+						<img src="../img/admin/edit.gif" alt="'.$this->l('Modify this product').'" /></a>&nbsp;
+						<a href="index.php?tab=AdminCatalog&id_product='.$prod['id_product'].'&deleteproduct&token='.Tools::getAdminToken('AdminCatalog'.intval(Tab::getIdFromClassName('AdminCatalog')).intval($cookie->id_employee)).'" onclick="return confirm(\''.addslashes($this->l('Do you want to delete').' '.$prod['name']).' ?\');">
 						<img src="../img/admin/delete.gif" alt="'.$this->l('Delete this product').'" /></a>
 					</td>
 				</tr>';
