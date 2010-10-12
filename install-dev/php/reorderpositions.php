c73b51e6b5e21d9c4ecd525ae9d74042f5d35e7c
vAugagneur <vincent.augagneur@prestashop.com>
2010-10-12 12:30:16 +0000

    [+] BO : Fix update script to change category name without number position

--- install-dev/php/reorderpositions.php
+++ install-dev/php/reorderpositions.php
@@ -11,36 +11,34 @@ function reorderpositions()
 	}
 	
 	//clean Category position and delete old position system
+	Language::loadLanguages();
 	$language = Language::getLanguages();
 	$cat_parent = Db::getInstance()->ExecuteS('SELECT DISTINCT c.id_parent FROM `'._DB_PREFIX_.'category` c WHERE id_category != 1');
-	
 	foreach($cat_parent as $parent)
 	{
 		$result = Db::getInstance()->ExecuteS('
 							SELECT DISTINCT c.*, cl.*
 							FROM `'._DB_PREFIX_.'category` c 
-							LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category` AND `id_lang` = '.Configuration::get('PS_LANG_DEFAULT').')
-							WHERE c.id_parent = '.$parent['id_parent'].'
+							LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category` AND `id_lang` = '.intval(Configuration::get('PS_LANG_DEFAULT')).')
+							WHERE c.id_parent = '.intval($parent['id_parent']).'
 							ORDER BY name ASC');
 		foreach($result as $i => $categ)
 		{
-			$sizeof = sizeof($result);
-				for ($i = 0; $i < $sizeof; ++$i){
-						$sql = '
-						UPDATE `'._DB_PREFIX_.'category`
-						SET `position` = '.intval($i).'
-						WHERE `id_parent` = '.intval($categ['id_parent']).'
-						AND `id_category` = '.intval($result[$i]['id_category']);
-						Db::getInstance()->Execute($sql);
-						p($categ['name']);
-					}
+		$sizeof = sizeof($result);
+			for ($i = 0; $i < $sizeof; ++$i)
+			{
+				Db::getInstance()->Execute('
+				UPDATE `'._DB_PREFIX_.'category`
+				SET `position` = '.intval($i).'
+				WHERE `id_parent` = '.intval($categ['id_parent']).'
+				AND `id_category` = '.intval($result[$i]['id_category']));
+			}
 		
-		foreach($language as $lang)
-			Db::getInstance()->Execute('UPDATE `'._DB_PREFIX_.'category` c 
-			LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category`)  
-			SET `name` = \''.Category::hideCategoryPosition($categ['name']).'\' 
-			WHERE c.id_category = '.$categ['id_category'].' AND id_lang = \''.$lang['id_lang'].'\'');
-		
+			foreach($language as $lang)
+				Db::getInstance()->Execute('UPDATE `'._DB_PREFIX_.'category` c 
+				LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category`)  
+				SET `name` = \''.Category::hideCategoryPosition($categ['name']).'\' 
+				WHERE c.id_category = '.intval($categ['id_category']).' AND id_lang = \''.intval($lang['id_lang']).'\'');
 		}
 	}
 	
@@ -49,27 +47,9 @@ function reorderpositions()
 	
 	foreach($cms_cat as $i => $categ)
 	{
-		CMS::cleanPositions(intval($categ['id_category']));
+		CMS::cleanPositions(intval($categ['id_cms_category']));
 	}
-	
-	//clean CMS Category position
-	$cat = CMSCategory::getCategories(1, false, false);
-	
-	foreach($cat as $i => $categ)
-	{
-		CMSCategory::cleanPositions(intval($categ['id_parent']));
-	}
+
 }
 
-
-
-
-function cleanCategoryPosition()
-{
-	$result = Db::getInstance()->ExecuteS('
-						SELECT DISTINCT c.*, cl.*
-						FROM `'._DB_PREFIX_.'category` c 
-						LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category` AND `id_lang` = '.intval($params['cookie']->id_lang).')
-						ORDER BY `level_depth` ASC, c.`position` ASC')
-}
 ?>
