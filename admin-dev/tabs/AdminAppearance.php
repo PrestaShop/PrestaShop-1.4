82ae6a4e45ee70d8f1c2b68f92834743330286e4
rChambily <rChambily@prestashop.com>
2010-11-10 09:13:56 +0000

    // Version 1.3 in headers changed to Version 1.4

--- admin-dev/tabs/AdminAppearance.php
+++ admin-dev/tabs/AdminAppearance.php
@@ -1,6 +1,15 @@
 <?php
 
-include_once(dirname(__FILE__).'/AdminThemes.php');
+/**
+  * Database tab for admin panel, AdminDb.php
+  * @category admin
+  *
+  * @author PrestaShop <support@prestashop.com>
+  * @copyright PrestaShop
+  * @license http://www.opensource.org/licenses/osl-3.0.php Open-source licence 3.0
+  * @version 1.4
+  *
+  */
 
 class AdminAppearance extends AdminThemes // extends AdminThemes only for retro-compatibility
 {
