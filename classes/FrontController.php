e808fe3eb6c97c0ec536fd80c9bc2ec363990631
rChambily <rChambily@prestashop.com>
2010-11-12 14:25:52 +0000

    [-] FO : ASC => asc for google referencement compatibility. Bug #5850 fixed

--- classes/FrontController.php
+++ classes/FrontController.php
@@ -371,7 +371,7 @@ class FrontControllerCore
 	{
 		$stock_management = intval(Configuration::get('PS_STOCK_MANAGEMENT')) ? true : false; // no display quantity order if stock management disabled
 		$orderByValues = array(0 => 'name', 1 => 'price', 2 => 'date_add', 3 => 'date_upd', 4 => 'position', 5 => 'manufacturer_name', 6 => 'quantity');
-		$orderWayValues = array(0 => 'ASC', 1 => 'DESC');
+		$orderWayValues = array(0 => 'asc', 1 => 'desc');
 		$this->orderBy = Tools::strtolower(Tools::getValue('orderby', $orderByValues[intval(Configuration::get('PS_PRODUCTS_ORDER_BY'))]));
 		$this->orderWay = Tools::strtoupper(Tools::getValue('orderway', $orderWayValues[intval(Configuration::get('PS_PRODUCTS_ORDER_WAY'))]));
 		if (!in_array($this->orderBy, $orderByValues))
