b9e47f898be52ae95bc95961eed0805746d375c2
dMetzger <damien.metzger@prestashop.com>
2010-11-17 17:01:17 +0000

    // updater sql bug

--- install-dev/sql/upgrade/1.4.0.3.sql
+++ install-dev/sql/upgrade/1.4.0.3.sql
@@ -5,7 +5,7 @@ DELETE FROM `PREFIX_tab_lang` WHERE `id_tab` NOT IN (SELECT id_tab FROM `PREFIX_
 DELETE FROM `PREFIX_access` WHERE `id_tab` NOT IN (SELECT id_tab FROM `PREFIX_tab`);
 
 INSERT INTO `PREFIX_module` (`name`, `active`) VALUES ('statsforecast', 1);
-INSERT INTO `PREFIX_hook_module` (`id_module`, `id_hook`) (SELECT id_module, 32 FROM `PREFIX_module` WHERE `class_name` = 'statsforecast');
+INSERT INTO `PREFIX_hook_module` (`id_module`, `id_hook`) (SELECT id_module, 32 FROM `PREFIX_module` WHERE `name` = 'statsforecast');
 
 ALTER TABLE `PREFIX_orders` ADD `conversion_rate` decimal(13,6) NOT NULL default 1 AFTER `payment`;
 UPDATE `PREFIX_orders` o SET o.`conversion_rate` = (
