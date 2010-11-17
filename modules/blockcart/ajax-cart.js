d9784b9410d9d9fe5994bc660f411a77d8a92e9e
vAugagneur <vincent.augagneur@prestashop.com>
2010-11-17 16:39:44 +0000

    [-] MO : fixed bug in blockcart

--- modules/blockcart/ajax-cart.js
+++ modules/blockcart/ajax-cart.js
@@ -207,7 +207,10 @@ var ajaxCart = {
 			cache: false,
 			dataType : "json",
 			data: 'delete=1&id_product=' + idProduct + '&ipa=' + ((idCombination != null && parseInt(idCombination)) ? idCombination : '') + ((customizationId && customizationId != null) ? '&id_customization=' + customizationId : '') + '&token=' + static_token + '&ajax=true',
-			success: function(jsonData)	{ ajaxCart.updateCart(jsonData) },
+			success: function(jsonData)	{ ajaxCart.updateCart(jsonData) 
+				if ($('body').attr('id') == 'order')
+					deletProductFromSummary(idProduct+'_'+idCombination);
+			},
 			error: function() {alert('ERROR: unable to delete the product');}
 		});
 	},
@@ -315,6 +318,7 @@ var ajaxCart = {
 
 	//refresh display of vouchers (needed for vouchers in % of the total)
 	refreshVouchers : function (jsonData) {
+	console.log(jsonData);
 		$(jsonData.discounts).each(function(){
 			//fix ie6 bug (one more item 'undefined' in IE6)
 			if (this.id != undefined && $('#bloc_cart_voucher_' + this.id).length == 1)
@@ -485,20 +489,23 @@ var ajaxCart = {
 					errors += jsonData.errors[error] + "\n";
 			alert(errors);
 		}
-		ajaxCart.expand();
-		ajaxCart.updateCartEverywhere(jsonData);
-		ajaxCart.hideOldProducts(jsonData);
-		ajaxCart.displayNewProducts(jsonData);
-		ajaxCart.refreshVouchers(jsonData);
-		
-		//update 'first' and 'last' item classes
-		$('#cart_block dl.products dt').removeClass('first_item').removeClass('last_item').removeClass('item');
-		$('#cart_block dl.products dt:first').addClass('first_item');
-		$('#cart_block dl.products dt:not(:first,:last)').addClass('item');
-		$('#cart_block dl.products dt:last').addClass('last_item');
-		
-		//reset the onlick events in relation to the cart block (it allow to bind the onclick event to the new 'delete' buttons added)
-		ajaxCart.overrideButtonsInThePage();
+		else
+		{
+			ajaxCart.expand();
+			ajaxCart.updateCartEverywhere(jsonData);
+			ajaxCart.hideOldProducts(jsonData);
+			ajaxCart.displayNewProducts(jsonData);
+			ajaxCart.refreshVouchers(jsonData);
+			
+			//update 'first' and 'last' item classes
+			$('#cart_block dl.products dt').removeClass('first_item').removeClass('last_item').removeClass('item');
+			$('#cart_block dl.products dt:first').addClass('first_item');
+			$('#cart_block dl.products dt:not(:first,:last)').addClass('item');
+			$('#cart_block dl.products dt:last').addClass('last_item');
+			
+			//reset the onlick events in relation to the cart block (it allow to bind the onclick event to the new 'delete' buttons added)
+			ajaxCart.overrideButtonsInThePage();
+		}
 	},
 	
 	//update general cart informations everywere in the page
