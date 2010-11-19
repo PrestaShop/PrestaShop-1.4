de0745e45964e5fcc3787c785d237f7360068aef
aFolletete <alain.folletete@prestashop.com>
2010-11-19 10:15:23 +0000

    // clean comments

--- admin-dev/tabs/AdminStats.php
+++ admin-dev/tabs/AdminStats.php
@@ -4,10 +4,11 @@
   * Statistics
   * @category stats
   *
-  * @author Damien Metzger / Epitech
-  * @copyright Epitech / PrestaShop
+  * @author PrestaShop <support@prestashop.com>
+  * @copyright PrestaShop
   * @license http://www.opensource.org/licenses/osl-3.0.php Open-source licence 3.0
-  * @version 1.2
+  * @version 1.4
+  *
   */
 
 include_once(dirname(__FILE__).'/AdminStatsTab.php');
