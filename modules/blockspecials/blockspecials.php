894c2923087374b24360b59545d52565d5e06561
nPellicari <nans.pellicari@prestashop.com>
2010-11-24 09:30:41 +0000

    [-] MO : #6124 In BlockSpecials::hookRightColumn() add dates params into Product::getRandomSpecial() method.

--- modules/blockspecials/blockspecials.php
+++ modules/blockspecials/blockspecials.php
@@ -55,7 +55,8 @@ class BlockSpecials extends Module
 	public function hookRightColumn($params)
 	{
 		global $smarty;
-		if (!$special = Product::getRandomSpecial(intval($params['cookie']->id_lang)) AND !Configuration::get('PS_BLOCK_SPECIALS_DISPLAY'))
+		$date_today = date('Y-m-d H:i:s', time());
+		if (!$special = Product::getRandomSpecial(intval($params['cookie']->id_lang),$date_today,$date_today) AND !Configuration::get('PS_BLOCK_SPECIALS_DISPLAY'))
 			return;
 		$smarty->assign(array('special' => $special,
 													'priceWithoutReduction_tax_excl' => Tools::ps_round($special['price_without_reduction'] / (1 + $special['rate'] / 100), 2),
