a1fa5d5b3f9c8e2c5c0dacbc43e04ff5d6180cce
dJorge <dimitri.jorge@prestashop.com>
2010-12-08 10:10:54 +0000

    // Added copyright header in php files

--- admin-dev/tabs/AdminBackup.php
+++ admin-dev/tabs/AdminBackup.php
@@ -1,20 +1,30 @@
 <?php
+/*
+* Copyright (C) 2007-2010 PrestaShop 
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License (OSL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/osl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author Prestashop SA <contact@prestashop.com>
+*  @copyright  Copyright (c) 2007-2010 Prestashop SA : 6 rue lacepede, 75005 PARIS
+*  @version  Release: $Revision: 1.4 $
+*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*  International Registred Trademark & Property of PrestaShop SA
+*/
 
-/**
-  * Backup management tab for admin panel, AdminBackup.php
-  * @category admin
-  *
-  * @author Andrew Brampton
-  * @copyright Andrew Brampton
-  * @license http://www.opensource.org/licenses/osl-3.0.php Open-source licence 3.0
-  * @version 0.1
-  *
-  * @todo When exporting data, ExecuteS is used which returns a large array. I would prefer Db supported a way to retreive one row at a time
-  * @todo Abstract some of the methods into the Db class, for example, listing table names, exporting schema
-  * @todo Add a import backup option
-  * @todo Make the filters work
-  */
-  
 include_once(PS_ADMIN_DIR.'/../classes/AdminTab.php');
 
 class AdminBackup extends AdminTab
--- classes/Module.php
+++ classes/Module.php
@@ -1,16 +1,29 @@
 <?php
-
-/**
-  * Module class, Module.php
-  * Modules management
-  * @category classes
-  *
-  * @author PrestaShop <support@prestashop.com>
-  * @copyright PrestaShop
-  * @license http://www.opensource.org/licenses/osl-3.0.php Open-source licence 3.0
-  * @version 1.4
-  *
-  */
+/*
+* Copyright (C) 2007-2010 PrestaShop 
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License (OSL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/osl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author Prestashop SA <contact@prestashop.com>
+*  @copyright  Copyright (c) 2007-2010 Prestashop SA : 6 rue lacepede, 75005 PARIS
+*  @version  Release: $Revision: 1.4 $
+*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*  International Registred Trademark & Property of PrestaShop SA
+*/
 
 abstract class ModuleCore
 {
