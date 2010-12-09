9c06153cabd62542f3de44759de70cdab1041746
dMetzger <damien.metzger@prestashop.com>
2010-12-09 15:53:32 +0000

    [-] BO : overflow hidden on the shop name, for long, long, long, long, long shop name

--- install-dev/sql/upgrade/1.4.0.5.sql
+++ install-dev/sql/upgrade/1.4.0.5.sql
@@ -4,3 +4,5 @@ SET NAMES 'utf8';
 UPDATE PREFIX_tab SET id_parent = 8 WHERE id_tab = @alias LIMIT 1;
 @stores = (SELECT id_tab FROM PREFIX_tab WHERE class_name = "AdminStores" LIMIT 1);
 UPDATE PREFIX_tab SET id_parent = 9 WHERE id_tab = @stores LIMIT 1;
+@pdf = (SELECT id_tab FROM PREFIX_tab WHERE class_name = "AdminPDF" LIMIT 1);
+UPDATE PREFIX_tab SET id_parent = 3 WHERE id_tab = @pdf LIMIT 1;
