af6ed81318e9f349ad39e1b7233e1e266220ab80
dMetzger <damien.metzger@prestashop.com>
2010-12-10 17:34:13 +0000

    [-] MO : from unsigned to signed for the ip address stored by fianetfraud

--- modules/fianetfraud/install.sql
+++ modules/fianetfraud/install.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `PREFIX_fianet_fraud` (
 		`id_cart` int(11) unsigned NOT NULL,
-		`ip_address` int(11) unsigned NOT NULL,
+		`ip_address` int(11) NOT NULL,
 		`date` datetime NOT NULL,
 			KEY `id_cart_index` (`id_cart`),
 			KEY `ip_address_index` (`ip_address`)
