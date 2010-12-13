1298a694442afaf7504226af03ae06386d9420cb
aFolletete <alain.folletete@prestashop.com>
2010-12-13 12:45:55 +0000

    [-] SQL : fixed bug #6404 - Sql Error on prestashop upgrading process

--- install-dev/sql/upgrade/1.4.0.5.sql
+++ install-dev/sql/upgrade/1.4.0.5.sql
@@ -1,13 +1,13 @@
 SET NAMES 'utf8';
 
-@alias = (SELECT id_tab FROM PREFIX_tab WHERE class_name = "AdminAlias" LIMIT 1);
-UPDATE PREFIX_tab SET id_parent = 8 WHERE id_tab = @alias LIMIT 1;
-@stores = (SELECT id_tab FROM PREFIX_tab WHERE class_name = "AdminStores" LIMIT 1);
-UPDATE PREFIX_tab SET id_parent = 9 WHERE id_tab = @stores LIMIT 1;
-@pdf = (SELECT id_tab FROM PREFIX_tab WHERE class_name = "AdminPDF" LIMIT 1);
-UPDATE PREFIX_tab SET id_parent = 3 WHERE id_tab = @pdf LIMIT 1;
-@tabs = (SELECT id_tab FROM PREFIX_tab WHERE class_name = "AdminTabs" LIMIT 1);
-UPDATE PREFIX_tab SET id_parent = 29 WHERE id_tab = @tabs LIMIT 1;
+SET @alias = (SELECT IFNULL((SELECT `id_tab` FROM `PREFIX_tab` WHERE `class_name` = "AdminAlias" LIMIT 1), '0'));
+UPDATE `PREFIX_tab` SET `id_parent` = 8 WHERE `id_tab` = @alias LIMIT 1;
+SET @stores = (SELECT IFNULL((SELECT `id_tab` FROM `PREFIX_tab` WHERE `class_name` = "AdminStores" LIMIT 1), '0'));
+UPDATE `PREFIX_tab` SET `id_parent` = 9 WHERE `id_tab` = @stores LIMIT 1;
+SET @pdf = (SELECT IFNULL((SELECT `id_tab` FROM `PREFIX_tab` WHERE `class_name` = "AdminPDF" LIMIT 1), '0'));
+UPDATE `PREFIX_tab` SET `id_parent` = 3 WHERE `id_tab` = @pdf LIMIT 1;
+SET @tabs = (SELECT IFNULL((SELECT `id_tab` FROM `PREFIX_tab` WHERE `class_name` = "AdminTabs" LIMIT 1), '0'));
+UPDATE `PREFIX_tab` SET `id_parent` = 29 WHERE `id_tab` = @tabs LIMIT 1;
 
 ALTER TABLE `PREFIX_image_type` ADD `stores` tinyint(1) NOT NULL default '1';
 
@@ -19,7 +19,7 @@ ADD `width` FLOAT NOT NULL AFTER `location`,
 ADD `height` FLOAT NOT NULL AFTER `width`,
 ADD `depth` FLOAT NOT NULL AFTER `height`;
 
-@id_module = (SELECT `id_module` FROM `PREFIX_module` WHERE `name` = "statshome" LIMIT 1);
+SET @id_module = (SELECT IFNULL((SELECT `id_module` FROM `PREFIX_module` WHERE `name` = "statshome" LIMIT 1), '0'));
 DELETE FROM `PREFIX_module` WHERE `id_module` = @id_module;
 DELETE FROM `PREFIX_hook_module` WHERE `id_module` = @id_module;
 
