e43daa46ea040ce58d8614ad3d1a7de7f2d0d1dc
vAugagneur <vincent.augagneur@prestashop.com>
2010-12-22 17:52:53 +0000

    [+] TR : added translation

---
+++ modules/blocklayered/fr.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{blocklayered}prestashop>blocklayered_84241e458cdd5162745500a59a3680f3'] = 'Navigation à facette';
+$_MODULE['<{blocklayered}prestashop>blocklayered_d7df62361bd02e736cbf22913f2f507e'] = 'Affiche un bloc avec des filtres de navigation';
+$_MODULE['<{blocklayered}prestashop>blocklayered_c32516babc5b6c47eb8ce1bfc223253c'] = 'Catalogue';
+$_MODULE['<{blocklayered}prestashop>blocklayered_b8f78b1a8b9e8a50b2426e028c702db2'] = 'Achetez par categories';
---
+++ modules/importerOsc/fr.php
@@ -0,0 +1,7 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{importerOsc}prestashop>importerOsc_06d9bacf6c5590c1f854448b544e43fc'] = 'OsCommerce Importateur';
+$_MODULE['<{importerOsc}prestashop>importerOsc_ebeda04e800e975ddc45bec90e05cc9a'] = 'Ce module permet d\'importer à partir d\'OsCommerce vers prestashop';
+$_MODULE['<{importerOsc}prestashop>importerOsc_5c503a150740945c58605bf9a2b613fe'] = 'Langue par default OsCommerce';
