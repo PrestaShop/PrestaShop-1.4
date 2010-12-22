eb9b4f9bf40e5793e6e657c7ea06268f4d290565
nPellicari <nans.pellicari@prestashop.com>
2010-12-22 18:12:51 +0000

    [+] MO : set a valid category module

--- modules/twenga/twenga.php
+++ modules/twenga/twenga.php
@@ -89,7 +89,7 @@ class Twenga extends PaymentModule
 	    $this->current_index = $currentIndex;
 	    $this->token = Tools::getValue('token');
 	 	$this->name = 'twenga';
-	 	$this->tab = 'Exports';
+	 	$this->tab = 'smart_shopping';
 	 	$this->version = '1.0';
 		
 	 	parent::__construct();
