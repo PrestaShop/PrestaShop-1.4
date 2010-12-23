894c874692983f5c29dc331e244c6cd01369f6de
rGaillard <remi.gaillard@prestashop.com>
2010-12-23 14:30:06 +0000

    [-] FO: Fixed #6534

--- config/autoload.php
+++ config/autoload.php
@@ -31,6 +31,8 @@ function __autoload($className)
 	{
 		if (function_exists('smartyAutoload') AND smartyAutoload($className)) 
 			return true;
+		if (!file_exists(dirname(__FILE__).'/../classes/'.$className.'.php'))
+			return;	
 		require_once(dirname(__FILE__).'/../classes/'.str_replace(chr(0), '', $className).'.php');
 		if (file_exists(dirname(__FILE__).'/../override/classes/'.$className.'.php'))
 			require_once(dirname(__FILE__).'/../override/classes/'.$className.'.php');
