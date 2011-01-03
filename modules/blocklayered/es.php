bc61d5645a9fe60a5f0c3eedce132d10865de936
aFolletete <alain.folletete@prestashop.com>
2011-01-03 14:51:27 +0000

    // adding missing spannish trnaslations

---
+++ modules/blocklayered/es.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{blocklayered}prestashop>blocklayered_84241e458cdd5162745500a59a3680f3'] = 'Bloque navegación por facetas';
+$_MODULE['<{blocklayered}prestashop>blocklayered_d7df62361bd02e736cbf22913f2f507e'] = 'Muestra un bloque con los filtros de la navegación por facetas';
+$_MODULE['<{blocklayered}prestashop>blocklayered_c32516babc5b6c47eb8ce1bfc223253c'] = 'Catálogo';
+$_MODULE['<{blocklayered}prestashop>blocklayered_b8f78b1a8b9e8a50b2426e028c702db2'] = 'Tienda por categorÍa';
