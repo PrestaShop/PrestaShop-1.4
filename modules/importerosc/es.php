01884be39137c373b2d44cf97b08b0e4e78338ad
vAugagneur <vincent.augagneur@prestashop.com>
2011-01-25 12:43:55 +0000

    //

--- modules/importerosc/es.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{importerOsc}prestashop>importerOsc_e273168c3697c35b8c737d14b1a5fb26'] = 'OsCommerce Importador';
-$_MODULE['<{importerOsc}prestashop>importerOsc_6042ff2da3ae0b5b78e5cc36ede31eb8'] = 'Este módulo permite importar a partir de OsCommerce hacia PrestaShop';
-$_MODULE['<{importerOsc}prestashop>importerOsc_2737b271f276b90d8a04ea4b9d18b920'] = 'Idioma por defecto OsCommerce';
--- modules/importerosc/fr.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{importerOsc}prestashop>importerOsc_e273168c3697c35b8c737d14b1a5fb26'] = 'Import depuis OsCommerce';
-$_MODULE['<{importerOsc}prestashop>importerOsc_6042ff2da3ae0b5b78e5cc36ede31eb8'] = 'Ce module permet d\'importer des données d\'OsCommerce vers PrestaShop';
-$_MODULE['<{importerOsc}prestashop>importerOsc_2737b271f276b90d8a04ea4b9d18b920'] = 'Langue par défault OsCommerce';
-$_MODULE['<{importerOsc}prestashop>importerOsc_1b473540f20c3e31ead53f84511d6c77'] = 'Devise OsCommerce par défaut';
-$_MODULE['<{importerOsc}prestashop>importerOsc_a5fd50447be7a08a8f1a59911339b90b'] = 'Groupe OsCommerce par défaut';
