00ee41414ce9ca9da9d0efd812b5bf98115e591e
vAugagneur <vincent.augagneur@prestashop.com>
2011-01-25 13:21:23 +0000

    //added translation

--- modules/importerosc/fr.php
+++ modules/importerosc/fr.php
@@ -2,8 +2,8 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{importerosc}prestashop>importerOsc_e273168c3697c35b8c737d14b1a5fb26'] = 'Import OsCommerce';
-$_MODULE['<{importerosc}prestashop>importerOsc_6042ff2da3ae0b5b78e5cc36ede31eb8'] = 'Ce module vous permet d\'importer à partir de votre boutique osCommerce vers Prestashop';
-$_MODULE['<{importerosc}prestashop>importerOsc_2737b271f276b90d8a04ea4b9d18b920'] = 'Language par défaut OsCommerce';
-$_MODULE['<{importerosc}prestashop>importerOsc_1b473540f20c3e31ead53f84511d6c77'] = 'Device par défaut OsCommerce';
-$_MODULE['<{importerosc}prestashop>importerOsc_a5fd50447be7a08a8f1a59911339b90b'] = 'Groupe par défaut OsCommerce';
+$_MODULE['<{importerosc}prestashop>importerosc_e273168c3697c35b8c737d14b1a5fb26'] = 'Import OsCommerce';
+$_MODULE['<{importerosc}prestashop>importerosc_6042ff2da3ae0b5b78e5cc36ede31eb8'] = 'Ce module permet d\'importer votre boutique OsCommerce dans prestashop';
+$_MODULE['<{importerosc}prestashop>importerosc_2737b271f276b90d8a04ea4b9d18b920'] = 'Langue par défaut OsCommerce';
+$_MODULE['<{importerosc}prestashop>importerosc_1b473540f20c3e31ead53f84511d6c77'] = 'Devise par défaut OsCommerce';
+$_MODULE['<{importerosc}prestashop>importerosc_a5fd50447be7a08a8f1a59911339b90b'] = 'Group par défaut OsCommerce';
