347611d6c8861a0270210b04572b9bf8e324c862
aFolletete <alain.folletete@prestashop.com>
2011-02-07 17:47:38 +0000

    [+] MO : adding new native module: Criteo

---
+++ modules/criteo/fr.php
@@ -0,0 +1,13 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{criteo}prestashop>criteo_0c90fc1c188056486dbb210f25023b8a'] = 'Criteo';
+$_MODULE['<{criteo}prestashop>criteo_8f67a4f9e07e3c7fa0569c375c24cae7'] = 'Export de produit pour Criteo et l\'affichage des tags en Front Office';
+$_MODULE['<{criteo}prestashop>criteo_7ccf58c950043c9fbfed668df13ce608'] = 'Paramètres mis à jour';
+$_MODULE['<{criteo}prestashop>criteo_2667504f82250eacbd377f121b5c57e0'] = 'Export Criteo';
+$_MODULE['<{criteo}prestashop>criteo_96814c8ba0e8753f60d3ded7799ac5f8'] = 'Identifiant Criteo normal';
+$_MODULE['<{criteo}prestashop>criteo_e60d35bcd2d65206757417532f509366'] = 'Identifiant Criteo conversion';
+$_MODULE['<{criteo}prestashop>criteo_2a0b3f9396d21be5b3eb7e3bed1ed495'] = 'URL du widget';
+$_MODULE['<{criteo}prestashop>criteo_a4d3b161ce1309df1c4e25df28694b7b'] = 'Envoyer';
+$_MODULE['<{criteo}prestashop>criteo_c05d5199e627ece973cad37ccf3b24da'] = 'URL à communiquer à Criteo:';
