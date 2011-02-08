25eaa5eed089be48d4cdfafdfde9d8598d74bf63
fBrignoli <franck.brignoli@prestashop.com>
2011-02-08 14:11:09 +0000

    [-] Installer : Bug Fixed #7526 - temporary table

--- install-dev/sql/upgrade/1.4.0.11.sql
+++ install-dev/sql/upgrade/1.4.0.11.sql
@@ -1,11 +1,11 @@
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`)
 VALUES
 (
-'PS_INVOICE_START_NUMBER',
-(SELECT GREATEST(`value`, (SELECT CAST(MAX(`invoice_number`) AS CHAR) AS `invoice_number` FROM `PREFIX_orders`))  FROM `PREFIX_configuration` tmp WHERE `name` = 'PS_INVOICE_NUMBER' ),
+'PREFIX_INVOICE_START_NUMBER',
+( SELECT `invoice_number` FROM (SELECT GREATEST(`value`, (SELECT CAST(MAX(`invoice_number`) AS CHAR) FROM `PREFIX_orders`)) AS `invoice_number`  FROM `PREFIX_configuration`  WHERE `name` = 'PREFIX_INVOICE_NUMBER' ) as tmp),
 NOW(),
 NOW()
-);
+)
 
 DELETE FROM `PREFIX_configuration` WHERE `name` = 'PS_INVOICE_NUMBER';
 
