ebbbeea9c4318b979811762706e4d15a99912575
mMarinetti <michael.marinetti@prestashop.com>
2011-02-18 16:22:42 +0000

    // fixed error update/insert lang

--- classes/ObjectModel.php
+++ classes/ObjectModel.php
@@ -240,7 +240,7 @@ abstract class ObjectModelCore
 					// used to insert missing lang entries
 					$where_lang = '`'.pSQL($this->identifier).'` = '.(int)($this->id).' AND `id_lang` = '.(int)($field['id_lang']);
 
-					$lang_found = Db::getInstance()->getRow('SELECT COUNT(*) FROM `'.pSQL(_DB_PREFIX_.$this->table).'_lang` WHERE '. $where_lang)->getValue();
+					$lang_found = Db::getInstance()->getValue('SELECT COUNT(*) FROM `'.pSQL(_DB_PREFIX_.$this->table).'_lang` WHERE '. $where_lang);
 
 					if ($lang_found)
 						$result &= Db::getInstance()->AutoExecute(_DB_PREFIX_.$this->table.'_lang', $field, 'INSERT');
