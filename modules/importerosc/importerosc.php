35871423b196623ba63a5015837e6f35e0530cff
vAugagneur <vincent.augagneur@prestashop.com>
2011-02-21 10:51:07 +0000

    [-] MO : import oscommerce product status

--- modules/importerosc/importerosc.php
+++ modules/importerosc/importerosc.php
@@ -245,7 +245,7 @@ class importerosc extends ImportModule
 		$identifier = 'id_product';
 		$products = $this->ExecuteS('
 									SELECT p.`products_id` as id_product, p.`products_quantity` as quantity, p.`products_model` as reference, p.`products_price` as price, p.`products_weight` as weight,
-									1 as active, p.`manufacturers_id` as id_manufacturer, pd.language_id as id_lang, pd.products_name as name, pd.products_description as description, 
+									p.`products_status` as active, p.`manufacturers_id` as id_manufacturer, pd.language_id as id_lang, pd.products_name as name, pd.products_description as description, 
 									CONCAT(\'http://'.Tools::getValue('shop_url').'\/images/\',p.`products_image`) as images,
 									(SELECT ptc.categories_id FROM `'.addslashes($this->prefix).'products_to_categories` ptc WHERE ptc.`products_id` = p.`products_id` LIMIT 1) as id_category_default
 									FROM	`'.addslashes($this->prefix).'products` p LEFT JOIN `'.addslashes($this->prefix).'products_description` pd ON (p.products_id = pd.products_id)
