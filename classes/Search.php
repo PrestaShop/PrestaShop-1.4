7ccfc8c13fbd886ba3b516fd2f4528b58bd2e987
rGaillard <remi.gaillard@prestashop.com>
2011-02-23 15:06:03 +0000

    [-] FO: Fixed bug #7690 on search with cache

--- classes/Search.php
+++ classes/Search.php
@@ -248,7 +248,18 @@ class SearchCore
 		LIMIT '.(int)(($pageNumber - 1) * $pageSize).','.(int)$pageSize;
 
 		$result = $db->ExecuteS($queryResults);
-		$total = $db->getValue('SELECT FOUND_ROWS()');
+		$total = $db->getValue('SELECT COUNT(*)
+		FROM '._DB_PREFIX_.'product p
+		INNER JOIN `'._DB_PREFIX_.'product_lang` pl ON (p.`id_product` = pl.`id_product` AND pl.`id_lang` = '.(int)$id_lang.')
+		LEFT JOIN `'._DB_PREFIX_.'tax_rule` tr ON (p.`id_tax_rules_group` = tr.`id_tax_rules_group`
+		                                           AND tr.`id_country` = '.(int)Country::getDefaultCountryId().'
+	                                           	   AND tr.`id_state` = 0)
+	    LEFT JOIN `'._DB_PREFIX_.'tax` tax ON (tax.`id_tax` = tr.`id_tax`)
+		LEFT JOIN `'._DB_PREFIX_.'product_attribute` pa ON (p.`id_product` = pa.`id_product` AND default_on = 1)
+		LEFT JOIN `'._DB_PREFIX_.'manufacturer` m ON m.`id_manufacturer` = p.`id_manufacturer`
+		LEFT JOIN `'._DB_PREFIX_.'image` i ON (i.`id_product` = p.`id_product` AND i.`cover` = 1)
+		LEFT JOIN `'._DB_PREFIX_.'image_lang` il ON (i.`id_image` = il.`id_image` AND il.`id_lang` = '.(int)$id_lang.')
+		WHERE p.`id_product` '.$productPool);
 
 		Module::hookExec('search', array('expr' => $expr, 'total' => $total));
 
