80b6d2136a86cc1f0c253e0d7d92b5be13ebd1e1
fSerny <fabien.serny@prestashop.com>
2011-02-24 16:55:12 +0000

    [*] INSTALLER : Preactivation Paypal (new theme and field check)

--- install-dev/partner/paypal.php
+++ install-dev/partner/paypal.php
@@ -5,7 +5,7 @@
 
 		if (@file_get_contents('http://www.prestashop.com/partner/preactivation-xml.php'))
 		{
-			$result = simplexml_load_file('https://www.prestashop.com/partner/preactivation-xml.php?version=1.0&partner=paypal&country_iso_code='.addslashes($_GET['country_iso_code']));
+			$result = simplexml_load_file('https://www.prestashop.com/partner/preactivation/xml.php?version=1.0&partner=paypal&country_iso_code='.addslashes($_GET['country_iso_code']));
 			if ($result)
 			{
 				$varList = "";
@@ -23,28 +23,31 @@
 					}
 					elseif ($field->type == 'select')
 					{
-						echo '<select id="paypalform_'.$field->key.'" name="paypalform_'.$field->key.'">';
+						echo '<select id="paypalform_'.$field->key.'" name="paypalform_'.$field->key.'" style="width:175px;border:1px solid #D41958">';
 						foreach ($field->values as $key => $value)
 							echo '<option id="paypalform_'.$field->key.'_'.$key.'" value="'.$value->value.'" '.(trim($value->value) == trim($field->default) ? 'selected="selected"' : '').'>'.$value->label.'</option>';
 						echo '</select><br />';
 					}
 					elseif ($field->type == 'date')
 					{
-						echo '<select id="paypalform_'.$field->key.'_year" name="paypalform_'.$field->key.'_year">';
+						echo '<select id="paypalform_'.$field->key.'_year" name="paypalform_'.$field->key.'_year" style="border:1px solid #D41958">';
 						for ($i = 81; (date('Y') - $i) < date('Y'); $i--)
 							echo '<option value="'.(date('Y') - $i).'">'.(date('Y') - $i).'</option>';
 						echo '</select>';
-						echo '<select id="paypalform_'.$field->key.'_month" name="paypalform_'.$field->key.'_month">';
+						echo '<select id="paypalform_'.$field->key.'_month" name="paypalform_'.$field->key.'_month" style="border:1px solid #D41958">';
 						for ($i = 1; $i <= 12; $i++)
 							echo '<option value="'.($i < 10 ? '0'.$i : $i).'">'.($i < 10 ? '0'.$i : $i).'</option>';
 						echo '</select>';
-						echo '<select id="paypalform_'.$field->key.'_day" name="paypalform_'.$field->key.'_day">';
+						echo '<select id="paypalform_'.$field->key.'_day" name="paypalform_'.$field->key.'_day" style="border:1px solid #D41958">';
 						for ($i = 1; $i <= 31; $i++)
 							echo '<option value="'.($i < 10 ? '0'.$i : $i).'">'.($i < 10 ? '0'.$i : $i).'</option>';
 						echo '</select>';
 					}
 					echo '</div><br clear="left" />';
-					$varList .= "'&".$field->key."='+$('#paypalform_".$field->key."').val()+\n";
+					if ($field->type == 'date')
+						$varList .= "'&".$field->key."='+$('#paypalform_".$field->key."_year').val()+'-'+$('#paypalform_".$field->key."_month').val()+'-'+$('#paypalform_".$field->key."_day').val()+\n";
+					else
+						$varList .= "'&".$field->key."='+$('#paypalform_".$field->key."').val()+\n";
 				}
 				echo '<p align="right"><input id="paypalform_button" class="button little" type="button" value="Next" style="font-size: 0.8em;" /></p>
 				<div style="color: red; font-size: 0.8em;" id="paypalform_error"></div>
@@ -77,7 +80,41 @@
 
 	if ($_GET['request'] == 'send')
 	{
-		$url = 'https://www.prestashop.com/partner/preactivation-actions.php?version=1.0&partner=paypal';
+		$url = 'https://www.prestashop.com/partner/preactivation/actions.php?version=1.0&partner=paypal';
+
+		// Protect fields
+		foreach ($_GET as $key => $value)
+			$_GET[$key] = strip_tags(str_replace(array('\'', '"'), '', trim($value)));
+		$_GET['state_iso_code'] = 'TX';
+
+		// Get validation method for fields
+		require_once('../../classes/Validate.php');
+		$result = simplexml_load_file('https://www.prestashop.com/partner/preactivation/xml.php?version=1.0&partner=paypal&request=validate&country_iso_code='.addslashes($_GET['country_iso_code']));
+		if (!$result)
+		{
+			echo 'KO|Could not connect with Prestashop.com';
+			exit;
+		}
+
+		// Check errors
+		$error = '';
+		foreach ($result->field as $field)
+			if (isset($field->required) && trim($field->required) == 'yes' && empty($error))
+			{
+				if (empty($_GET[trim($field->key)]))
+					$error = 'Field "'.trim($field->label).'" is empty.';
+				else if (isset($field->validate) && !call_user_func('ValidateCore::'.trim($field->validate), $_GET[trim($field->key)]))
+					$error = 'Field "'.trim($field->label).'" is invalid.';
+			}
+
+		if (!empty($error))
+		{
+			echo 'KO|'.$error;
+			exit;
+		}
+
+
+		// Encore Get, Send It and Get Answers
 		foreach ($_GET as $key => $val)
 			$url .= '&'.$key.'='.urlencode($val);
 		$content = @file_get_contents($url);
