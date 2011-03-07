62c16a3599502e6d2ddf0dd57c7ad94a9a1510d0
mMarinetti <michael.marinetti@prestashop.com>
2011-03-07 18:16:59 +0000

    [-] Installer : Fixed #8143 - url_rewrite corrected in ps_meta_lang

--- install-dev/sql/upgrade/1.4.0.16.sql
+++ install-dev/sql/upgrade/1.4.0.16.sql
@@ -9,6 +9,11 @@ SELECT IFNULL(
 , 0
 )
 
+UPDATE  `PREFIX_meta_lang` SET  `url_rewrite` =  'kontaktieren-sie-uns' WHERE id_meta = 3 AND id_lang = 4 AND url_rewrite = 'Kontaktieren Sie uns';
+UPDATE  `PREFIX_meta_lang` SET  `url_rewrite` =  'kennwort-wiederherstellung' WHERE id_meta = 7 AND id_lang = 4 AND url_rewrite = 'Kennwort Wiederherstellung';
+UPDATE  `PREFIX_meta_lang` SET  `url_rewrite` =  'il-mio-account' WHERE id_meta = 18 AND id_lang = 5 AND url_rewrite = 'il mio-account';
+UPDATE  `PREFIX_meta_lang` SET  `url_rewrite` =  'nota-di-ordine' WHERE id_meta = 20 AND id_lang = 5 AND url_rewrite = 'nota di-ordine';
+
 
 /* PHP:add_module_to_hook(blockcategories, afterCreateHtaccess); */;
 
