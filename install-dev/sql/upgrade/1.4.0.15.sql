63f53f53cd288ab0a8c893cb11339405bc8fa028
aFolletete <alain.folletete@prestashop.com>
2011-03-13 17:45:57 +0000

    [-] SQL : fixed bug #8287 - sql upgrade problem 1.14.0.14 to 1.14.0.15

--- install-dev/sql/upgrade/1.4.0.15.sql
+++ install-dev/sql/upgrade/1.4.0.15.sql
@@ -32,7 +32,7 @@ INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VAL
 
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`)
 VALUES ('PS_USE_ECOTAX',
-		  (SELECT IF((SELECT ecotax FROM `PREFIX_product` WHERE  `ecotax` != 0),'1','0')),
+		  (SELECT IF((SELECT `ecotax` FROM `PREFIX_product` WHERE  `ecotax` != 0 LIMIT 1),'1','0')),
         NOW(),
         NOW());
 
