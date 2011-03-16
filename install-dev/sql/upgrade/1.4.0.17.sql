d1b8f6ea14023817cf0fb64fdafac558700598c4
aFolletete <alain.folletete@prestashop.com>
2011-03-16 14:02:20 +0000

    // translations

--- install-dev/sql/upgrade/1.4.0.17.sql
+++ install-dev/sql/upgrade/1.4.0.17.sql
@@ -5,4 +5,4 @@ UPDATE `PREFIX_stock_mvt_reason` SET `id_stock_mvt_reason`=`id_stock_mvt_reason`
 UPDATE `PREFIX_stock_mvt` SET `id_stock_mvt_reason`=`id_stock_mvt_reason`+2;
 UPDATE `PREFIX_stock_mvt_reason_lang` SET `id_stock_mvt_reason`=`id_stock_mvt_reason`+2 ORDER BY `id_stock_mvt_reason` DESC;
 INSERT INTO `PREFIX_stock_mvt_reason` (`id_stock_mvt_reason` ,`sign` ,`date_add` ,`date_upd`) VALUES ('1', '1', NOW(), NOW()), ('2', '-1', NOW(), NOW());
-INSERT INTO `PREFIX_stock_mvt_reason_lang` (`id_stock_mvt_reason` ,`id_lang` ,`name`) VALUES ('1', '1', 'Increase'), ('1', '2', 'Augmenter'), ('1', '3', 'Increase'), ('1', '4', 'Increase'),('1', '5', 'Increase'), ('2', '1', 'Decrease'), ('2', '2', 'Diminuer'), ('2', '3', 'Decrease'), ('2', '4', 'Decrease'), ('2', '5', 'Decrease');
+INSERT INTO `PREFIX_stock_mvt_reason_lang` (`id_stock_mvt_reason` ,`id_lang` ,`name`) VALUES ('1', '1', 'Increase'), ('1', '2', 'Augmenter'), ('1', '3', 'Increase'), ('1', '4', 'Erhöhen'),('1', '5', 'Aumento'), ('2', '1', 'Decrease'), ('2', '2', 'Diminuer'), ('2', '3', 'Decrease'), ('2', '4', 'Reduzieren'), ('2', '5', 'Diminuzione');
