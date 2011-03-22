e2947802eaf9f056754033dbbb07c51cdfa41490
aKorczak <anatole.korczak@prestashop.com>
2011-03-22 09:40:40 +0000

    [-] BO : install fix (now works)

--- install-dev/sql/db_settings_lite.sql
+++ install-dev/sql/db_settings_lite.sql
@@ -65,9 +65,8 @@ INSERT INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`
 (62, 'categoryDeletion', 'Category removal', '', 0, 0),
 (63, 'beforeAuthentication', 'Before Authentication', 'Before authentication', 0, 0),
 (64, 'paymentTop', 'Top of payment page', 'Top of payment page', 0, 0),
-(65, 'afterCreateHtaccess', 'After htaccess creation', 'After htaccess creation', 0, 0);
-(65, 'afterSaveAdminMeta', 'After save configuration in AdminMeta', 'After save configuration in AdminMeta', 0, 0);
-;
+(65, 'afterCreateHtaccess', 'After htaccess creation', 'After htaccess creation', 0, 0),
+(66, 'afterSaveAdminMeta', 'After save configuration in AdminMeta', 'After save configuration in AdminMeta', 0, 0);
 
 INSERT INTO `PREFIX_configuration` (`id_configuration`, `name`, `value`, `date_add`, `date_upd`) VALUES
 (1, 'PS_LANG_DEFAULT', '1', NOW(), NOW()),
