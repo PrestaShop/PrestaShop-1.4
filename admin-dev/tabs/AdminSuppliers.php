ee55fe205a907abde3dcd5925c4fc4fd01ba751b
vAugagneur <vincent.augagneur@prestashop.com>
2011-03-25 08:54:29 +0000

    [-] BO : fixed bug #8649 - Suppliers Description validates wrong

--- admin-dev/tabs/AdminSuppliers.php
+++ admin-dev/tabs/AdminSuppliers.php
@@ -146,7 +146,7 @@ class AdminSuppliers extends AdminTab
 				foreach ($this->_languages as $language)
 					echo '
 					<div id="description_'.$language['id_lang'].'" style="display: '.($language['id_lang'] == $this->_defaultFormLanguage ? 'block' : 'none').'; float: left;">
-						<input size="33" type="text" name="description_'.$language['id_lang'].'" value="'.htmlentities($this->getFieldValue($supplier, 'description', (int)($language['id_lang'])), ENT_COMPAT, 'UTF-8').'" /><sup> *</sup>
+						<input size="33" type="text" name="description_'.$language['id_lang'].'" value="'.htmlentities($this->getFieldValue($supplier, 'description', (int)($language['id_lang'])), ENT_COMPAT, 'UTF-8').'" />
 						<span class="hint" name="help_box">'.$this->l('Invalid characters:').' <>;=#{}<span class="hint-pointer">&nbsp;</span></span>
 						<p class="clear">'.$this->l('Will appear in supplier list').'</p>
 					</div>';							
