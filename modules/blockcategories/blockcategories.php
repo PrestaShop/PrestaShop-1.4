fb0b1711b121f636fe866d02a6a49d7cc863e881
dMetzger <damien.metzger@prestashop.com>
2011-04-11 09:56:11 +0000

    // Category block optimization

--- modules/blockcategories/blockcategories.php
+++ modules/blockcategories/blockcategories.php
@@ -148,13 +148,12 @@ class BlockCategories extends Module
 			$maxdepth = Configuration::get('BLOCK_CATEG_MAX_DEPTH');
 
 			if (!$result = Db::getInstance(_PS_USE_SQL_SLAVE_)->ExecuteS('
-				SELECT DISTINCT c.*, cl.*
+				SELECT c.id_parent, c.id_category, cl.name, cl.description, cl.link_rewrite
 				FROM `'._DB_PREFIX_.'category` c
 				LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category` AND `id_lang` = '.$id_lang.')
 				LEFT JOIN `'._DB_PREFIX_.'category_group` cg ON (cg.`id_category` = c.`id_category`)
-				WHERE 1'
-				.((int)($maxdepth) != 0 ? ' AND `level_depth` <= '.(int)($maxdepth) : '').'
-				AND (c.`active` = 1 OR c.`id_category` = 1)
+				WHERE (c.`active` = 1 OR c.`id_category` = 1)
+				'.((int)($maxdepth) != 0 ? ' AND `level_depth` <= '.(int)($maxdepth) : '').'
 				AND cg.`id_group` = '.$id_group.'
 				ORDER BY `level_depth` ASC, c.`position` ASC')
 			)
