5b165252c2d736a51e1fc7f8f1d6af2dab98732e
vSchoener <vincent.schoener@prestashop.com>
2011-04-19 08:29:59 +0000

    [-] MO: Carrier Compare, update french translation

---
+++ modules/carriercompare/fr.php
@@ -0,0 +1,21 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{carriercompare}prestashop>carriercompare_115484428c82f859100c07ae1170d9e0'] = 'Estimation des livraisons';
+$_MODULE['<{carriercompare}prestashop>carriercompare_1b28c1bcdfb00fe76bf8612d5cd81b01'] = 'Module permettant de faire une estimation des coûts de livraison avant de rentrer dans le processus de commande';
+$_MODULE['<{carriercompare}prestashop>carriercompare_ed70a57a190d596171cf324c16a2277d'] = 'L\'identifiant Stage est invalide';
+$_MODULE['<{carriercompare}prestashop>carriercompare_66e799a222e07b1cf25498465028061d'] = 'L\'identifiant Country est invalide';
+$_MODULE['<{carriercompare}prestashop>carriercompare_8250266f9d2fb72b4f8921a0b998546e'] = 'Veuillez utiliser un code postal valide en fonction du pays choisi';
+$_MODULE['<{carriercompare}prestashop>carriercompare_e3f585e306c3cda18369730a6a628f7b'] = 'L\'identifiant du transporteur est invalide';
+$_MODULE['<{carriercompare}prestashop>carriercompare_efaf5c3827ef576b05de32ad458f196c'] = 'Cet identifiant de transporteur n\'est pas disponible pour cette sélection';
+$_MODULE['<{carriercompare}prestashop>carriercompare_edc5cf1b3bab64b1d45ef51c63b53887'] = 'Impossibilité de mettre a jour le panier';
+$_MODULE['<{carriercompare}prestashop>carriercompare_e7a6ca4e744870d455a57b644f696457'] = 'Offert !';
+$_MODULE['<{carriercompare}prestashop>carriercompare_fbc1bef527a3a092053a244fd8d87abf'] = 'Estimez votre livraison & taxes';
+$_MODULE['<{carriercompare}prestashop>carriercompare_59716c97497eb9694541f7c3d37b1a4d'] = 'Pays';
+$_MODULE['<{carriercompare}prestashop>carriercompare_46a2a41cc6e552044816a2d04634545d'] = 'Etat';
+$_MODULE['<{carriercompare}prestashop>carriercompare_e0ed8e71dbd22a1e91cb570b52443fd5'] = 'Code postal';
+$_MODULE['<{carriercompare}prestashop>carriercompare_914419aa32f04011357d3b604a86d7eb'] = 'Transporteur';
+$_MODULE['<{carriercompare}prestashop>carriercompare_3601146c4e948c32b6424d2c0a7f0118'] = 'Prix';
+$_MODULE['<{carriercompare}prestashop>carriercompare_f620c0ed0c89129c9dd8953406300061'] = 'Aucun  transporteur n\'est disponible pour cette sélection';
+$_MODULE['<{carriercompare}prestashop>carriercompare_c6b61ece4cb72c71e84d91a4d22aa449'] = 'Lancer l\'estimation';
