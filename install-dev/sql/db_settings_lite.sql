fd60c4d0d39a1623b571c168c510f8d1fd8aae42
aFolletete <alain.folletete@prestashop.com>
2011-04-20 08:22:00 +0000

    // fix sql

--- install-dev/sql/db_settings_lite.sql
+++ install-dev/sql/db_settings_lite.sql
@@ -1027,7 +1027,7 @@ INSERT INTO `PREFIX_stock_mvt_reason_lang` (`id_stock_mvt_reason`, `id_lang`, `n
 (5, 5, 'Riassortimento');
 
 INSERT INTO `PREFIX_address_format` (`id_country`, `format`)
-(SELECT `id_country` as id_country, 'firstname lastname\ncompany\nvat_number\naddress1\naddress2\npostcode city\ncountry\nphone' as format from ps_country);
+(SELECT `id_country` as id_country, 'firstname lastname\ncompany\nvat_number\naddress1\naddress2\npostcode city\ncountry\nphone' as format FROM `PREFIX_country`);
 
 UPDATE `PREFIX_address_format` set `format`='firstname lastname
 company
