abc33cc2c7c4e7c098043fa3570fd15984a20f01
aKorczak <anatole.korczak@prestashop.com>
2011-05-03 13:10:06 +0000

    [-] WS : Fixed bug PSCFI-1731

--- classes/Order.php
+++ classes/Order.php
@@ -1038,6 +1038,7 @@ class OrderCore extends ObjectModel
 		$id_order_state = 1; // TODO
 		$customer = new Customer($this->id_customer);
 		$paymentModule->validateOrder($this->id_cart, $id_order_state, $this->total_paid,	$this->payment,	NULL, array(), null, false, $customer->secure_key);
+		$this->id = $paymentModule->currentOrder;
 		return true;
 	}
 
