e81ffbe4ff32b281cf705d16b3651ff862a80be7
mMarinetti <michael.marinetti@prestashop.com>
2011-05-06 16:48:27 +0000

    [+] MO : blockcategories can now be hooked on footer

--- modules/blockcategories/blockcategories.php
+++ modules/blockcategories/blockcategories.php
@@ -36,7 +36,6 @@ class BlockCategories extends Module
 		$this->tab = 'front_office_features';
 		$this->version = '2.0';
 		$this->author = 'PrestaShop';
-		$this->need_instance = 0;
 
 		parent::__construct();
 
@@ -76,6 +75,7 @@ class BlockCategories extends Module
 		{
 			$maxDepth = (int)(Tools::getValue('maxDepth'));
 			$dhtml = Tools::getValue('dhtml');
+			$nbrColumns = Tools::getValue('nbrColumns',4);
 			if ($maxDepth < 0)
 				$output .= '<div class="alert error">'.$this->l('Maximum depth: Invalid number.').'</div>';
 			elseif ($dhtml != 0 AND $dhtml != 1)
@@ -84,6 +84,7 @@ class BlockCategories extends Module
 			{
 				Configuration::updateValue('BLOCK_CATEG_MAX_DEPTH', (int)($maxDepth));
 				Configuration::updateValue('BLOCK_CATEG_DHTML', (int)($dhtml));
+				Configuration::updateValue('BLOCK_CATEG_NBR_COLUMN_FOOTER', $nbrColumns);
 				$this->_clearBlockcategoriesCache();
 				$output .= '<div class="conf confirm"><img src="../img/admin/ok.gif" alt="'.$this->l('Confirmation').'" />'.$this->l('Settings updated').'</div>';
 			}
@@ -110,6 +111,11 @@ class BlockCategories extends Module
 					<input type="radio" name="dhtml" id="dhtml_off" value="0" '.(!Tools::getValue('dhtml', Configuration::get('BLOCK_CATEG_DHTML')) ? 'checked="checked" ' : '').'/>
 					<label class="t" for="dhtml_off"> <img src="../img/admin/disabled.gif" alt="'.$this->l('Disabled').'" title="'.$this->l('Disabled').'" /></label>
 					<p class="clear">'.$this->l('Activate dynamic (animated) mode for sublevels').'</p>
+				</div>
+				<label>'.$this->l('Footer columns number').'</label>			
+				<div class="margin-form">
+					<input type="text" name="nbrColumns" value="'.Configuration::get('BLOCK_CATEG_NBR_COLUMN_FOOTER').'" />
+					<p class="clear">'.$this->l('Set the number of footer columns').'</p>
 				</div>
 				<center><input type="submit" name="submitBlockCategories" value="'.$this->l('Save').'" class="button" /></center>
 			</fieldset>
@@ -196,6 +202,84 @@ class BlockCategories extends Module
 		}
 		$smarty->cache_lifetime = 31536000; // 1 Year
 		$display = $this->display(__FILE__, 'blockcategories.tpl', $smartyCacheId);
+		Tools::restoreCacheSettings();
+		return $display;
+	}
+
+	public function hookFooter($params)
+	{
+		global $smarty, $cookie;
+
+		$id_customer = (int)($params['cookie']->id_customer);
+		$id_group = $id_customer ? Customer::getDefaultGroupId($id_customer) : _PS_DEFAULT_CUSTOMER_GROUP_;
+		$id_product = (int)(Tools::getValue('id_product', 0));
+		$id_category = (int)(Tools::getValue('id_category', 0));
+		$id_lang = (int)($params['cookie']->id_lang);
+		$smartyCacheId = 'blockcategories|'.$id_group.'_'.$id_lang.'_'.$id_product.'_'.$id_category;
+
+		Tools::enableCache();
+		if (!$this->isCached('blockcategories_footer.tpl', $smartyCacheId))
+		{
+			$maxdepth = Configuration::get('BLOCK_CATEG_MAX_DEPTH');
+
+			if (!$result = Db::getInstance(_PS_USE_SQL_SLAVE_)->ExecuteS('
+				SELECT c.id_parent, c.id_category, cl.name, cl.description, cl.link_rewrite
+				FROM `'._DB_PREFIX_.'category` c
+				LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category` AND `id_lang` = '.$id_lang.')
+				LEFT JOIN `'._DB_PREFIX_.'category_group` cg ON (cg.`id_category` = c.`id_category`)
+				WHERE (c.`active` = 1 OR c.`id_category` = 1)
+				'.((int)($maxdepth) != 0 ? ' AND `level_depth` <= '.(int)($maxdepth) : '').'
+				AND cg.`id_group` = '.$id_group.'
+				ORDER BY `level_depth` ASC, c.`position` ASC')
+			)
+				return;
+			$resultParents = array();
+			$resultIds = array();
+
+			foreach ($result as &$row)
+			{
+				$resultParents[$row['id_parent']][] = &$row;
+				$resultIds[$row['id_category']] = &$row;
+			}
+			//$nbrColumns = Configuration::get('BLOCK_CATEG_NBR_COLUMNS_FOOTER');
+			$nbrColumns = Configuration::get('BLOCK_CATEG_NBR_COLUMN_FOOTER');
+			if(!$nbrColumns)
+				$nbrColumns=3;
+			$numberColumn = abs(sizeof($result)/$nbrColumns);
+			$widthColumn= floor(100/$nbrColumns);
+			$smarty->assign('numberColumn', $numberColumn);
+			$smarty->assign('widthColumn', $widthColumn);
+			
+			$blockCategTree = $this->getTree($resultParents, $resultIds, Configuration::get('BLOCK_CATEG_MAX_DEPTH'));
+			unset($resultParents);
+			unset($resultIds);
+			$isDhtml = (Configuration::get('BLOCK_CATEG_DHTML') == 1 ? true : false);
+
+			if (Tools::isSubmit('id_category'))
+			{
+				$cookie->last_visited_category = $id_category;
+				$smarty->assign('currentCategoryId', $cookie->last_visited_category);
+			}
+			if (Tools::isSubmit('id_product'))
+			{
+				if (!isset($cookie->last_visited_category) OR !Product::idIsOnCategoryId($id_product, array('0' => array('id_category' => $cookie->last_visited_category))))
+				{
+					$product = new Product($id_product);
+					if (isset($product) AND Validate::isLoadedObject($product))
+						$cookie->last_visited_category = (int)($product->id_category_default);
+				}
+				$smarty->assign('currentCategoryId', (int)($cookie->last_visited_category));
+			}
+			$smarty->assign('blockCategTree', $blockCategTree);
+
+			if (file_exists(_PS_THEME_DIR_.'modules/blockcategories/blockcategories_footer.tpl'))
+				$smarty->assign('branche_tpl_path', _PS_THEME_DIR_.'modules/blockcategories/category-tree-branch.tpl');
+			else
+				$smarty->assign('branche_tpl_path', _PS_MODULE_DIR_.'blockcategories/category-tree-branch.tpl');
+			$smarty->assign('isDhtml', $isDhtml);
+		}
+		$smarty->cache_lifetime = 31536000; // 1 Year
+		$display = $this->display(__FILE__, 'blockcategories_footer.tpl', $smartyCacheId);
 		Tools::restoreCacheSettings();
 		return $display;
 	}
--- modules/blockcategories/fr.php
+++ modules/blockcategories/fr.php
@@ -15,5 +15,7 @@ $_MODULE['<{blockcategories}prestashop>blockcategories_971fd8cc345d8bd9f92e9f7d8
 $_MODULE['<{blockcategories}prestashop>blockcategories_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Activé';
 $_MODULE['<{blockcategories}prestashop>blockcategories_b9f5c797ebbf55adccdd8539a65a0241'] = 'Desactivé';
 $_MODULE['<{blockcategories}prestashop>blockcategories_2c1199ee18b344152f35ec237acb5796'] = 'Activer l\'arbre dynamique (animé)';
+$_MODULE['<{blockcategories}prestashop>blockcategories_4f32360218e145e3ba681a2343015eb3'] = 'Nombre de colonnes pour le footer';
+$_MODULE['<{blockcategories}prestashop>blockcategories_3d81ca758785a8528208613752ad2c81'] = 'Détermine le nombre de colonne pour le footer';
 $_MODULE['<{blockcategories}prestashop>blockcategories_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
 $_MODULE['<{blockcategories}prestashop>blockcategories_af1b98adf7f686b84cd0b443e022b7a0'] = 'Catégories';
