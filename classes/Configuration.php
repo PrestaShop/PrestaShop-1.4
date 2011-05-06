2bccfe986851cd634199aef8581fbc41206146a1
dMetzger <damien.metzger@prestashop.com>
2011-05-06 15:15:56 +0000

    // Configuration fix

--- classes/Configuration.php
+++ classes/Configuration.php
@@ -297,10 +297,10 @@ class ConfigurationCore extends ObjectModel
 		
 		foreach ($result AS $row)
 		{
-			if ($row['c_value'])
-				self::$_CONF[$row['name']] = stripslashes($row['c_value']);
-			if ($row['cl_value'])
+			if ($row['id_lang'])
 				self::$_CONF_LANG[(int)($row['id_lang'])][$row['name']] = stripslashes($row['cl_value']);
+			else
+				self::$_CONF[$row['name']] = stripslashes($row['c_value']);
 		}
 	}
 	
