a94c94a2d1e8635d9b4df23195d995af6d25a131
tDidierjean <thomas.didierjean@prestashop.com>
2011-05-09 13:46:11 +0000

    [-] BO : fixed bug #PSCFI-1812 - CSV Import bug - wrong nice URLs

--- classes/Tools.php
+++ classes/Tools.php
@@ -882,30 +882,61 @@ class ToolsCore
 		return (($lang AND is_array($lang) AND key_exists($key, $lang)) ? stripslashes($lang[$key]) : $key);
 	}
 
+	
+	/**
+	 * Return the friendly url from the provided string
+	 *
+	 * @param string $str
+	 * @param bool $utf8_decode => needs to be marked as deprecated
+	 * @return string
+	 */
 	public static function link_rewrite($str, $utf8_decode = false)
 	{
-		$purified = '';
-		$length = self::strlen($str);
-		if ($utf8_decode)
-			$str = utf8_decode($str);
-		for ($i = 0; $i < $length; $i++)
-		{
-			$char = self::substr($str, $i, 1);
-			if (self::strlen(htmlentities($char)) > 1)
-			{
-				$entity = htmlentities($char, ENT_COMPAT, 'UTF-8');
-				$purified .= $entity{1};
-			}
-			elseif (preg_match('|[[:alpha:]]{1}|u', $char))
-				$purified .= $char;
-			elseif (preg_match('<[[:digit:]]|-{1}>', $char))
-				$purified .= $char;
-			elseif ($char == ' ')
-				$purified .= '-';
-			elseif ($char == '\'')
-				$purified .= '-';
-		}
-		return trim(self::strtolower($purified));
+		return self::str2url($str);
+	}
+	
+	/**
+	 * Return a friendly url made from the provided string
+	 * If the mbstring library is available, the output is the same as the js function of the same name
+	 * 
+	 * @param string $str
+	 * @return string
+	 */
+	public static function str2url($str)
+	{
+		if (function_exists('mb_strtolower'))
+			$str = mb_strtolower($str, 'utf-8');
+
+		$str = trim($str);
+		$str = preg_replace('/[\x{0105}\x{0104}\x{00E0}\x{00E1}\x{00E2}\x{00E3}\x{00E4}\x{00E5}]/u','a', $str);
+		$str = preg_replace('/[\x{00E7}\x{010D}\x{0107}\x{0106}]/u','c', $str);
+		$str = preg_replace('/[\x{010F}]/u','d', $str);
+		$str = preg_replace('/[\x{00E8}\x{00E9}\x{00EA}\x{00EB}\x{011B}\x{0119}\x{0118}]/u','e', $str);
+		$str = preg_replace('/[\x{00EC}\x{00ED}\x{00EE}\x{00EF}]/u','i', $str);
+		$str = preg_replace('/[\x{0142}\x{0141}\x{013E}\x{013A}]/u','l', $str);
+		$str = preg_replace('/[\x{00F1}\x{0148}]/u','n', $str);
+		$str = preg_replace('/[\x{00F2}\x{00F3}\x{00F4}\x{00F5}\x{00F6}\x{00F8}\x{00D3}]/u','o', $str);
+		$str = preg_replace('/[\x{0159}\x{0155}]/u','r', $str);
+		$str = preg_replace('/[\x{015B}\x{015A}\x{0161}]/u','s', $str);
+		$str = preg_replace('/[\x{00DF}]/u','ss', $str);
+		$str = preg_replace('/[\x{0165}]/u','t', $str);
+		$str = preg_replace('/[\x{00F9}\x{00FA}\x{00FB}\x{00FC}\x{016F}]/u','u', $str);
+		$str = preg_replace('/[\x{00FD}\x{00FF}]/u','y', $str);
+		$str = preg_replace('/[\x{017C}\x{017A}\x{017B}\x{0179}\x{017E}]/u','z', $str);
+		$str = preg_replace('/[\x{00E6}]/u','ae', $str);
+		$str = preg_replace('/[\x{0153}]/u','oe', $str);
+	
+		// Remove all non-whitelist chars.
+		$str = preg_replace('/[^a-zA-Z0-9\s\'\:\/\[\]-]/','', $str);
+		$str = preg_replace('/[\s\'\:\/\[\]-]+/',' ', $str);
+		$str = preg_replace('/[ ]/','-', $str);
+		$str = preg_replace('/[\/]/','-', $str);
+		
+		// If it was not possible to lowercase the string with mb_strtolower, we do it after the transformations.
+		// This way we lose fewer special chars. 
+		$str = strtolower($str);
+
+		return $str;
 	}
 
 	/**
