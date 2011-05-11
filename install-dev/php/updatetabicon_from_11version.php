f9c67fe9075573fc168a8a6303ea60f2cedb71b6
mMarinetti <michael.marinetti@prestashop.com>
2011-05-11 16:20:14 +0000

    // oops

--- install-dev/php/updatetabicon_from_11version.php
+++ install-dev/php/updatetabicon_from_11version.php
@@ -26,30 +26,22 @@
 */
 function updatetabicon_from_11version()
 {
-		file_put_contents("/home/michael/checkMaj.log","\r\nPASSE START\r\n",FILE_APPEND);
 	global $oldversion;
 	if (version_compare($oldversion,'1.5.0.0','<'))
 	{
-		file_put_contents("/home/michael/checkMaj.log",'before db',FILE_APPEND);
-file_put_contents("/home/michael/checkMaj.log",'SELECT `id_tab`,`class_name` FROM '._DB_PREFIX_.'tab',FILE_APPEND);
 
 		$rows = Db::getInstance()->ExecuteS('SELECT `id_tab`,`class_name` FROM '._DB_PREFIX_.'tab');
-		FB::info($rows,'rows');
-		file_put_contents("/home/michael/checkMaj.log",'after db',FILE_APPEND);
-		file_put_contents("/home/michael/checkMaj.log",'rows:'.print_r($rows,true),FILE_APPEND);
 		if (sizeof($rows))
 		{
 			$img_dir = scandir(_PS_IMG_DIR_.'/t/');
 			$result = true;
 			foreach ($rows as $tab)
 			{
-				file_put_contents("/home/michael/checkMaj.log",$tab['id_tab'],FILE_APPEND);
 				if (file_exists(_PS_IMG_DIR_.'/t/'.$tab['id_tab'].'.gif') 
 					AND !file_exists(_PS_IMG_DIR_.'/t/'.$tab['class_name'].'.gif'))
 					$result &= rename(_PS_IMG_DIR_.'/t/'.$tab['id_tab'].'.gif',_PS_IMG_DIR_.'/t/'.$tab['class_name'].'.gif');
 			}
 		}
-	}else 
-		file_put_contents("/home/michael/checkMaj.log",'version doesnt match',FILE_APPEND);
+	}
 	return true;
 }
