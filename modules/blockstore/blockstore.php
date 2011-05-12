87d4d7e2ae4dd5d3d8feaeedbab53ac246670105
mMarinetti <michael.marinetti@prestashop.com>
2011-05-12 16:31:21 +0000

    [-] MO : Fixed open basedir

--- modules/blockstore/blockstore.php
+++ modules/blockstore/blockstore.php
@@ -71,7 +71,7 @@ class BlockStore extends Module
 	
 	function hookHeader($params)
 	{
-		Tools::addCSS(_THEME_CSS_DIR_.'modules/'.$this->name.'/blockstore.css', 'all');
+		Tools::addCSS($this->_path.'/blockstore.css', 'all');
 	}
 	
 	public function postProcess()
