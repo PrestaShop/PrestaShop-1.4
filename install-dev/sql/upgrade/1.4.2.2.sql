eb24bce140c7aaa2e36da241215e583a46bae2ab
mMarinetti <michael.marinetti@prestashop.com>
2011-05-17 12:09:39 +0000

    // secure key recovery from 1.4.0.2 in table cart

--- install-dev/sql/upgrade/1.4.2.2.sql
+++ install-dev/sql/upgrade/1.4.2.2.sql
@@ -6,3 +6,6 @@ UPDATE `PREFIX_country` SET `display_tax_label` = '1' WHERE `id_country` = 21;
 /* PHP:add_module_to_hook(blockcms, leftColumn); */;
 /* PHP:add_module_to_hook(blockcms, rightColumn); */;
 /* PHP:add_module_to_hook(blockcms, footer); */;
+
+UPDATE `PREFIX_cart` ca SET `secure_key` = (SELECT `secure_key` from `PREFIX_customer` `cu` WHERE `cu`.`id_customer` = `ca`.`id_customer`) WHERE `ca`.`secure_key` = -1;
+
