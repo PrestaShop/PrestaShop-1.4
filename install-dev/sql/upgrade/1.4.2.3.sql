dd27f6f274346c36bdcc460a913e764189d0f085
tDidierjean <thomas.didierjean@prestashop.com>
2011-05-23 16:44:32 +0000

    [*] Installer : added unique index to table category_group

--- install-dev/sql/upgrade/1.4.2.3.sql
+++ install-dev/sql/upgrade/1.4.2.3.sql
@@ -31,5 +31,9 @@ INSERT IGNORE INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_up
 
 UPDATE `PREFIX_tab_lang` SET `name` = 'SEO & URLs' WHERE `id_tab` = (SELECT `id_tab` FROM `PREFIX_tab` WHERE `class_name` = 'AdminMeta' LIMIT 1) AND `id_lang` IN (SELECT `id_lang` FROM `PREFIX_lang` WHERE `iso_code` IN ('en','fr','es','de','it'));
 
-/* PHP:add_missing_rewrite_value(); */;
+/* These 3 lines (remove_duplicate, drop index, add unique) MUST stay together in this order */
 /* PHP:remove_duplicate_category_groups(); */;
+ALTER TABLE `PREFIX_category_group` DROP INDEX `category_group_index`;
+ALTER TABLE `PREFIX_category_group` ADD UNIQUE `category_group_index` (`id_category`,`id_group`);
+
+/* PHP:add_missing_rewrite_value(); */;
