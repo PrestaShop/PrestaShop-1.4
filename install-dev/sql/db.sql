797f49e1a60c0bd4e45108c603c0c276073dfc32
tDidierjean <thomas.didierjean@prestashop.com>
2011-05-24 09:07:12 +0000

    // add unique index to category group

--- install-dev/sql/db.sql
+++ install-dev/sql/db.sql
@@ -212,7 +212,7 @@ CREATE TABLE `PREFIX_category` (
 CREATE TABLE `PREFIX_category_group` (
   `id_category` int(10) unsigned NOT NULL,
   `id_group` int(10) unsigned NOT NULL,
-  KEY `category_group_index` (`id_category`,`id_group`),
+  UNIQUE KEY `category_group_index` (`id_category`,`id_group`),
   KEY `id_category` (`id_category`),
   KEY `id_group` (`id_group`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
