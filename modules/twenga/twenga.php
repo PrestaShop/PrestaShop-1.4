36b11987fd7697801d00090f8b2b2d1c16f80cd3
fSerny <fabien.serny@prestashop.com>
2011-05-24 14:15:48 +0000

    // Forget to update Twenga module version from my previous commit

--- modules/twenga/twenga.php
+++ modules/twenga/twenga.php
@@ -122,7 +122,7 @@ class Twenga extends PaymentModule
 		$this->token = Tools::getValue('token');
 	 	$this->name = 'twenga';
 	 	$this->tab = 'smart_shopping';
-	 	$this->version = '1.5';
+	 	$this->version = '1.6';
 		
 	 	parent::__construct();
 	
