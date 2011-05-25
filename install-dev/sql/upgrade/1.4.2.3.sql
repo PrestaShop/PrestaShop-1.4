753eef70c861856d8b388dc3567163afdafc337c
aFolletete <alain.folletete@prestashop.com>
2011-05-25 08:21:53 +0000

    // update version on installer

--- install-dev/sql/upgrade/1.4.2.3.sql
+++ install-dev/sql/upgrade/1.4.2.3.sql
@@ -27,13 +27,4 @@ UPDATE `PREFIX_order_state_lang` SET `name` = 'PayPal Anmeldung erfolgreich' WHE
 
 UPDATE `PREFIX_meta_lang` SET `url_rewrite` = 'identita' WHERE `url_rewrite` = 'Identità';
 
-INSERT IGNORE INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES ('PS_BACKUP_DROP_TABLE', 1, NOW(), NOW());
-
-UPDATE `PREFIX_tab_lang` SET `name` = 'SEO & URLs' WHERE `id_tab` = (SELECT `id_tab` FROM `PREFIX_tab` WHERE `class_name` = 'AdminMeta' LIMIT 1) AND `id_lang` IN (SELECT `id_lang` FROM `PREFIX_lang` WHERE `iso_code` IN ('en','fr','es','de','it'));
-
-/* These 3 lines (remove_duplicate, drop index, add unique) MUST stay together in this order */
-/* PHP:remove_duplicate_category_groups(); */;
-ALTER TABLE `PREFIX_category_group` DROP INDEX `category_group_index`;
-ALTER TABLE `PREFIX_category_group` ADD UNIQUE `category_group_index` (`id_category`,`id_group`);
-
 /* PHP:add_missing_rewrite_value(); */;
