1e9566de8dcf64709035025c32177633ad098fb6
dMetzger <damien.metzger@prestashop.com>
2011-06-03 12:32:13 +0000

    // Code cleaning

--- config/autoload.php
+++ config/autoload.php
@@ -38,23 +38,23 @@ function __autoload($className)
 	
 	// This is a Core class and its name is the same as its declared name
 	if (substr($className, -4) == 'Core')
-		require($classDir.substr($className, 0, -4).'.php');
+		require_once($classDir.substr($className, 0, -4).'.php');
 	else
 	{
 		if ($file_in_override && $file_in_classes)
 		{
-			require($classDir.str_replace(chr(0), '', $className).'.php');
-			require($overrideDir.$className.'.php');
+			require_once($classDir.str_replace(chr(0), '', $className).'.php');
+			require_once($overrideDir.$className.'.php');
 		}
 		elseif (!$file_in_override && $file_in_classes)
 		{
-			require($classDir.str_replace(chr(0), '', $className).'.php');
+			require_once($classDir.str_replace(chr(0), '', $className).'.php');
 			$classInfos = new ReflectionClass($className.((interface_exists($className, false) or class_exists($className, false)) ? '' : 'Core'));
 			if (!$classInfos->isInterface())
 				eval(($classInfos->isAbstract() ? 'abstract ' : '').'class '.$className.' extends '.$className.'Core {}');
 		}
 		elseif ($file_in_override && !$file_in_classes)
-			require($overrideDir.$className.'.php');
+			require_once($overrideDir.$className.'.php');
 	}
 }
 
