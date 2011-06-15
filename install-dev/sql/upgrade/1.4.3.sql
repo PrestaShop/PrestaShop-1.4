47223953d151e5f203866fd33bc8414bb870d1b1
mDeflotte <maxence.deflotte@prestashop.com>
2011-06-15 14:05:15 +0000

    [-] Installer : add a ";" to SQL requeste

--- install-dev/sql/upgrade/1.4.3.sql
+++ install-dev/sql/upgrade/1.4.3.sql
@@ -8,7 +8,7 @@ city
 State:name
 postcode
 Country:name' 
-WHERE id_country = (SELECT id_country FROM `PREFIX_country` WHERE `iso_code`='GB')
+WHERE id_country = (SELECT id_country FROM `PREFIX_country` WHERE `iso_code`='GB');
 
 UPDATE `PREFIX_country` SET `contains_states` = 1 WHERE `id_country` = 145;
 
