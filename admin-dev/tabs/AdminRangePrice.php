8a1c45fd7ea8efbf01fc8bafbfdeab69a3182d2e
fBrignoli <franck.brignoli@prestashop.com>
2011-06-22 13:30:37 +0000

    // revert PSCFI-2282

--- admin-dev/tabs/AdminRangePrice.php
+++ admin-dev/tabs/AdminRangePrice.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* 2007-2011 PrestaShop
+* 2007-2011 PrestaShop 
 *
 * NOTICE OF LICENSE
 *
@@ -40,14 +40,14 @@ class AdminRangePrice extends AdminTab
 		'carrier_name' => array('title' => $this->l('Carrier'), 'align' => 'center', 'width' => 25, 'filter_key' => 'ca!name'),
 		'delimiter1' => array('title' => $this->l('From'), 'width' => 86, 'price' => true, 'align' => 'right'),
 		'delimiter2' => array('title' => $this->l('To'), 'width' => 86, 'price' => true, 'align' => 'right'));
-
+		
 		$this->_join = 'LEFT JOIN '._DB_PREFIX_.'carrier ca ON (ca.`id_carrier` = a.`id_carrier`)';
 		$this->_select = 'ca.`name` AS carrier_name';
 		$this->_where = 'AND ca.`deleted` = 0';
 
 		parent::__construct();
 	}
-
+	
 	public function displayListContent($token = NULL)
 	{
 		foreach ($this->_list as $key => $list)
@@ -72,20 +72,13 @@ class AdminRangePrice extends AdminTab
 		if (!($obj = $this->loadObject(true)))
 			return;
 		$currency = new Currency(Configuration::get('PS_CURRENCY_DEFAULT'));
-		$default_country = new Country((int)Configuration::get('PS_COUNTRY_DEFAULT'));
 
 		$carrierArray = array();
 		$carriers = Carrier::getCarriers((int)(Configuration::get('PS_LANG_DEFAULT')), true , false,false, NULL, PS_CARRIERS_AND_CARRIER_MODULES_NEED_RANGE);
 		$id_carrier = Tools::getValue('id_carrier', $obj->id_carrier);
 		foreach ($carriers AS $carrier)
 			if (!$carrier['is_free'])
-			{
 				$carrierArray[] = '<option value="'.(int)($carrier['id_carrier']).'"'.(($carrier['id_carrier'] == $id_carrier) ? ' selected="selected"' : '').'>'.$carrier['name'].'</option><sup>*</sup>';
-
-				$carrier_taxes[(int)$carrier['id_carrier']] = TaxRulesGroup::getTaxesRate((int)$carrier['id_tax_rules_group'], (int)Configuration::get('PS_COUNTRY_DEFAULT'), 0, 0);
-			}
-
-		echo $this->displayJavascript($carrier_taxes);
 
 		echo '
 		<form action="'.$currentIndex.'&submitAdd'.$this->table.'=1&token='.$this->token.'" method="post">
@@ -95,7 +88,7 @@ class AdminRangePrice extends AdminTab
 				<div class="margin-form">';
 			if (count($carrierArray))
 			{
-				echo '<select id="id_carrier" name="id_carrier">';
+				echo '<select name="id_carrier">';
 				foreach ($carrierArray AS $carrierOption)
 					echo $carrierOption;
 				echo '</select>
@@ -107,18 +100,12 @@ class AdminRangePrice extends AdminTab
 				</div>
 				<label>'.$this->l('From:').' </label>
 				<div class="margin-form">
-					'.$currency->getSign('left').'<input type="text" size="7" id="delimiter1_excl" name="delimiter1_excl" value="" />'.$currency->getSign('right').'<sup>*</sup>
-					'.$this->l('(tax excl.)').'
-					'.$currency->getSign('left').'<input type="text" size="7" id="delimiter1" name="delimiter1" value="'.Tools::ps_round($this->getFieldValue($obj, 'delimiter1'), 2).'" />'.$currency->getSign('right').'<sup>*</sup>
-					'.$this->l('(tax incl.)').'
+					'.$currency->getSign('left').'<input type="text" size="4" name="delimiter1" value="'.htmlentities($this->getFieldValue($obj, 'delimiter1'), ENT_COMPAT, 'UTF-8').'" />'.$currency->getSign('right').'<sup>*</sup>
 					<p class="clear">'.$this->l('Range start (included)').'</p>
 				</div>
 				<label>'.$this->l('To:').' </label>
 				<div class="margin-form">
-					'.$currency->getSign('left').'<input type="text" size="7" id="delimiter2_excl" name="delimiter2_excl" value="" />'.$currency->getSign('right').'<sup>*</sup>
-					'.$this->l('(tax excl.)').'
-					'.$currency->getSign('left').'<input type="text" size="7" id="delimiter2" name="delimiter2" value="'.Tools::ps_round($this->getFieldValue($obj, 'delimiter2'), 2).'" />'.$currency->getSign('right').'<sup>*</sup>
-					'.$this->l('(tax incl.)').'
+					'.$currency->getSign('left').'<input type="text" size="4" name="delimiter2" value="'.htmlentities($this->getFieldValue($obj, 'delimiter2'), ENT_COMPAT, 'UTF-8').'" />'.$currency->getSign('right').'<sup>*</sup>
 					<p class="clear">'.$this->l('Range end (excluded)').'</p>
 				</div>
 				<div class="margin-form">
@@ -128,51 +115,6 @@ class AdminRangePrice extends AdminTab
 			</fieldset>
 		</form>';
 	}
-
-	public function displayJavascript($carrier_taxes)
-	{
-		$html = '<script type="text/javascript">'."\n";
-		$html .= 'var carrier_taxes = new Array();'."\n";
-		foreach($carrier_taxes as $id_carrier => $tax)
-			$html .= 'carrier_taxes['.(int)$id_carrier.'] = '.(float)$tax."\n";
-
-		$html .= '
-				function setPriceTaxExcl(id)
-				{
-						var val = $(id).val() / (1 + getTaxValue());
-						$(id+"_excl").val(ps_round(val,6));
-				}
-
-				function setPriceInput(id)
-				{
-					setPriceTaxExcl(id); // init
-
-					$(id).keyup(function() {
-						setPriceTaxExcl(id);
-					})
-
-					$(id+"_excl").keyup(function() {
-						var val = $(id+"_excl").val() * (1 + getTaxValue());
-						$(id).val(ps_round(val, 2));
-					})
-				}
-
-				function getTaxValue()
-				{
-					selected_carrier = $("#id_carrier").val();
-					tax = carrier_taxes[selected_carrier];
-					return parseFloat(tax / 100);
-				}
-
-				$(document).ready(function(){
-					setPriceInput("#delimiter1");
-					setPriceInput("#delimiter2");
-				})
-				';
-
-		$html .= '</script>'."\n";
-
-		return $html;
-	}
 }
+
 
