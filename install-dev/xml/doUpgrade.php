685247c66305c0b0b46d55c0b7065099ae196f9e
mMarinetti <michael.marinetti@prestashop.com>
2011-07-15 15:11:01 +0000

    [-] Installer : fix error about version number (1.4.3.0 included when current version is 1.4.3)

--- install-dev/xml/doUpgrade.php
+++ install-dev/xml/doUpgrade.php
@@ -162,7 +162,8 @@ include_once(DEFINES_FILE);
 
 $oldversion = _PS_VERSION_;
 
-$versionCompare =  version_compare(INSTALL_VERSION, _PS_VERSION_);
+$versionCompare =  version_compare(INSTALL_VERSION, $oldversion);
+
 if ($versionCompare == '-1')
 {
 	$logger->logError('This installer is too old.');
@@ -204,9 +205,27 @@ if (empty($upgradeFiles))
 }
 natcasesort($upgradeFiles);
 $neededUpgradeFiles = array();
+
+// fix : complete version number if there is not all 4 numbers
+// for example replace 1.4.3 by 1.4.3.0
+// consequences : file 1.4.3.0.sql will be skipped if oldversion = 1.4.3
+// @since 1.4.4.0
+$arrayVersion = preg_split('#\.#', $oldversion);
+$versionNumbers = sizeof($arrayVersion);
+
+if ($versionNumbers != 4)
+	$arrayVersion = array_pad($arrayVersion, 4, '0');
+
+$oldversion = implode('.', $arrayVersion);
+// end of fix 
+
 foreach ($upgradeFiles AS $version)
-	if (version_compare($version, _PS_VERSION_) == 1 AND version_compare(INSTALL_VERSION, $version) != -1)
+{
+
+	if (version_compare($version, $oldversion) == 1 AND version_compare(INSTALL_VERSION, $version) != -1)
 		$neededUpgradeFiles[] = $version;
+}
+
 if (empty($neededUpgradeFiles))
 {
 	$logger->logError('No upgrade is possible.');
