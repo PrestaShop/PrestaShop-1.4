e8fbc6e8642cedb1ba21aececb190672ee03890a
mMarinetti <michael.marinetti@prestashop.com>
2011-07-29 14:49:48 +0000

    // next version will be 1.4.5.0

--- install-dev/model.php
+++ install-dev/model.php
@@ -44,7 +44,7 @@ if ($tmp = strpos($_SERVER['REQUEST_URI'], '?'))
 	$_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 0, $tmp);
 $_SERVER['REQUEST_URI'] = str_replace('//', '/', $_SERVER['REQUEST_URI']);
 
-define('INSTALL_VERSION', '1.4.4.0');
+define('INSTALL_VERSION', '1.4.5.0');
 define('INSTALL_PATH', dirname(__FILE__));
 define('PS_INSTALLATION_IN_PROGRESS', true);
 include_once(INSTALL_PATH.'/classes/ToolsInstall.php');
