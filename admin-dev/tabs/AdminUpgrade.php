cde77f7c10106fe09acbe4821f78b9fa6a86c3d6
mDeflotte <maxence.deflotte@prestashop.com>
2011-08-01 08:04:54 +0000

    [-] BO : Bugfix #PSCFI-2821 - Remove a french sentence

--- admin-dev/tabs/AdminUpgrade.php
+++ admin-dev/tabs/AdminUpgrade.php
@@ -1109,7 +1109,7 @@ class AdminUpgrade extends AdminPreferences
 			}
 			else if (!method_exists(get_class($this), 'ajaxProcess'.$action))
 			{
-				$this->nextDesc = sprintf($this->l('action "%1$s" non trouvée '), $action);
+				$this->nextDesc = sprintf($this->l('action "%1$s" not found'), $action);
 				$this->next = 'error';
 				$this->error = '1';
 			}
