cfd82dc96eef044397d0aca44ed37d814f3138d5
aFolletete <alain.folletete@prestashop.com>
2011-08-02 08:19:57 +0000

    // clean code

--- classes/StockMvt.php
+++ classes/StockMvt.php
@@ -42,10 +42,12 @@ class StockMvtCore extends ObjectModel
 	protected	$table = 'stock_mvt';
 	protected 	$identifier = 'id_stock_mvt';
 	
-
  	protected 	$fieldsRequired = array('id_product', 'id_stock_mvt_reason', 'quantity');
- 	protected 	$fieldsValidate = array('id_product' => 'isUnsignedId', 'id_product_attribute' => 'isUnsignedId','id_order' => 'isUnsignedId','id_employee' => 'isUnsignedId',
- 													'quantity' => 'isInt', 'id_stock_mvt_reason' => 'isUnsignedId');
+ 	protected 	$fieldsValidate = array(
+		'id_product' => 'isUnsignedId', 'id_product_attribute' => 'isUnsignedId', 
+		'id_order' => 'isUnsignedId','id_employee' => 'isUnsignedId',
+ 		'quantity' => 'isInt', 'id_stock_mvt_reason' => 'isUnsignedId'
+	);
 
 	protected	$webserviceParameters = array(
 		'objectsNodeName' => 'stock_movements',
@@ -58,8 +60,6 @@ class StockMvtCore extends ObjectModel
 			'id_stock_mvt_reason' => array('xlink_resource'=> 'stock_movement_reasons'),
 		),
 	);
-	
-	
 	
 	public function getFields()
 	{
@@ -79,51 +79,59 @@ class StockMvtCore extends ObjectModel
 	{
 		if (!parent::add($autodate, $nullValues))
 			return false;
+		
 		if (!$update_quantity)
 			return true;
 
 		if ($this->id_product_attribute)
 		{
-			$product = new Product((int)($this->id_product), false, Configuration::get('PS_LANG_DEFAULT'));
-			return (Db::getInstance()->Execute('UPDATE '._DB_PREFIX_.'product_attribute SET quantity=quantity+'.(int)$this->quantity.'
-															WHERE id_product='.(int)$product->id.' AND id_product_attribute='.(int)$this->id_product_attribute) AND $product->updateQuantityProductWithAttributeQuantity());
+			$product = new Product((int)$this->id_product, false, Configuration::get('PS_LANG_DEFAULT'));
+			return (Db::getInstance()->Execute('
+				UPDATE `'._DB_PREFIX_.'product_attribute` 
+				SET `quantity` = quantity+'.(int)$this->quantity.'
+				WHERE `id_product` = '.(int)$product->id.' 
+				AND `id_product_attribute` = '.(int)$this->id_product_attribute) 
+			&& $product->updateQuantityProductWithAttributeQuantity());
 		}
 		else
-			return Db::getInstance()->Execute('UPDATE '._DB_PREFIX_.'product SET quantity=quantity+'.(int)$this->quantity.' WHERE id_product='.(int)$this->id_product);
+			return Db::getInstance()->Execute('
+				UPDATE `'._DB_PREFIX_.'product` 
+				SET `quantity` = quantity+'.(int)$this->quantity.' 
+				WHERE `id_product` = '.(int)$this->id_product);
 	}
 	
 	public static function addMissingMvt($id_employee)
 	{
-		$products_without_attributes = Db::getInstance()->ExecuteS('SELECT p.id_product, pa.id_product_attribute, (p.quantity - SUM(IFNULL(sm.quantity, 0))) quantity
-																						FROM '._DB_PREFIX_.'product p
-																						LEFT JOIN '._DB_PREFIX_.'stock_mvt sm ON (sm.id_product = p.id_product)
-																						LEFT JOIN '._DB_PREFIX_.'product_attribute pa ON (pa.id_product = p.id_product)
-																						WHERE pa.id_product_attribute IS NULL
-																						GROUP BY p.id_product');
+		$products_without_attributes = Db::getInstance()->ExecuteS('
+			SELECT p.`id_product`, pa.`id_product_attribute`, (p.`quantity` - SUM(IFNULL(sm.`quantity`, 0))) quantity
+			FROM `'._DB_PREFIX_.'product` p
+			LEFT JOIN `'._DB_PREFIX_.'stock_mvt` sm ON (sm.`id_product` = p.`id_product`)
+			LEFT JOIN `'._DB_PREFIX_.'product_attribute` pa ON (pa.`id_product` = p.`id_product`)
+			WHERE pa.`id_product_attribute` IS NULL
+			GROUP BY p.`id_product`
+		');
 		
-		$products_with_attributes = Db::getInstance()->ExecuteS('SELECT p.id_product, pa.id_product_attribute, SUM(pa.quantity) - SUM(IFNULL(sm.quantity, 0)) quantity
-																						FROM '._DB_PREFIX_.'product p
-																						LEFT JOIN '._DB_PREFIX_.'product_attribute pa ON (pa.id_product = p.id_product)
-																						LEFT JOIN '._DB_PREFIX_.'stock_mvt sm ON (sm.id_product = pa.id_product AND sm.id_product_attribute = pa.id_product_attribute)
-																						WHERE pa.id_product_attribute IS NOT NULL
-																						GROUP BY pa.id_product_attribute');
-
-		
+		$products_with_attributes = Db::getInstance()->ExecuteS('
+			SELECT p.`id_product`, pa.`id_product_attribute`, SUM(pa.`quantity`) - SUM(IFNULL(sm.`quantity`, 0)) quantity
+			FROM `'._DB_PREFIX_.'product` p
+			LEFT JOIN `'._DB_PREFIX_.'product_attribute` pa ON (pa.`id_product` = p.`id_product`)
+			LEFT JOIN `'._DB_PREFIX_.'stock_mvt` sm ON (sm.`id_product` = pa.`id_product` AND sm.`id_product_attribute` = pa.`id_product_attribute`)
+			WHERE pa.`id_product_attribute` IS NOT NULL
+			GROUP BY pa.`id_product_attribute`
+		');
 
 		$products = array_merge($products_without_attributes, $products_with_attributes);
 		if ($products)
-		{
 			foreach ($products AS $product)
 			{
 				if (!$product['quantity'])
 					continue;
-				$mvt = new self();
+				$mvt = new StockMvt();
 				foreach ($product AS $k => $row)
 					$mvt->{$k} = $row;
 				$mvt->id_employee = (int)$id_employee;
 				$mvt->id_stock_mvt_reason = _STOCK_MOVEMENT_MISSING_REASON_;
 				$mvt->add(true, false, false);
 			}
-		}
 	}	
 }
