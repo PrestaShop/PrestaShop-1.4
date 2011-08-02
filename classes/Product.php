f9ae4973e42d965d17cea34f029ffd39bdd24309
aFolletete <alain.folletete@prestashop.com>
2011-08-02 16:39:05 +0000

    [-] BO : fix bug #PSCFI-2814 - If we add a new product in "this category" ==> result always Home in product

--- classes/Product.php
+++ classes/Product.php
@@ -1567,7 +1567,8 @@ class ProductCore extends ObjectModel
 		$row = Db::getInstance(_PS_USE_SQL_SLAVE_)->ExecuteS('
 		SELECT cp.`id_category`, cl.`name`, cl.`link_rewrite` FROM `'._DB_PREFIX_.'category_product` cp
 		LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (cp.`id_category` = cl.`id_category`)
-		WHERE cp.`id_product` = '.(int)$id_product);
+		WHERE cp.`id_product` = '.(int)$id_product.'
+		AND cl.`id_lang` = '.(int)$id_lang);
 		foreach ($row as $val)
 			$ret[$val['id_category']] = $val;
 		return $ret;
