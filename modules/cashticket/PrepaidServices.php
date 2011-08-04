fc496e394301cf813555c81b31a77433cb800b05
vSchoener <vincent.schoener@prestashop.com>
2011-08-04 13:59:00 +0000

    [-] MO : CashTicket. configuration verify if an existing certficate exist and add error messages

--- modules/cashticket/PrepaidServices.php
+++ modules/cashticket/PrepaidServices.php
@@ -298,6 +298,14 @@ abstract class PrepaidServices extends PaymentModule
 
 		foreach ($this->_getAllowedCurrencies() AS $currency)
 		{
+			$mid = Configuration::get($this->prefix.'MERCHANT_ID_'.$currency['iso_code']);
+			$certificateExiste = '';
+			$passwordExist = '';
+			if (file_exists($this->certificat_dir.$mid.'.pem'))
+				$certificateExiste = '<div style="color:#00b511; text-align:center;">'.$this->l('A certificate has been found for this configuration').' : '.$mid.'.pem'.'</div><br />';
+			if (Configuration::get($this->prefix.'KEYRING_PW_'.$currency['iso_code']))
+				$passwordExist = '<div style="color:#00b511; text-align:center;">'.$this->l('A password has already been saved');
+		
 			$currencies_configuration .= '
 			<tr>
 				<td class="currency_label">'.$this->getL('configuration_in').' '.$currency['name'].' '.$currency['sign'].'</td>
@@ -311,10 +319,12 @@ abstract class PrepaidServices extends PaymentModule
 					<div class="margin-form">
 						<input type="file" name="ct_keyring_certificate_'.$currency['iso_code'].'" />
 					</div>
+					'.$certificateExiste.'
 					<label>'.$this->getL('keyring_pw').'</label>
 					<div class="margin-form">
-						<input type="text" name="ct_keyring_pw_'.$currency['iso_code'].'" value="'.Tools::htmlentitiesUTF8(Configuration::get($this->prefix.'KEYRING_PW_'.$currency['iso_code'])).'"/>
+						<input type="password" name="ct_keyring_pw_'.$currency['iso_code'].'" value=""/>
 					</div>
+					'.$passwordExist.'
 				</td>
 			</tr>';
 		}
@@ -513,6 +523,11 @@ abstract class PrepaidServices extends PaymentModule
 			$mid = trim(Tools::getValue('ct_merchant_id_'.$currency['iso_code']));
 
 			Configuration::updateValue($this->prefix.'MERCHANT_ID_'.$currency['iso_code'], $mid);
+			
+			$pass = Tools::getValue('ct_keyring_pw_'.$currency['iso_code']);
+			if (!empty($pass))
+				Configuration::updateValue($this->prefix.'KEYRING_PW_'.$currency['iso_code'], Tools::getValue('ct_keyring_pw_'.$currency['iso_code']));
+
 			Configuration::updateValue($this->prefix.'KEYRING_PW_'.$currency['iso_code'], Tools::getValue('ct_keyring_pw_'.$currency['iso_code']));
 
 			if (isset($_FILES['ct_keyring_certificate_'.$currency['iso_code']]))
