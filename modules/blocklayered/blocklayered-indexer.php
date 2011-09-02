66223d5619c58e35f9006cba9d69e2b15beb08a5
mDeflotte <maxence.deflotte@prestashop.com>
2011-09-02 12:56:15 +0000

    // MO : Block layered - Remove unusefull comments

--- modules/blocklayered/blocklayered-indexer.php
+++ modules/blocklayered/blocklayered-indexer.php
@@ -4,9 +4,6 @@ include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../init.php');
 include(dirname(__FILE__).'/blocklayered.php');
 
-// Token : 3474f99fd9
-// Url test: http://prestashop.localhost/modules/blocklayered/blocklayered-indexer.php?token=3474f99fd9
-
 if (substr(Tools::encrypt('blocklayered/index'),0,10) != Tools::getValue('token') || !Module::isInstalled('blocklayered'))
 	die;
 
