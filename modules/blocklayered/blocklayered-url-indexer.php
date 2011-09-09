9131bd632de2c5e369db1033a12da3f3a1368216
mDeflotte <maxence.deflotte@prestashop.com>
2011-09-09 12:57:37 +0000

    // MO : Blocklayered - SEO : url indexing

---
+++ modules/blocklayered/blocklayered-url-indexer.php
@@ -0,0 +1,10 @@
+<?php
+
+include(dirname(__FILE__).'/../../config/config.inc.php');
+include(dirname(__FILE__).'/../../init.php');
+include(dirname(__FILE__).'/blocklayered.php');
+
+//if (substr(Tools::encrypt('blocklayered/index'),0,10) != Tools::getValue('token') || !Module::isInstalled('blocklayered'))
+//	die;
+
+BlockLayered::indexUrl();
