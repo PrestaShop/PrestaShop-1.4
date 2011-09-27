397bae946d4fd5783bf7d3e442863c3ff814c655
vSchoener <vincent.schoener@prestashop.com>
2011-09-27 12:29:50 +0000

    // change MR version

--- modules/mondialrelay/mondialrelay.php
+++ modules/mondialrelay/mondialrelay.php
@@ -59,7 +59,7 @@ class MondialRelay extends Module
 	{
 		$this->name		= 'mondialrelay';
 		$this->tab		= 'shipping_logistics';
-		$this->version	= '1.7.2';
+		$this->version	= '1.7.3';
 
 		parent::__construct();
 
