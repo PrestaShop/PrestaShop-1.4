a7fca7026110720d4d6f05139f3f0fd7b0bd639f
aNiassy <augustin.niassy@prestashop.com>
2011-10-24 14:23:16 +0000

    [-] BO #PSCFI-3878 : fixed a bug of position calculation for tabs

--- classes/Tab.php
+++ classes/Tab.php
@@ -244,7 +244,7 @@ class TabCore extends ObjectModel
 	 */
 	public static function getNewLastPosition($id_parent)
 	{
-		return (Db::getInstance()->getValue('SELECT MAX(position)+1 FROM `'._DB_PREFIX_.'tab` WHERE `id_parent` = '.(int)($id_parent)));
+		return (Db::getInstance()->getValue('SELECT IFNULL(MAX(position),0)+1 FROM `'._DB_PREFIX_.'tab` WHERE `id_parent` = '.(int)($id_parent)));
 	}
 
 	public function move($direction)
