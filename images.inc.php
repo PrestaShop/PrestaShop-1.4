d6123066691765d133a3887c432908dffab67aef
jObregon <jorge.obregon@prestashop.com>
2011-11-04 17:18:27 +0000

    // Fixed to read extensions .jpeg and .jpg

--- images.inc.php
+++ images.inc.php
@@ -251,6 +251,7 @@ function imageResize($sourceFile, $destFile, $destWidth = NULL, $destHeight = NU
 	}
 	
 	imagecopyresampled($destImage, $sourceImage, (int)(($destWidth - $nextWidth) / 2), (int)(($destHeight - $nextHeight) / 2), 0, 0, $nextWidth, $nextHeight, $sourceWidth, $sourceHeight);
+
 	return (returnDestImage($fileType, $destImage, $destFile));
 }
 
@@ -325,7 +326,8 @@ function returnDestImage($type, $ressource, $filename)
 		case 'png':
 			$quality = (Configuration::get('PS_PNG_QUALITY') === false ? 7 : Configuration::get('PS_PNG_QUALITY'));
 			$flag = imagepng($ressource, $filename, (int)$quality);
-			break;
+			break;		
+		case 'jpg':
 		case 'jpeg':
 		default:
 			$quality = (Configuration::get('PS_JPEG_QUALITY') === false ? 90 : Configuration::get('PS_JPEG_QUALITY'));
