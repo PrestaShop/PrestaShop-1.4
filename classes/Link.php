ed8de5564caa50335a5bf3771b7944782c2f1040
aNiassy <augustin.niassy@prestashop.com>
2011-11-14 16:22:33 +0000

    [-] FO : #PSCFI-4041 : fixed a problem of treatment of datas formet in Link::getProductLink()

--- classes/Link.php
+++ classes/Link.php
@@ -59,18 +59,70 @@ class LinkCore
 	/**
 	  * Return the correct link for product/category/supplier/manufacturer
 	  *
-	  * @param mixed $id_OBJ Can be either the object or the ID only
+	  * @param mixed $id_product Can be either the object or the ID only
 	  * @param string $alias Friendly URL (only if $id_OBJ is the object)
 	  * @return string link
 	  */
 	public function getProductLink($id_product, $alias = NULL, $category = NULL, $ean13 = NULL, $id_lang = NULL)
 	{
+		global $cookie;
 		if (is_object($id_product))
-			return ($this->allow == 1)?(_PS_BASE_URL_.__PS_BASE_URI__.$this->getLangLink((int)$id_lang).((isset($id_product->category) AND !empty($id_product->category) AND $id_product->category != 'home') ? $id_product->category.'/' : '').(int)$id_product->id.'-'.$id_product->link_rewrite.($id_product->ean13 ? '-'.$id_product->ean13 : '').'.html') :
-			(_PS_BASE_URL_.__PS_BASE_URI__.'product.php?id_product='.(int)$id_product->id);
-		elseif ($alias)
-			return ($this->allow == 1)?(_PS_BASE_URL_.__PS_BASE_URI__.$this->getLangLink((int)$id_lang).(($category AND $category != 'home') ? ($category.'/') : '').(int)$id_product.'-'.$alias.($ean13 ? '-'.$ean13 : '').'.html') :
-			(_PS_BASE_URL_.__PS_BASE_URI__.'product.php?id_product='.(int)$id_product);
+		{		
+			$link = '';
+			if ($this->allow == 1)
+			{
+				$link .= (_PS_BASE_URL_.__PS_BASE_URI__.$this->getLangLink((int)$id_lang));
+				
+				if (isset($id_product->category) AND !empty($id_product->category) AND $id_product->category != 'home')
+					$link .= $id_product->category.'/';
+				else
+					$link .= '';
+
+				$link .= (int)$id_product->id.'-';
+				if (is_array($id_product->link_rewrite))
+					$id_product->link_rewrite[(int)$cookie->id_lang];
+				else 
+				 	$id_product->link_rewrite;
+									if ($id_product->ean13)
+					$link .='-'.$id_product->ean13;
+				else
+					$link .= '';
+
+				$link .= 'html';
+			}
+			else 
+			{
+				$link .= (_PS_BASE_URL_.__PS_BASE_URI__.'product.php?id_product='.(int)$id_product->id);
+			}
+			return $link;
+		}
+		
+		else if ($alias)
+		{
+			$link = '';
+			if ($this->allow == 1)
+			{
+				$link .= (_PS_BASE_URL_.__PS_BASE_URI__.$this->getLangLink((int)$id_lang));
+				
+				if ($category AND $category != 'home')
+					$string .= $category.'/';
+				else 
+				 	$string .= '';
+				 
+				$string .= (int)$id_product.'-'.$alias;
+				
+				if ($ean13) 
+					$string .='-'.$ean13;
+				else 
+					$string .= '';
+			
+				$string .= '.html';
+			}
+			else
+				$string .=(_PS_BASE_URL_.__PS_BASE_URI__.'product.php?id_product='.(int)$id_product);
+			return $string;
+		}
+		
 		else
 			return _PS_BASE_URL_.__PS_BASE_URI__.'product.php?id_product='.(int)$id_product;
 	}
