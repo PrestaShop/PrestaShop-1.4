a14d3018ec0761db5f1220dbc28be07e0dd02514
tDidierjean <thomas.didierjean@prestashop.com>
2011-12-05 09:14:22 +0000

    [-] Classes : fixed bug #PSCFI-4352 - remove unused parameter added in previous commit

--- classes/Validate.php
+++ classes/Validate.php
@@ -43,7 +43,7 @@ class ValidateCore
 	* @param string $email e-mail address to validate
 	* @return boolean Validity is ok or not
 	*/
-	public static function isEmail($email, $required = true)
+	public static function isEmail($email)
 	{
 		return !empty($email) AND preg_match('/^[a-z0-9!#$%&\'*+\/=?^`{}|~_-]+[.a-z0-9!#$%&\'*+\/=?^`{}|~_-]*@[a-z0-9]+[._a-z0-9-]*\.[a-z0-9]+$/ui', $email);
 	}
