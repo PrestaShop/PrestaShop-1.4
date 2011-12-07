3facc3eb55ad53de1f350599b681b3ff54502a7f
fGaillard <francois.gaillard@prestashop.com>
2011-12-07 17:02:29 +0000

    [-] Installer : #PSCFI-4383 : BugFix SQL installation script corrected, unique key for specific_price_priority

--- install-dev/sql/db.sql
+++ install-dev/sql/db.sql
@@ -1620,7 +1620,8 @@ CREATE TABLE `PREFIX_specific_price_priority` (
 	`id_specific_price_priority` INT NOT NULL AUTO_INCREMENT ,
 	`id_product` INT NOT NULL ,
 	`priority` VARCHAR( 80 ) NOT NULL ,
-	PRIMARY KEY ( `id_specific_price_priority` , `id_product` )
+	PRIMARY KEY ( `id_specific_price_priority` , `id_product` ),
+	UNIQUE KEY `id_produc` (`id_product`)
 )  ENGINE=ENGINE_TYPE  DEFAULT CHARSET=utf8;
 
 CREATE TABLE `PREFIX_log` (
