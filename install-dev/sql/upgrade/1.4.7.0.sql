4d9b373490386a3ff204cb4c9a582f3aa09b76e2
mDeflotte <maxence.deflotte@prestashop.com>
2011-12-12 17:03:20 +0000

    // BugFix : Adding order_tax table on upgrading

--- install-dev/sql/upgrade/1.4.7.0.sql
+++ install-dev/sql/upgrade/1.4.7.0.sql
@@ -2,3 +2,11 @@ SET NAMES 'utf8';
 
 ALTER TABLE `PREFIX_category_product` DROP INDEX `category_product_index`;
 ALTER TABLE `PREFIX_category_product` ADD UNIQUE `category_product_index` (`id_category`,`id_product`);
+
+
+CREATE TABLE IF NOT EXISTS `PREFIX_order_tax` (
+  `id_order` int(11) NOT NULL,
+  `tax_name` varchar(40) NOT NULL,
+  `tax_rate` decimal(6,3) NOT NULL,
+  `amount` decimal(20,6) NOT NULL
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
