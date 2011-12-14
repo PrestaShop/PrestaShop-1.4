c9517ad613041d468a660cf604f1394197405229
aKorczak <anatole.korczak@prestashop.com>
2011-12-14 18:02:09 +0000

    [-] WS : Compatibility for PHP as CGI

--- webservice/dispatcher.php
+++ webservice/dispatcher.php
@@ -25,6 +25,21 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
+//set http auth headers for apache+php-cgi work around
+if (isset($_SERVER['HTTP_AUTHORIZATION']) && preg_match('/Basic\s+(.*)$/i', $_SERVER['HTTP_AUTHORIZATION'], $matches))
+{
+	list($name, $password) = explode(':', base64_decode($matches[1]));
+	$_SERVER['PHP_AUTH_USER'] = strip_tags($name);
+}
+
+//set http auth headers for apache+php-cgi work around if variable gets renamed by apache
+if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION']) && preg_match('/Basic\s+(.*)$/i', $_SERVER['REDIRECT_HTTP_AUTHORIZATION'], $matches))
+{
+	list($name, $password) = explode(':', base64_decode($matches[1]));
+	$_SERVER['PHP_AUTH_USER'] = strip_tags($name);
+}
+
+
 ob_start();
 
 require_once(dirname(__FILE__).'/../config/config.inc.php');
