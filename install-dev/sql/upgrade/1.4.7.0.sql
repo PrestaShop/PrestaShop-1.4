702e53c8d6384e6b51dafde871eecabbd47407a0
fGaillard <francois.gaillard@prestashop.com>
2012-01-11 15:29:14 +0000

    [-] MO : #PSCFI-3917 : BugFix for sql upgrade file, id module was incorrect

--- install-dev/sql/upgrade/1.4.7.0.sql
+++ install-dev/sql/upgrade/1.4.7.0.sql
@@ -13,7 +13,11 @@ CREATE TABLE IF NOT EXISTS `PREFIX_order_tax` (
 
 INSERT INTO `PREFIX_hook` (`name`, `title`, `description`, `position`, `live_edit`) VALUES
 ('frontCanonicalRedirect', 'Front Canonical Redirect', 'Check for 404 errors before canonical redirects', 0, 0);
-INSERT INTO `PREFIX_hook_module` (`id_module`, `id_hook`, `position`) (SELECT id_module, 95, (SELECT max_position from (SELECT MAX(position)+1 as max_position FROM `PREFIX_hook_module` WHERE `id_hook` = 95) tmp) FROM `PREFIX_module` WHERE `name` = 'pagesnotfound');
+
+SET @id_hook = (SELECT `id_hook` FROM `PREFIX_hook` WHERE `name` = 'frontCanonicalRedirect');
+SET @position = (SELECT IFNULL(MAX(`position`),0)+1 FROM `PREFIX_hook_module` WHERE `id_hook` = @id_hook);
+SET @id_module = (SELECT `id_module` FROM `PREFIX_module` WHERE `name` = 'pagesnotfound');
+
+INSERT INTO `PREFIX_hook_module` (`id_hook`, `id_module`, `position`) VALUES (@id_hook, @id_module, @position);
 
 ALTER TABLE `PREFIX_order_state` ADD COLUMN `deleted` tinyint(1) UNSIGNED NOT NULL default '0' AFTER `delivery`;
-
