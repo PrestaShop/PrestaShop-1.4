7a876f4fe0da702e47fe496c02cf56e2ff9511e0
fBrignoli <franck.brignoli@prestashop.com>
2012-02-07 15:10:25 +0000

    [-] MO : Google Sitemap - duplicate pictures on combination

--- modules/gsitemap/gsitemap.php
+++ modules/gsitemap/gsitemap.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* 2007-2011 PrestaShop 
+* 2007-2011 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -37,10 +37,10 @@ class Gsitemap extends Module
 	{
 		$this->name = 'gsitemap';
 		$this->tab = 'seo';
-		$this->version = '1.7';
+		$this->version = '1.8';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
-		
+
 		parent::__construct();
 
 		$this->displayName = $this->l('Google sitemap');
@@ -55,7 +55,7 @@ class Gsitemap extends Module
 		file_put_contents(GSITEMAP_FILE, '');
 		return parent::uninstall();
 	}
-	
+
 	private function _postValidation()
 	{
 		file_put_contents(GSITEMAP_FILE, '');
@@ -64,7 +64,7 @@ class Gsitemap extends Module
 		else
 			fclose($fp);
 	}
-	
+
 	private function getUrlWith($url, $key, $value)
 	{
 		if (empty($value))
@@ -80,14 +80,14 @@ class Gsitemap extends Module
 		Configuration::updateValue('GSITEMAP_ALL_PRODUCTS', (int)Tools::getValue('GSITEMAP_ALL_PRODUCTS'));
 		$link = new Link();
 		$langs = Language::getLanguages();
-				
+
 		$xmlString = <<<XML
 <?xml version="1.0" encoding="UTF-8" ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
 xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 </urlset>
 XML;
-		
+
 		$xml = new SimpleXMLElement($xmlString);
 
 		if (Configuration::get('PS_REWRITING_SETTINGS') && count($langs) > 1)
@@ -116,19 +116,24 @@ XML;
 
 		$tmp = null;
 		$res = null;
+		$done = null;
 		foreach ($products as $product)
 		{
-			if ($tmp == $product['id_product'])
-				$res[$tmp]['images'] []= array('id_image' => $product['id_image'], 'legend_image' => $product['legend_image']);
-			else
+			if (!isset($done[$product['id_image']]))
 			{
-				$tmp = $product['id_product'];
-				$res[$tmp] = $product;
-				unset($res[$tmp]['id_image'], $res[$tmp]['legend_image']);
-				$res[$tmp]['images'] []= array('id_image' => $product['id_image'], 'legend_image' => $product['legend_image']);
+				if ($tmp == $product['id_product'])
+				   $res[$tmp]['images'] []= array('id_image' => $product['id_image'], 'legend_image' => $product['legend_image']);
+				else
+				{
+					$tmp = $product['id_product'];
+					$res[$tmp] = $product;
+					unset($res[$tmp]['id_image'], $res[$tmp]['legend_image']);
+					$res[$tmp]['images'] []= array('id_image' => $product['id_image'], 'legend_image' => $product['legend_image']);
+				}
+				$done[$product['id_image']] = true;
 			}
 		}
-		
+
 		foreach ($res as $product)
 		{
 			if (($priority = 0.7 - ($product['level_depth'] / 10)) < 0.1)
@@ -138,7 +143,7 @@ XML;
 			$sitemap = $this->_addSitemapNode($xml, htmlspecialchars($tmpLink), $priority, 'weekly', substr($product['date_upd'], 0, 10));
 			$sitemap = $this->_addSitemapNodeImage($sitemap, $product);
 		}
-		
+
 		/* Categories Generator */
 		if (Configuration::get('PS_REWRITING_SETTINGS'))
 			$categories = Db::getInstance()->ExecuteS('
@@ -151,19 +156,19 @@ XML;
 		else
 			$categories = Db::getInstance()->ExecuteS(
 			'SELECT c.id_category, c.level_depth, DATE_FORMAT(IF(date_upd,date_upd,date_add), \'%Y-%m-%d\') AS date_upd
-			FROM '._DB_PREFIX_.'category c 
-			ORDER BY c.id_category ASC');			
+			FROM '._DB_PREFIX_.'category c
+			ORDER BY c.id_category ASC');
 
-		
+
 		foreach($categories as $category)
 		{
 			if (($priority = 0.9 - ($category['level_depth'] / 10)) < 0.1)
 				$priority = 0.1;
-			
-			$tmpLink = Configuration::get('PS_REWRITING_SETTINGS') ? $link->getCategoryLink((int)$category['id_category'], $category['link_rewrite'], (int)$category['id_lang']) : $link->getCategoryLink((int)$category['id_category']);	
+
+			$tmpLink = Configuration::get('PS_REWRITING_SETTINGS') ? $link->getCategoryLink((int)$category['id_category'], $category['link_rewrite'], (int)$category['id_lang']) : $link->getCategoryLink((int)$category['id_category']);
 			$this->_addSitemapNode($xml, htmlspecialchars($tmpLink), $priority, 'weekly', substr($category['date_upd'], 0, 10));
 		}
-		
+
 		/* CMS Generator */
 		if (Configuration::get('GSITEMAP_ALL_CMS') || !Module::isInstalled('blockcms'))
 			$sql_cms = '
@@ -180,14 +185,14 @@ XML;
 			LEFT JOIN '._DB_PREFIX_.'lang l ON (cl.id_lang = l.id_lang)
 			WHERE l.`active` = 1
 			ORDER BY cl.id_cms, cl.id_lang ASC';
-		
+
 		$cmss = Db::getInstance(_PS_USE_SQL_SLAVE_)->ExecuteS($sql_cms);
 		foreach($cmss as $cms)
 		{
 			$tmpLink = Configuration::get('PS_REWRITING_SETTINGS') ? $link->getCMSLink((int)$cms['id_cms'], $cms['link_rewrite'], false, (int)$cms['id_lang']) : $link->getCMSLink((int)$cms['id_cms']);
-			$this->_addSitemapNode($xml, $tmpLink, '0.8', 'daily');				
+			$this->_addSitemapNode($xml, $tmpLink, '0.8', 'daily');
 		}
-		
+
 		/* Add classic pages (contact, best sales, new products...) */
 		$pages = array(
 			'supplier' => false,
@@ -198,17 +203,17 @@ XML;
 			'authentication' => true,
 			'best-sales' => false,
 			'contact-form' => true);
-		
+
 		// Don't show suppliers and manufacturers if they are disallowed
 		if (!Module::getInstanceByName('blockmanufacturer')->id && !Configuration::get('PS_DISPLAY_SUPPLIERS'))
 			unset($pages['manufacturer']);
-			
+
 		if (!Module::getInstanceByName('blocksupplier')->id && !Configuration::get('PS_DISPLAY_SUPPLIERS'))
 			unset($pages['supplier']);
 
 		// Generate nodes for pages
-		if (Configuration::get('PS_REWRITING_SETTINGS'))		
-			foreach ($pages as $page => $ssl)		
+		if (Configuration::get('PS_REWRITING_SETTINGS'))
+			foreach ($pages as $page => $ssl)
 				foreach($langs as $lang)
 					$this->_addSitemapNode($xml, $link->getPageLink($page.'.php', $ssl, $lang['id_lang']), '0.5', 'monthly');
 		else
@@ -216,7 +221,7 @@ XML;
 				$this->_addSitemapNode($xml, $link->getPageLink($page.'.php', $ssl), '0.5', 'monthly');
 
 		$xmlString = $xml->asXML();
-		
+
 		$fp = fopen(GSITEMAP_FILE, 'w');
 		fwrite($fp, $xmlString);
 		fclose($fp);
@@ -226,9 +231,9 @@ XML;
 		$this->_html .= $res ? $this->l('Sitemap file generated') : $this->l('Error while creating sitemap file');
 		$this->_html .= '</h3>';
 	}
-	
+
 	private function _addSitemapNode($xml, $loc, $priority, $change_freq, $last_mod = NULL)
-	{		
+	{
 		$sitemap = $xml->addChild('url');
 		$sitemap->addChild('loc', $loc);
 		$sitemap->addChild('priority', number_format($priority,1,'.',''));
@@ -237,7 +242,7 @@ XML;
 		$sitemap->addChild('changefreq', $change_freq);
 		return $sitemap;
 	}
-	
+
 	private function _addSitemapNodeImage($xml, $product)
 	{
 		foreach ($product['images'] as $img)
@@ -245,7 +250,7 @@ XML;
 			$link = new Link();
 			$image = $xml->addChild('image', null, 'http://www.google.com/schemas/sitemap-image/1.1');
 			$image->addChild('loc', $link->getImageLink($product['link_rewrite'], (int)$product['id_product'].'-'.(int)$img['id_image']), 'http://www.google.com/schemas/sitemap-image/1.1');
-			
+
 			$legend_image = preg_replace('/(&+)/i', '&amp;', $img['legend_image']);
 			$image->addChild('caption', $legend_image, 'http://www.google.com/schemas/sitemap-image/1.1');
 			$image->addChild('title', $legend_image, 'http://www.google.com/schemas/sitemap-image/1.1');
@@ -260,7 +265,7 @@ XML;
 			$fstat = fstat($fp);
 			fclose($fp);
 			$xml = simplexml_load_file(GSITEMAP_FILE);
-			
+
 			$nbPages = count($xml->url);
 
 			$this->_html .= '<p>'.$this->l('Your Google sitemap file is online at the following address:').'<br />
@@ -286,7 +291,7 @@ XML;
 			value="'.((!file_exists(GSITEMAP_FILE)) ? $this->l('Generate sitemap file') : $this->l('Update sitemap file')).'" />
 		</form>';
 	}
-	
+
 	public function getContent()
 	{
 		$this->_html .= '<h2>'.$this->l('Search Engine Optimization').'</h2>
@@ -308,3 +313,4 @@ XML;
 		return $this->_html;
 	}
 }
+
