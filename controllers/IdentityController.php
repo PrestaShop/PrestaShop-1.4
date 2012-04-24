5c2cd9e103761b5071d8d866b5ac87a7f17fb184
rGaillard <remi.gaillard@prestashop.com>
2012-04-24 14:21:26 +0000

    // Small fix

--- controllers/IdentityController.php
+++ controllers/IdentityController.php
@@ -38,26 +38,6 @@ class IdentityControllerCore extends FrontController
 		
 		$customer = new Customer((int)(self::$cookie->id_customer));
 
-		if (sizeof($_POST))
-		{
-			$exclusion = array('secure_key', 
-									'old_passwd', 
-									'passwd', 
-									'active', 
-									'date_add', 
-									'date_upd', 
-									'last_passwd_gen', 
-									'newsletter_date_add', 
-									'id_default_group', 
-									'ip_registration_newsletter',
-									'note',
-									'is_guest');
-			$fields = $customer->getFields();
-			foreach ($fields AS $key => $value)
-				if (!in_array($key, $exclusion))
-					$customer->{$key} = key_exists($key, $_POST) ? trim($_POST[$key]) : 0;
-		}
-
 		if (isset($_POST['years']) AND isset($_POST['months']) AND isset($_POST['days']))
 			$customer->birthday = (int)($_POST['years']).'-'.(int)($_POST['months']).'-'.(int)($_POST['days']);
 
@@ -81,7 +61,7 @@ class IdentityControllerCore extends FrontController
 				else
 				{
 					$prev_id_default_group = $customer->id_default_group;
-					$this->errors = array_merge($this->errors, $customer->validateControler());
+					$this->errors = $customer->validateControler(true, true);
 				}
 				if (!sizeof($this->errors))
 				{
