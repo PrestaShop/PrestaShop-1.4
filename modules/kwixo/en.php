33338c716e8dc94b58c1017b265ab3dc1eb0903b
lLefevre <loic.lefevre@prestashop.com>
2012-05-14 09:25:26 +0000

    [*] TR : Added translations english

--- modules/kwixo/en.php
+++ modules/kwixo/en.php
@@ -2,9 +2,3 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{receiveandpay}default>payment_return_88d9cab9eb73f3a87af2adf46541ec38'] = 'has been successfully registered.';
-$_MODULE['<{receiveandpay}default>payment_return_5293c99f20a27d590f31d4f6956961cb'] = 'For any questions or for further information, please contact our';
-$_MODULE['<{receiveandpay}default>payment_return_e04bb9b1574088efc2c94407c87a00d4'] = 'We noticed a problem with your order. If you think this is an error, you can contact our';
-$_MODULE['<{receiveandpay}default>receiveandpay_d7f3568a9f953c1e988eccca865b998e'] = 'Accepts payments by \"ReceiveAndPay\"';
-
-?>
