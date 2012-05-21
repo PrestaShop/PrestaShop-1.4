69f40b0b68240a99e2cb7760b94958f39650c0cb
vSchoener <vincent.schoener@prestashop.com>
2012-05-21 17:09:06 +0000

    [-] MO : Paysafecard, Fix the display of the template (merge from 1.5)

--- modules/paysafecard/redirect.php
+++ modules/paysafecard/redirect.php
@@ -30,11 +30,47 @@ include(dirname(__FILE__).'/paysafecard.php');
 
 $module = new PaysafeCard();
 
+// Use for 1.4 / 1,5 to display the front
+class PSC_Display extends FrontController
+{
+	public $module;
+
+	// Assign template, on 1.4 create it else assign for 1.5
+	public function setTemplate($template)
+	{
+		if (_PS_VERSION_ >= '1.5')
+			parent::setTemplate($template);
+		else
+			$this->template = $template;
+	}
+
+	// Need module to get the error message
+	public function __construct($module)
+	{
+		parent::__construct();
+
+		$this->module = $module;
+	}
+
+	// Overload displayContent for 1.4
+	public function displayContent()
+	{
+		echo Context::getContext()->smarty->fetch($this->template);
+	}
+
+	// Assign smarty content
+	public function displayTemplate()
+	{
+		$this->setTemplate(dirname(__FILE__).'/redirect.tpl');
+		Context::getContext()->smarty->assign('psc_error_message', $this->module->getL('cant_create_dispo'));
+	}
+}
+
 $url = (_PS_VERSION_ < '1.5') ? 'order.php?step=3' : 'index.php?controller=order&step=3';
 
 if (!$cart->id OR $cart->id_customer == 0 OR $cart->id_address_delivery == 0 OR $cart->id_address_invoice == 0 OR !$module->active)
 	Tools::redirect($url);
-	
+
 $currency = new Currency($cart->id_currency);
 if (!$module->isCurrencyActive($currency->iso_code))
 	Tools::redirect($url);
@@ -43,10 +79,9 @@ $result = $module->createDisposition($cart);
 
 if ($result['return_code'] != 0)
 {
-	include(dirname(__FILE__).'/../../header.php');
-	echo $module->getL('cant_create_dispo');
-	include(dirname(__FILE__).'/../../footer.php');
+	$controller = new PSC_Display($module);
+	$controller->displayTemplate();
+	$controller->run();
 }
 else
 	Tools::redirectLink($result['message']);
-
