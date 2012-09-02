e0244065b40c7aa83549981f72c1b8af63286567
gCharmes <guillaume.charmes@prestashop.com>
2012-09-02 01:16:47 +0000

    [-] FO : Added missing header in .js files

--- themes/prestashop/js/tools/treeManagement.js
+++ themes/prestashop/js/tools/treeManagement.js
@@ -1,3 +1,29 @@
+/*
+* 2007-2012 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2012 PrestaShop SA
+*  @version  Release: $Revision$
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
 //animate the opening of the branch (span.grower jQueryElement)
 function openBranch(jQueryElement, noAnimation) {
 		jQueryElement.addClass('OPEN').removeClass('CLOSE');
@@ -30,21 +56,21 @@ $(document).ready(function () {
 	{
 		//add growers to each ul.tree elements
 		$('ul.tree.dhtml ul').prev().before("<span class='grower OPEN'> </span>");
-		
+
 		//dynamically add the '.last' class on each last item of a branch
 		$('ul.tree.dhtml ul li:last-child, ul.tree.dhtml li:last-child').addClass('last');
-		
+
 		//collapse every expanded branch
 		$('ul.tree.dhtml span.grower.OPEN').addClass('CLOSE').removeClass('OPEN').parent().find('ul:first').hide();
 		$('ul.tree.dhtml').show();
-		
+
 		//open the tree for the selected branch
 			$('ul.tree.dhtml .selected').parents().each( function() {
 				if ($(this).is('ul'))
 					toggleBranch($(this).prev().prev(), true);
 			});
 			toggleBranch( $('ul.tree.dhtml .selected').prev(), true);
-		
+
 		//add a fonction on clicks on growers
 		$('ul.tree.dhtml span.grower').click(function(){
 			toggleBranch($(this));
