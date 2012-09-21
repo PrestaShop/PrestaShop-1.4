3d4a69fbf0666d05fe1b0f20c3db505beecadeaa
fGaillard <francois.gaillard@prestashop.com>
2012-09-21 16:17:27 +0000

    // Hook id fixed in upgrade script

--- install-dev/sql/upgrade/1.4.9.1.sql
+++ install-dev/sql/upgrade/1.4.9.1.sql
@@ -10,4 +10,4 @@ INSERT INTO `PREFIX_hook_module` (`id_module`, `id_hook` , `position`)
 INSERT INTO `PREFIX_hook_module` (`id_module`, `id_hook` , `position`)
 (SELECT id_module, 50, (SELECT max_position from (SELECT MAX(position)+1 as max_position FROM `PREFIX_hook_module` WHERE `id_hook` = 50) tmp) FROM `PREFIX_module` WHERE `name` = 'backwardcompatibility');
 INSERT INTO `PREFIX_hook_module` (`id_module`, `id_hook` , `position`)
-(SELECT id_module, 54, (SELECT max_position from (SELECT MAX(position)+1 as max_position FROM `PREFIX_hook_module` WHERE `id_hook` = 50) tmp) FROM `PREFIX_module` WHERE `name` = 'backwardcompatibility');
+(SELECT id_module, 54, (SELECT max_position from (SELECT MAX(position)+1 as max_position FROM `PREFIX_hook_module` WHERE `id_hook` = 54) tmp) FROM `PREFIX_module` WHERE `name` = 'backwardcompatibility');
