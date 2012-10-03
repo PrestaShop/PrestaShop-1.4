e0632220e53b741597ecc53c44fa5ec7cb55fb0a
fGaillard <francois.gaillard@prestashop.com>
2012-10-03 09:50:52 +0000

    // SoColissimo : Added accented chars replacement method into the module

--- modules/socolissimo/socolissimo.php
+++ modules/socolissimo/socolissimo.php
@@ -386,15 +386,15 @@ class Socolissimo extends CarrierModule
 			'trClientNumber' => (int)$params['address']->id_customer,
 			'orderId' => $this->formatOrderId((int)($params['address']->id)),
 			'numVersion' => $this->getNumVersion(),
-			'ceCivility' => Tools::replaceAccentedChars($this->getTitle(new Customer($params['address']->id_customer))),
-			'ceFirstName' => Tools::replaceAccentedChars(substr($params['address']->firstname, 0, 29)),
-			'ceCompanyName' => Tools::replaceAccentedChars(substr($params['address']->company, 0, 38)),
-			'ceAdress3'  => Tools::replaceAccentedChars(substr($params['address']->address1, 0, 38)),
-			'ceAdress4' => Tools::replaceAccentedChars(substr($params['address']->address2, 0, 38)),
-			'ceZipCode' => Tools::replaceAccentedChars($params['address']->postcode),
-			'ceTown' => Tools::replaceAccentedChars(substr($params['address']->city, 0, 32)),
-			'ceEmail' => Tools::replaceAccentedChars($params['cookie']->email),
-			'cePhoneNumber' => Tools::replaceAccentedChars(str_replace(array(' ', '.', '-', ',', ';', '+', '/', '\\', '+', '(', ')'),'',$params['address']->phone_mobile)),
+			'ceCivility' => $this->replaceAccentedChars($this->getTitle(new Customer($params['address']->id_customer))),
+			'ceFirstName' => $this->replaceAccentedChars(substr($params['address']->firstname, 0, 29)),
+			'ceCompanyName' => $this->replaceAccentedChars(substr($params['address']->company, 0, 38)),
+			'ceAdress3'  => $this->replaceAccentedChars(substr($params['address']->address1, 0, 38)),
+			'ceAdress4' => $this->replaceAccentedChars(substr($params['address']->address2, 0, 38)),
+			'ceZipCode' => $this->replaceAccentedChars($params['address']->postcode),
+			'ceTown' => $this->replaceAccentedChars(substr($params['address']->city, 0, 32)),
+			'ceEmail' => $this->replaceAccentedChars($params['cookie']->email),
+			'cePhoneNumber' => $this->replaceAccentedChars(str_replace(array(' ', '.', '-', ',', ';', '+', '/', '\\', '+', '(', ')'),'',$params['address']->phone_mobile)),
 			'dyWeight' => (float)($params['cart']->getTotalWeight()) * 1000,
 			'trParamPlus' => $carrierSo->id,
 			'trReturnUrlKo' => htmlentities($this->url, ENT_NOQUOTES, 'UTF-8'),
@@ -792,6 +792,56 @@ class Socolissimo extends CarrierModule
 		else
 			$gender = new Gender($customer->id_gender);
 		return isset($gender->name) ? $gender->name : $title;
+	}
+
+	/**
+	 * @param $str
+	 * @return mixed
+	 */
+	public function replaceAccentedChars($str)
+	{
+		return preg_replace(
+			array(
+				/* Lowercase */
+				'/[\x{0105}\x{00E0}\x{00E1}\x{00E2}\x{00E3}\x{00E4}\x{00E5}]/u',
+				'/[\x{00E7}\x{010D}\x{0107}]/u',
+				'/[\x{010F}]/u',
+				'/[\x{00E8}\x{00E9}\x{00EA}\x{00EB}\x{011B}\x{0119}]/u',
+				'/[\x{00EC}\x{00ED}\x{00EE}\x{00EF}]/u',
+				'/[\x{0142}\x{013E}\x{013A}]/u',
+				'/[\x{00F1}\x{0148}]/u',
+				'/[\x{00F2}\x{00F3}\x{00F4}\x{00F5}\x{00F6}\x{00F8}]/u',
+				'/[\x{0159}\x{0155}]/u',
+				'/[\x{015B}\x{0161}]/u',
+				'/[\x{00DF}]/u',
+				'/[\x{0165}]/u',
+				'/[\x{00F9}\x{00FA}\x{00FB}\x{00FC}\x{016F}]/u',
+				'/[\x{00FD}\x{00FF}]/u',
+				'/[\x{017C}\x{017A}\x{017E}]/u',
+				'/[\x{00E6}]/u',
+				'/[\x{0153}]/u',
+
+				/* Uppercase */
+				'/[\x{0104}\x{00C0}\x{00C1}\x{00C2}\x{00C3}\x{00C4}\x{00C5}]/u',
+				'/[\x{00C7}\x{010C}\x{0106}]/u',
+				'/[\x{010E}]/u',
+				'/[\x{00C8}\x{00C9}\x{00CA}\x{00CB}\x{011A}\x{0118}]/u',
+				'/[\x{0141}\x{013D}\x{0139}]/u',
+				'/[\x{00D1}\x{0147}]/u',
+				'/[\x{00D3}]/u',
+				'/[\x{0158}\x{0154}]/u',
+				'/[\x{015A}\x{0160}]/u',
+				'/[\x{0164}]/u',
+				'/[\x{00D9}\x{00DA}\x{00DB}\x{00DC}\x{016E}]/u',
+				'/[\x{017B}\x{0179}\x{017D}]/u',
+				'/[\x{00C6}]/u',
+				'/[\x{0152}]/u',
+			),
+			array(
+				'a', 'c', 'd', 'e', 'i', 'l', 'n', 'o', 'r', 's', 'ss', 't', 'u', 'y', 'z', 'ae', 'oe',
+				'A', 'C', 'D', 'E', 'L', 'N', 'O', 'R', 'S', 'T', 'U', 'Z', 'AE', 'OE'
+			),
+			$str);
 	}
 }
 
