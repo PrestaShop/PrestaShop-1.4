cdedf7e8c5a4357d4acb6945bc415f3f81cd3713
fGaillard <francois.gaillard@prestashop.com>
2012-10-04 17:27:25 +0000

    [+] TR : Backward compatibility : FR translation added

---
+++ modules/backwardcompatibility/fr.php
@@ -0,0 +1,17 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_17fe55c7ae5b1908d141b09516d018ff'] = 'Rétro compatibilité';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_529f879366286c3b40292a9fed1fb8f4'] = 'Améliore la compatibilité des modules';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_254f642527b45bc260048e30704edb39'] = 'Configuration';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_d1f19d06f76786943c8ec9fcfde29cee'] = 'Voici une liste des modules disponible pour cette boutique. Chacun des modules listés ci-dessous utilisent l\'outil de rétro-compatibilité.';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_939d841459eb572f9f556fbedbd5da22'] = 'Pour fonctionner correctement certains de ces modules doivent être mis à jour.';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_e55f75a29310d7b60f7ac1d390c8ae42'] = 'Module';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_cb0be544ecf97a73f481ef9b4c3c15b2'] = 'Version du Backward C.';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_a1915263415d561ab5f8c7a7fcaa60af'] = 'Droit en écriture';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_06933067aafd48425d67bcb01bba5cb6'] = 'Mise à jour';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_11322c818cc18c58db27d5ee17233bd9'] = 'Pas d\'accès en écriture';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_4200880db93930f652ba53ab226b6a26'] = 'Succes de la mise à jour';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_6d7d82d9c1d7f68064d14e673d53eb19'] = 'Échec de la mise à jour';
+$_MODULE['<{backwardcompatibility}prestashop>backwardcompatibility_afa148a549cce6fbeb05c3285caf05e1'] = 'Mettre à jour les modules';
