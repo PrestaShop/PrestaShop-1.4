4a475dde3ab8895bfc9c651abcd33c9a1cf8200a
François Gaillard <francois.gaillard@prestashop.com>
2012-11-22 17:33:58 +0100

    //remove svn keyword @version Release: $

--- modules/blockcustomerprivacy/blockcustomerprivacy.php
+++ modules/blockcustomerprivacy/blockcustomerprivacy.php
@@ -19,8 +19,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author Prestashop SA <contact@prestashop.com>
-*  @copyright  2007-2010 Prestashop SA
-*  @version  Release: $Revision: 1.4 $
+*  @copyright  2007-2010 Prestashop SA
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
