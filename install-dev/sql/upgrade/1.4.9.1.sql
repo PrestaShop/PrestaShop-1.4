05ddf3ca9c0b4b53c8c042ced55c0966a24cb37c
gRoussac <gregory.roussac@prestashop.com>
2013-01-21 16:09:40 +0100

    [-] Installer : Backport of #PSCFV-6064

--- install-dev/sql/upgrade/1.4.9.1.sql
+++ install-dev/sql/upgrade/1.4.9.1.sql
@@ -11,3 +11,6 @@ INSERT INTO `PREFIX_hook_module` (`id_module`, `id_hook` , `position`)
 (SELECT id_module, 50, (SELECT max_position from (SELECT MAX(position)+1 as max_position FROM `PREFIX_hook_module` WHERE `id_hook` = 50) tmp) FROM `PREFIX_module` WHERE `name` = 'backwardcompatibility');
 INSERT INTO `PREFIX_hook_module` (`id_module`, `id_hook` , `position`)
 (SELECT id_module, 54, (SELECT max_position from (SELECT MAX(position)+1 as max_position FROM `PREFIX_hook_module` WHERE `id_hook` = 54) tmp) FROM `PREFIX_module` WHERE `name` = 'backwardcompatibility');
+
+UPDATE `PREFIX_order_state` SET `send_email` = 1 WHERE `id_order_state` = (SELECT `value` FROM `PREFIX_configuration` WHERE `name` = 'PS_OS_WS_PAYMENT' LIMIT 1);
+UPDATE `PREFIX_order_state_lang` SET `template` = 'payment' WHERE `id_order_state` = (SELECT `value` FROM `PREFIX_configuration` WHERE `name` = 'PS_OS_WS_PAYMENT' LIMIT 1);
