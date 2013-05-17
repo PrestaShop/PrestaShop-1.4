ad94a87024d70cf5c8ab3d9feace1ed0a06ab81c
gRoussac <gregory.roussac@prestashop.com>
2013-05-17 16:47:56 +0200

    // additionnal \t

--- modules/editorial/EditorialClass.php
+++ modules/editorial/EditorialClass.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-class		EditorialClass extends ObjectModel
+class EditorialClass extends ObjectModel
 {
 	/** @var integer editorial id*/
 	public		$id = 1;
