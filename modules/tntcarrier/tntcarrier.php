4540fbadb5f5c4ea1c5352c3a80e82d4558301f5
Francois Gaillard <francois.gaillard@prestashop.com>
2013-06-21 10:48:58 +0200

    [-] MO : TNT Carrier : Fixed merge message

--- modules/tntcarrier/tntcarrier.php
+++ modules/tntcarrier/tntcarrier.php
@@ -51,11 +51,7 @@ class TntCarrier extends CarrierModule
 	{
 		$this->name = 'tntcarrier';
 		$this->tab = 'shipping_logistics';
-<<<<<<< Updated upstream
-		$this->version = '1.9.2';
-=======
 		$this->version = '1.9.3';
->>>>>>> Stashed changes
 		$this->author = 'PrestaShop';
 		$this->limited_countries = array('fr');
 		$this->module_key = 'd4dcfde9937b67002235598ac35cbdf8';
