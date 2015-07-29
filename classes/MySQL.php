3b2a2248409577f44258f7ba7e2224c6caf120b4
gRoussac <gregory.roussac@prestashop.com>
2015-07-29 12:20:41 +0200

    [-] : CORE : bypass mysql_connect warning

--- classes/MySQL.php
+++ classes/MySQL.php
@@ -30,7 +30,7 @@ class MySQLCore extends Db
 	{
 		if (!defined('_PS_DEBUG_SQL_'))
 			define('_PS_DEBUG_SQL_', false);
-		if ($this->_link = mysql_connect($this->_server, $this->_user, $this->_password))
+		if ($this->_link = @mysql_connect($this->_server, $this->_user, $this->_password))
 		{
 			if (!$this->set_db($this->_database))
 				die('The database selection cannot be made.');
